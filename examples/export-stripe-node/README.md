2ba23f438307d2030979094dbea71cb12732a3c8
Peter Marton <peter@tailfin.cloud>
2023-06-09 09:56:45 -0700

    feat(example): add stripe node (#20)

---
+++ examples/export-stripe-node/README.md
@@ -0,0 +1,57 @@
+# OpenMeter Stripe Example
+
+This OpenMeter example will show how you can report metered usage to Stripe; this is needed if you use Usage-Based Pricing (UBP), where customers pay after their reported consumption.
+As Stripe's usage report API has rate limits you can't use it directly for metering; This is where OpenMeter comes to the rescue to report aggregated usage to Stripe.
+
+You can read more about Stripe's Usage-Based Pricing features [here](https://stripe.com/docs/products-prices/pricing-models#usage-based-pricing).
+
+## Our Example
+
+In this example, we integrate usage reporting with Stripe for an imaginary serverless product called Epsilon which charges their customer after execution duration.
+To report usage, Stripe recommends a [periodic usage reporting](https://stripe.com/docs/billing/subscriptions/usage-based#report).
+In this example we will report usage when `npm start` is executed. In a real app you want to report usage periodically via cron or workflow management.
+
+## 1. Setting Up Stripe
+
+In this example we will create a metered product and price with a monthly recurring billing period, priced at $0.1 per unit.
+Run the setup code in this repo with your [Stripe key](https://dashboard.stripe.com/test/apikeys):
+
+```sh
+STRIPE_KEY=sk_test_... npm run setup
+```
+
+This will create product, price, customer and subscription entities in your Stripe test account and print out the links to the them:
+
+```text
+Stripe product created: https://dashboard.stripe.com/test/products/prod_xxx
+Stripe price created: https://dashboard.stripe.com/test/prices/price_xxx
+Stripe customer created: https://dashboard.stripe.com/test/customers/cus_xxx
+Stripe subscription created: https://dashboard.stripe.com/test/subscriptions/sub_xxx
+```
+
+## 2. Report Usage
+
+Ensure your OpenMeter runs. Check out the [quickstart guide](/quickstart) to see how to run OpenMeter.
+Run the following to report usage to Stripe:
+
+```sh
+STRIPE_KEY=sk_test_... npm start
+```
+
+You should see the usage reported to Stripe as:
+
+```text
+stripe_customer: cus_.., stripe_price: price_.., meter: m1, total_usage: 10.000000, from: 2023-06-07 14:00:00 -0700 PDT, to: 2023-07-07 14:00:00 -0700 PDT
+```
+
+If you visit the subscription on the Stripe dashboard, you should see usage reported on it.
+
+## 3. Check out the sample code
+
+Check out the sample code's `app.ts` file in this repo to see how to report usage to Stripe.
+
+> **Note** OpenMeter collects usage in windows. The default window duration is hourly. In this example we round start and end dates to the closest OpenMeter windows.
+> For example if a subscription's billing period ends at 1:45 PM, we will only reported at 1 PM so usage occuring after 1PM will slip into the next billing cycle.
+> It depends on your use-case what window size makes sense for your application. In OpenMeter you can configure window sizes per meter.
+
+> **Note** In the sample code, we call Stripe's report API with `action=set` We do this to ensure idempotency so that no double reporting can happen.
---
+++ examples/export-stripe-node/app.ts
@@ -0,0 +1,113 @@
+import assert from 'assert'
+import fs from 'fs'
+
+import yml from 'yaml'
+import Stripe from 'stripe'
+import moment from 'moment'
+import { OpenAPIClientAxios } from 'openapi-client-axios'
+
+import { Client as OpenMeterClient } from './openapi'
+
+// Environment variables
+assert.ok(
+  process.env.STRIPE_KEY,
+  'STRIPE_KEY environment variables is required'
+)
+
+const stripe = new Stripe(process.env.STRIPE_KEY, { apiVersion: '2022-11-15' })
+const openmeterApi = fs.readFileSync('../../api/openapi.yml', 'utf8')
+const openmeter = await new OpenAPIClientAxios({
+  definition: yml.parse(openmeterApi),
+  withServer: { url: 'http://localhost:8888' },
+}).initSync<OpenMeterClient>()
+
+// In a real app you will probably report hourly or daily and run this script at the same frequency via cron or workflow management
+const reportingFrequency = 'second'
+
+async function main() {
+  // We round down period to closest windows as OpenMeter aggregates usage in windows.
+  // Usage occuring between rounded down date and now will be attributed to the next billing period.
+  const to = moment().startOf(reportingFrequency).toDate()
+  const from = moment(to).subtract(1, reportingFrequency).toDate()
+  const { data: subscriptions } = await stripe.subscriptions.list({
+    status: 'active',
+  })
+
+  // Report usage for all active subscriptions
+  for (const subscription of subscriptions) {
+    // Skip subscriptions that started after `to`.
+    if (moment(subscription.current_period_start).isAfter(to)) {
+      continue
+    }
+
+    await reportUsage(subscription, from, to)
+  }
+}
+
+main()
+  .then(() => console.info('done'))
+  .catch((err) => console.error('failed', err))
+
+/**
+ * Reports usage to Stripe
+ */
+async function reportUsage(
+  subscription: Stripe.Subscription,
+  from: Date,
+  to: Date
+) {
+  for (const item of subscription.items.data) {
+    // Skip non metered items
+    if (item.price.recurring?.usage_type != 'metered') {
+      continue
+    }
+
+    // Skip subscription item if doesn't have corresponding OpenMeter meter
+    const meterId = item.metadata['om_meter_id']
+    if (!meterId) {
+      continue
+    }
+
+    // Query usage from OpenMeter for billing period
+    const customerId =
+      typeof subscription.customer === 'object'
+        ? subscription.customer.id
+        : subscription.customer
+    const values = await openmeter.getValuesByMeterId({
+      meterId,
+      subject: customerId,
+      from: from.toISOString(),
+      to: to.toISOString(),
+    })
+
+    // Sum usage windows
+    // TODO (pmarton): switch to OpenMeter aggregate API
+    const total = values.data.values?.reduce(
+      (total, { value }) => total + (value || 0),
+      0
+    )
+    if (total === undefined) {
+      continue
+    }
+
+    // Report usage to Stripe
+    const timestamp = moment(to).unix()
+    await stripe.subscriptionItems.createUsageRecord(
+      item.id,
+      {
+        quantity: total,
+        timestamp,
+        action: 'set',
+      },
+      {
+        // Ensures we only report once even if scripts runs multiple times.
+        idempotencyKey: `${item.id}-${timestamp}`,
+      }
+    )
+
+    // Debug log
+    console.debug(
+      `stripe_customer: ${customerId}, stripe_price: ${item.price.id}, meter: ${meterId}, total_usage: ${total}, from: ${from}, to: ${to}`
+    )
+  }
+}
