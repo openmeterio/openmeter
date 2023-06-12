8587197eec03009068a8a263d330d873d340bfeb
Peter Marton <email@martonpeter.com>
2023-06-12 14:21:56 -0700

    refactor(kafka_connect): cloudevents struct

--- internal/streaming/kafka_connector/cloudevents.go
+++ internal/streaming/kafka_connector/cloudevents.go
@@ -5,29 +5,22 @@ import (
 )
 
 type CloudEventsKafkaPayload struct {
-	Id      *string `json:"ID"`
-	Type    *string `json:"TYPE"`
-	Source  *string `json:"SOURCE"`
-	Subject string  `json:"SUBJECT"`
-	Time    *string `json:"TIME"`
-	Data    *string `json:"DATA"`
+	Id      string `json:"ID"`
+	Type    string `json:"TYPE"`
+	Source  string `json:"SOURCE"`
+	Subject string `json:"SUBJECT"`
+	Time    string `json:"TIME"`
+	Data    string `json:"DATA"`
 }
 
 func ToCloudEventsKafkaPayload(event event.Event) CloudEventsKafkaPayload {
-	eid := event.ID()
-	ety := event.Type()
-	eso := event.Source()
-	esu := event.Subject()
-	eti := event.Time().String()
-	ed := string(event.Data())
-
 	payload := CloudEventsKafkaPayload{
-		Id:      &eid,
-		Type:    &ety,
-		Source:  &eso,
-		Subject: esu,
-		Time:    &eti,
-		Data:    &ed,
+		Id:      event.ID(),
+		Type:    event.Type(),
+		Source:  event.Source(),
+		Subject: event.Subject(),
+		Time:    event.Time().String(),
+		Data:    string(event.Data()),
 	}
 
 	return payload
