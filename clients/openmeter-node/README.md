57ea5e3012cc9cfb7529b5ee7ea707ec26511217
Peter Marton <email@martonpeter.com>
2023-06-19 17:15:39 -0700

    chore(npm): publish

--- clients/openmeter-node/README.md
+++ clients/openmeter-node/README.md
@@ -15,15 +15,15 @@ const openmeter = new OpenMeter({ baseUrl: 'http://localhost:8888' })
 
 // Ingesting an event
 await openmeter.ingestEvents({
-    specversion: '1.0',
-    id: 'id-1',
-    source: 'my-app',
-    type: 'my-type',
-    subject: 'my-awesome-user-id',
-    time: new Date().toISOString(),
-    data: {
-        api_calls: 1
-    },
+	specversion: '1.0',
+	id: 'id-1',
+	source: 'my-app',
+	type: 'my-type',
+	subject: 'my-awesome-user-id',
+	time: new Date().toISOString(),
+	data: {
+		api_calls: 1,
+	},
 })
 
 // Fetching a meter
@@ -40,24 +40,30 @@ You can pass a custom `fetch` implementation to the constructor and extend the r
 import nodeFetch from 'node-fetch'
 import openmeter from '@openmeter/sdk'
 
-const openmeter = new OpenMeter({ baseUrl: 'http://localhost:8888', fetch: nodeFetch })
+const openmeter = new OpenMeter({
+	baseUrl: 'http://localhost:8888',
+	fetch: nodeFetch,
+})
 
-await openmeter.getMetersById({ meterId: 'm1' }, { headers: { 'x-foo': 'bar' } })
+await openmeter.getMetersById(
+	{ meterId: 'm1' },
+	{ headers: { 'x-foo': 'bar' } }
+)
 ```
 
 ### ingestEvents
 
 ```js
 const { error } = await openmeter.ingestEvents({
-    specversion: '1.0',
-    id: 'id-1',
-    source: 'my-app',
-    type: 'my-type',
-    subject: 'my-awesome-user-id',
-    time: new Date().toISOString(),
-    data: {
-        api_calls: 1
-    },
+	specversion: '1.0',
+	id: 'id-1',
+	source: 'my-app',
+	type: 'my-type',
+	subject: 'my-awesome-user-id',
+	time: new Date().toISOString(),
+	data: {
+		api_calls: 1,
+	},
 })
 ```
 
@@ -76,10 +82,13 @@ const { data, error } = await openmeter.getMetersById({ meterId: 'm1' })
 ### getValuesByMeterId
 
 ```js
-const { data, error } = await openmeter.getValuesByMeterId({ meterId: 'm1' }, {
-    subject: 'my-ubject',
-    windowSize: 'HOUR',
-    from: new Date('2023-01-01'),
-    to: new Date('2023-02-01')
-})
+const { data, error } = await openmeter.getValuesByMeterId(
+	{ meterId: 'm1' },
+	{
+		subject: 'my-ubject',
+		windowSize: 'HOUR',
+		from: new Date('2023-01-01'),
+		to: new Date('2023-02-01'),
+	}
+)
 ```
--- clients/openmeter-node/src/index.ts
+++ clients/openmeter-node/src/index.ts
@@ -1,55 +1,65 @@
-import createClient from "openapi-fetch";
-import type fetch from "node-fetch";
-import type { RequestInit } from "node-fetch";
-import type { paths, components, operations } from "../openapi.js"
+import createClient from 'openapi-fetch'
+import type fetch from 'node-fetch'
+import type { RequestInit } from 'node-fetch'
+import type { paths, components, operations } from '../openapi.js'
 
 export class OpenMeter {
-    private client: ReturnType<typeof createClient<paths>>
+	private client: ReturnType<typeof createClient<paths>>
 
-    constructor(opts: { baseUrl: string, fetch?: typeof fetch }) {
-        this.client = createClient<paths>(opts)
-    }
+	constructor(opts: { baseUrl: string; fetch?: typeof fetch }) {
+		this.client = createClient<paths>(opts)
+	}
 
-    public async ingestEvents(event: components['schemas']['Event'], opts?: RequestInit) {
-        return await this.client.post("/api/v1alpha1/events", {
-            ...opts,
-            headers: {
-                'content-type': 'application/cloudevents+json',
-                ...opts?.headers
-            },
-            body: event,
-        });
-    }
+	public async ingestEvents(
+		event: components['schemas']['Event'],
+		opts?: RequestInit
+	) {
+		return await this.client.post('/api/v1alpha1/events', {
+			...opts,
+			headers: {
+				'content-type': 'application/cloudevents+json',
+				...opts?.headers,
+			},
+			body: event,
+		})
+	}
 
-    public async getMeters(opts?: RequestInit) {
-        return await this.client.get("/api/v1alpha1/meters", {
-            ...opts,
-            headers: {
-                'content-type': 'application/json',
-                ...opts?.headers
-            },
-        });
-    }
+	public async getMeters(opts?: RequestInit) {
+		return await this.client.get('/api/v1alpha1/meters', {
+			...opts,
+			headers: {
+				'content-type': 'application/json',
+				...opts?.headers,
+			},
+		})
+	}
 
-    public async getMetersById(path: operations["getMetersById"]["parameters"]["path"], opts?: RequestInit) {
-        return await this.client.get("/api/v1alpha1/meters/{meterId}", {
-            ...opts,
-            params: { path },
-            headers: {
-                'content-type': 'application/json',
-                ...opts?.headers
-            },
-        });
-    }
+	public async getMetersById(
+		path: operations['getMetersById']['parameters']['path'],
+		opts?: RequestInit
+	) {
+		return await this.client.get('/api/v1alpha1/meters/{meterId}', {
+			...opts,
+			params: { path },
+			headers: {
+				'content-type': 'application/json',
+				...opts?.headers,
+			},
+		})
+	}
 
-    public async getValuesByMeterId(path: operations["getValuesByMeterId"]["parameters"]["path"], query: operations["getValuesByMeterId"]["parameters"]["query"], opts?: RequestInit) {
-        return await this.client.get("/api/v1alpha1/meters/{meterId}/values", {
-            ...opts,
-            params: { path, query },
-            headers: {
-                'content-type': 'application/json',
-                ...opts?.headers
-            },
-        });
-    }
+	public async getValuesByMeterId(
+		path: operations['getValuesByMeterId']['parameters']['path'],
+		query: operations['getValuesByMeterId']['parameters']['query'],
+		opts?: RequestInit
+	) {
+		return await this.client.get('/api/v1alpha1/meters/{meterId}/values', {
+			...opts,
+			params: { path, query },
+			headers: {
+				'content-type': 'application/json',
+				...opts?.headers,
+			},
+		})
+	}
 }
