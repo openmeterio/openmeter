b97202a7945a66e057f7deadf1eed3461f1cceba
Peter Marton <email@martonpeter.com>
2023-08-15 08:57:46 +0200

    fix(eslint): for node client

--- api/client/node/index.ts
+++ api/client/node/index.ts
@@ -4,72 +4,72 @@ import undici from 'undici'
 import { components } from './schemas/openapi.js'
 
 export type OpenMeterConfig = {
-	baseUrl: string
-	token?: string
-	username?: string
-	password?: string
-	headers?: IncomingHttpHeaders
+    baseUrl: string
+    token?: string
+    username?: string
+    password?: string
+    headers?: IncomingHttpHeaders
 }
 
 export type RequestOptions = {
-	headers?: IncomingHttpHeaders
+    headers?: IncomingHttpHeaders
 }
 
 /**
  * Usage Event
  */
 export type Event = {
-	/**
-	 * @description The version of the CloudEvents specification which the event uses.
-	 * @example 1.0
-	 */
-	specversion?: string
-	/**
-	 * @description Unique identifier for the event, defaults to uuid v4.
-	 * @example "5c10fade-1c9e-4d6c-8275-c52c36731d3c"
-	 */
-	id?: string
-	/**
-	 * Format: uri-reference
-	 * @description Identifies the context in which an event happened, defaults to: @openmeter/sdk
-	 * @example services/service-0
-	 */
-	source?: string
-	/**
-	 * @description Describes the type of event related to the originating occurrence.
-	 * @example "api_request"
-	 */
-	type: string
-	/**
- * @description Describes the subject of the event in the context of the event producer (identified by source).
- * @example "customer_id"
- */
-	subject: string
-	/**
-	 * Format: date-time
-	 * @description Date of when the occurrence happened.
-	 * @example new Date('2023-01-01T01:01:01.001Z')
-	 */
-	time?: Date
-	/**
-	 * Format: uri
-	 * @description Identifies the schema that data adheres to.
-	 */
-	dataschema?: string
-	/**
-	 * @description Content type of the data value. Must adhere to RFC 2046 format.
-	 * @example application/json
-	 * @enum {string|null}
-	 */
-	datacontenttype?: 'application/json'
-	/**
-	 * @description The event payload.
-	 * @example {
-	 *   "duration_ms": "12",
-	 *   "path": "/hello"
-	 * }
- */
-	data: Record<string, string | number | Record<string, string | number>>
+    /**
+     * @description The version of the CloudEvents specification which the event uses.
+     * @example 1.0
+     */
+    specversion?: string
+    /**
+     * @description Unique identifier for the event, defaults to uuid v4.
+     * @example "5c10fade-1c9e-4d6c-8275-c52c36731d3c"
+     */
+    id?: string
+    /**
+     * Format: uri-reference
+     * @description Identifies the context in which an event happened, defaults to: @openmeter/sdk
+     * @example services/service-0
+     */
+    source?: string
+    /**
+     * @description Describes the type of event related to the originating occurrence.
+     * @example "api_request"
+     */
+    type: string
+    /**
+     * @description Describes the subject of the event in the context of the event producer (identified by source).
+     * @example "customer_id"
+     */
+    subject: string
+    /**
+     * Format: date-time
+     * @description Date of when the occurrence happened.
+     * @example new Date('2023-01-01T01:01:01.001Z')
+     */
+    time?: Date
+    /**
+     * Format: uri
+     * @description Identifies the schema that data adheres to.
+     */
+    dataschema?: string
+    /**
+     * @description Content type of the data value. Must adhere to RFC 2046 format.
+     * @example application/json
+     * @enum {string|null}
+     */
+    datacontenttype?: 'application/json'
+    /**
+     * @description The event payload.
+     * @example {
+     *   "duration_ms": "12",
+     *   "path": "/hello"
+     * }
+     */
+    data: Record<string, string | number | Record<string, string | number>>
 }
 
 export type Problem = components['schemas']['Problem']
@@ -79,99 +79,99 @@ export type WindowSize = components['schemas']['WindowSize']
 type CloudEvents = components['schemas']['Event']
 
 export class OpenMeter {
-	private config: OpenMeterConfig
+    private config: OpenMeterConfig
 
-	constructor(config: OpenMeterConfig) {
-		this.config = config
-	}
+    constructor(config: OpenMeterConfig) {
+        this.config = config
+    }
 
-	public async ingestEvents(
-		usageEvent: Event,
-		options?: RequestOptions
-	): Promise<void> {
-		// We default where we can to lower the barrier to use CloudEvents
-		const payload: CloudEvents = {
-			specversion: usageEvent.specversion ?? '1.0',
-			id: usageEvent.id ?? crypto.randomUUID(),
-			source: usageEvent.source ?? '@openmeter/sdk',
-			type: usageEvent.type,
-			subject: usageEvent.subject,
-		}
+    public async ingestEvents(
+        usageEvent: Event,
+        options?: RequestOptions
+    ): Promise<void> {
+        // We default where we can to lower the barrier to use CloudEvents
+        const payload: CloudEvents = {
+            specversion: usageEvent.specversion ?? '1.0',
+            id: usageEvent.id ?? crypto.randomUUID(),
+            source: usageEvent.source ?? '@openmeter/sdk',
+            type: usageEvent.type,
+            subject: usageEvent.subject,
+        }
 
-		// Optional fields
-		if (usageEvent.time) {
-			payload.time = usageEvent.time.toISOString()
-		}
-		if (usageEvent.data) {
-			payload.data = usageEvent.data
-		}
-		if (usageEvent.dataschema) {
-			payload.dataschema = usageEvent.dataschema
-		}
-		if (usageEvent.datacontenttype) {
-			if (usageEvent.datacontenttype !== 'application/json') {
-				throw new TypeError(
-					`Unsupported datacontenttype: ${usageEvent.datacontenttype}`
-				)
-			}
+        // Optional fields
+        if (usageEvent.time) {
+            payload.time = usageEvent.time.toISOString()
+        }
+        if (usageEvent.data) {
+            payload.data = usageEvent.data
+        }
+        if (usageEvent.dataschema) {
+            payload.dataschema = usageEvent.dataschema
+        }
+        if (usageEvent.datacontenttype) {
+            if (usageEvent.datacontenttype !== 'application/json') {
+                throw new TypeError(
+                    `Unsupported datacontenttype: ${usageEvent.datacontenttype}`
+                )
+            }
 
-			payload.datacontenttype = usageEvent.datacontenttype
-		}
+            payload.datacontenttype = usageEvent.datacontenttype
+        }
 
-		// Making Request
-		const url = new URL('/api/v1/events', this.config.baseUrl)
-		const resp = await undici.request(url, {
-			method: 'POST',
-			body: JSON.stringify(payload),
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/cloudevents+json',
-				...this.authHeaders(),
-				...this.config.headers,
-				...options?.headers,
-			},
-		})
-		if (resp.statusCode > 299) {
-			const problem = (await resp.body.json()) as Problem
+        // Making Request
+        const url = new URL('/api/v1/events', this.config.baseUrl)
+        const resp = await undici.request(url, {
+            method: 'POST',
+            body: JSON.stringify(payload),
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/cloudevents+json',
+                ...this.authHeaders(),
+                ...this.config.headers,
+                ...options?.headers,
+            },
+        })
+        if (resp.statusCode > 299) {
+            const problem = (await resp.body.json()) as Problem
 
-			throw new HttpError('unexpected status code', {
-				statusCode: resp.statusCode,
-				problem,
-			})
-		}
-	}
+            throw new HttpError('unexpected status code', {
+                statusCode: resp.statusCode,
+                problem,
+            })
+        }
+    }
 
-	private authHeaders(): IncomingHttpHeaders {
-		if (this.config.token) {
-			return {
-				authorization: `Bearer ${this.config.token}`,
-			}
-		}
+    private authHeaders(): IncomingHttpHeaders {
+        if (this.config.token) {
+            return {
+                authorization: `Bearer ${this.config.token}`,
+            }
+        }
 
-		if (this.config.username && this.config.password) {
-			const encoded = Buffer.from(
-				`${this.config.username}:${this.config.password}`
-			).toString('base64')
-			return {
-				authorization: `Basic ${encoded}`,
-			}
-		}
+        if (this.config.username && this.config.password) {
+            const encoded = Buffer.from(
+                `${this.config.username}:${this.config.password}`
+            ).toString('base64')
+            return {
+                authorization: `Basic ${encoded}`,
+            }
+        }
 
-		return {}
-	}
+        return {}
+    }
 }
 
 class HttpError extends Error {
-	public statusCode: number
-	public problem: Problem
+    public statusCode: number
+    public problem: Problem
 
-	constructor(
-		message: string,
-		{ statusCode, problem }: { statusCode: number; problem: Problem }
-	) {
-		super(message)
-		this.name = 'HttpError'
-		this.statusCode = statusCode
-		this.problem = problem
-	}
+    constructor(
+        message: string,
+        { statusCode, problem }: { statusCode: number; problem: Problem }
+    ) {
+        super(message)
+        this.name = 'HttpError'
+        this.statusCode = statusCode
+        this.problem = problem
+    }
 }
--- api/client/node/test/api.test.ts
+++ api/client/node/test/api.test.ts
@@ -5,161 +5,163 @@ import { vi, describe, it, expect, beforeEach } from 'vitest'
 import { OpenMeter, type Event } from '../dist/index.js'
 
 declare module 'vitest' {
-	export interface TestContext {
-		openmeter: OpenMeter
-	}
+    export interface TestContext {
+        openmeter: OpenMeter
+    }
 }
 
 vi.spyOn(undici, 'request').getMockImplementation()
-vi.spyOn(crypto, 'randomUUID').mockReturnValue('aaf17be7-860c-4519-91d3-00d97da3cc65')
+vi.spyOn(crypto, 'randomUUID').mockReturnValue(
+    'aaf17be7-860c-4519-91d3-00d97da3cc65'
+)
 
 describe('api', () => {
-	beforeEach((ctx) => {
-		vi.clearAllMocks()
+    beforeEach((ctx) => {
+        vi.clearAllMocks()
 
-		ctx.openmeter = new OpenMeter({
-			baseUrl: 'http://127.0.0.1:8888',
-		})
-	})
+        ctx.openmeter = new OpenMeter({
+            baseUrl: 'http://127.0.0.1:8888',
+        })
+    })
 
-	describe('ingestEvents', () => {
-		it('should ingest event', async ({ openmeter }) => {
-			const event: Event = {
-				specversion: '1.0',
-				id: 'id-1',
-				source: 'my-app',
-				type: 'my-type',
-				subject: 'my-awesome-user-id',
-				time: new Date(),
-				data: {
-					api_calls: 1,
-				},
-			}
-			await openmeter.ingestEvents(event)
+    describe('ingestEvents', () => {
+        it('should ingest event', async ({ openmeter }) => {
+            const event: Event = {
+                specversion: '1.0',
+                id: 'id-1',
+                source: 'my-app',
+                type: 'my-type',
+                subject: 'my-awesome-user-id',
+                time: new Date(),
+                data: {
+                    api_calls: 1,
+                },
+            }
+            await openmeter.ingestEvents(event)
 
-			expect(undici.request).toHaveBeenCalledWith(
-				new URL('/api/v1/events', 'http://127.0.0.1:8888'),
-				{
-					method: 'POST',
-					body: JSON.stringify(event),
-					headers: {
-						Accept: 'application/json',
-						'Content-Type': 'application/cloudevents+json',
-					},
-				}
-			)
-		})
+            expect(undici.request).toHaveBeenCalledWith(
+                new URL('/api/v1/events', 'http://127.0.0.1:8888'),
+                {
+                    method: 'POST',
+                    body: JSON.stringify(event),
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/cloudevents+json',
+                    },
+                }
+            )
+        })
 
-		it('should ingest event with defaults', async ({ openmeter }) => {
-			const event: Event = {
-				type: 'my-type',
-				subject: 'my-awesome-user-id',
-				data: {
-					api_calls: 1,
-				},
-			}
-			await openmeter.ingestEvents(event)
+        it('should ingest event with defaults', async ({ openmeter }) => {
+            const event: Event = {
+                type: 'my-type',
+                subject: 'my-awesome-user-id',
+                data: {
+                    api_calls: 1,
+                },
+            }
+            await openmeter.ingestEvents(event)
 
-			expect(undici.request).toHaveBeenCalledWith(
-				new URL('/api/v1/events', 'http://127.0.0.1:8888'),
-				{
-					method: 'POST',
-					body: JSON.stringify({
-						specversion: '1.0',
-						id: 'aaf17be7-860c-4519-91d3-00d97da3cc65',
-						source: '@openmeter/sdk',
-						type: 'my-type',
-						subject: 'my-awesome-user-id',
-						data: {
-							api_calls: 1,
-						},
-					}),
-					headers: {
-						Accept: 'application/json',
-						'Content-Type': 'application/cloudevents+json',
-					},
-				}
-			)
-		})
-	})
+            expect(undici.request).toHaveBeenCalledWith(
+                new URL('/api/v1/events', 'http://127.0.0.1:8888'),
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        specversion: '1.0',
+                        id: 'aaf17be7-860c-4519-91d3-00d97da3cc65',
+                        source: '@openmeter/sdk',
+                        type: 'my-type',
+                        subject: 'my-awesome-user-id',
+                        data: {
+                            api_calls: 1,
+                        },
+                    }),
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/cloudevents+json',
+                    },
+                }
+            )
+        })
+    })
 
-	// describe('listMeters', () => {
-	// 	it('should list meters', async ({ openmeter }) => {
-	// 		await openmeter.meters.listMeters()
+    // describe('listMeters', () => {
+    // 	it('should list meters', async ({ openmeter }) => {
+    // 		await openmeter.meters.listMeters()
 
-	// 		expect(fetch).toHaveBeenCalledOnce()
-	// 		expect(fetch).toHaveBeenCalledWith(
-	// 			'http://127.0.0.1:8888/api/v1/meters',
-	// 			expect.objectContaining({
-	// 				method: 'GET',
-	// 				headers: new Headers({
-	// 					Accept: 'application/json',
-	// 				}),
-	// 			})
-	// 		)
-	// 	})
-	// })
+    // 		expect(fetch).toHaveBeenCalledOnce()
+    // 		expect(fetch).toHaveBeenCalledWith(
+    // 			'http://127.0.0.1:8888/api/v1/meters',
+    // 			expect.objectContaining({
+    // 				method: 'GET',
+    // 				headers: new Headers({
+    // 					Accept: 'application/json',
+    // 				}),
+    // 			})
+    // 		)
+    // 	})
+    // })
 
-	// describe('getMeter', () => {
-	// 	it('should get meter', async ({ openmeter }) => {
-	// 		const meterSlug = 'm1'
-	// 		await openmeter.meters.getMeter(meterSlug)
+    // describe('getMeter', () => {
+    // 	it('should get meter', async ({ openmeter }) => {
+    // 		const meterSlug = 'm1'
+    // 		await openmeter.meters.getMeter(meterSlug)
 
-	// 		expect(fetch).toHaveBeenCalledOnce()
-	// 		expect(fetch).toHaveBeenCalledWith(
-	// 			'http://127.0.0.1:8888/api/v1/meters/m1',
-	// 			expect.objectContaining({
-	// 				method: 'GET',
-	// 				headers: new Headers({
-	// 					Accept: 'application/json',
-	// 				}),
-	// 			})
-	// 		)
-	// 	})
-	// })
+    // 		expect(fetch).toHaveBeenCalledOnce()
+    // 		expect(fetch).toHaveBeenCalledWith(
+    // 			'http://127.0.0.1:8888/api/v1/meters/m1',
+    // 			expect.objectContaining({
+    // 				method: 'GET',
+    // 				headers: new Headers({
+    // 					Accept: 'application/json',
+    // 				}),
+    // 			})
+    // 		)
+    // 	})
+    // })
 
-	// describe('getMeterValues', () => {
-	// 	it('should get meter values', async ({ openmeter }) => {
-	// 		const meterSlug = 'm1'
-	// 		await openmeter.meters.getMeterValues(meterSlug)
+    // describe('getMeterValues', () => {
+    // 	it('should get meter values', async ({ openmeter }) => {
+    // 		const meterSlug = 'm1'
+    // 		await openmeter.meters.getMeterValues(meterSlug)
 
-	// 		expect(fetch).toHaveBeenCalledOnce()
-	// 		expect(fetch).toHaveBeenCalledWith(
-	// 			'http://127.0.0.1:8888/api/v1/meters/m1/values',
-	// 			expect.objectContaining({
-	// 				method: 'GET',
-	// 				headers: new Headers({
-	// 					Accept: 'application/json',
-	// 				}),
-	// 			})
-	// 		)
-	// 	})
+    // 		expect(fetch).toHaveBeenCalledOnce()
+    // 		expect(fetch).toHaveBeenCalledWith(
+    // 			'http://127.0.0.1:8888/api/v1/meters/m1/values',
+    // 			expect.objectContaining({
+    // 				method: 'GET',
+    // 				headers: new Headers({
+    // 					Accept: 'application/json',
+    // 				}),
+    // 			})
+    // 		)
+    // 	})
 
-	// 	it('should get meter values (with params)', async ({ openmeter }) => {
-	// 		const meterSlug = 'm2'
-	// 		const subject = 'user-1'
-	// 		const from = new Date('2021-01-01')
-	// 		const to = new Date('2021-01-02')
-	// 		const windowSize = WindowSize.HOUR
-	// 		await openmeter.meters.getMeterValues(
-	// 			meterSlug,
-	// 			undefined,
-	// 			subject,
-	// 			from,
-	// 			to,
-	// 			windowSize
-	// 		)
+    // 	it('should get meter values (with params)', async ({ openmeter }) => {
+    // 		const meterSlug = 'm2'
+    // 		const subject = 'user-1'
+    // 		const from = new Date('2021-01-01')
+    // 		const to = new Date('2021-01-02')
+    // 		const windowSize = WindowSize.HOUR
+    // 		await openmeter.meters.getMeterValues(
+    // 			meterSlug,
+    // 			undefined,
+    // 			subject,
+    // 			from,
+    // 			to,
+    // 			windowSize
+    // 		)
 
-	// 		expect(fetch).toHaveBeenCalledOnce()
-	// 		expect(fetch).toHaveBeenCalledWith(
-	// 			'http://127.0.0.1:8888/api/v1/meters/m2/values?subject=user-1&from=2021-01-01T00%3A00%3A00.000Z&to=2021-01-02T00%3A00%3A00.000Z&windowSize=HOUR',
-	// 			expect.objectContaining({
-	// 				method: 'GET',
-	// 				headers: new Headers({
-	// 					Accept: 'application/json',
-	// 				}),
-	// 			})
-	// 		)
-	// 	})
-	// })
+    // 		expect(fetch).toHaveBeenCalledOnce()
+    // 		expect(fetch).toHaveBeenCalledWith(
+    // 			'http://127.0.0.1:8888/api/v1/meters/m2/values?subject=user-1&from=2021-01-01T00%3A00%3A00.000Z&to=2021-01-02T00%3A00%3A00.000Z&windowSize=HOUR',
+    // 			expect.objectContaining({
+    // 				method: 'GET',
+    // 				headers: new Headers({
+    // 					Accept: 'application/json',
+    // 				}),
+    // 			})
+    // 		)
+    // 	})
+    // })
 })
