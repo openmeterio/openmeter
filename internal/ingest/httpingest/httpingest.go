0a0849f08f1614f3eb78ba2a42ebfbee6c10b53b
Zsombor Joel Gyurkovics <32017689+zsomborjoel@users.noreply.github.com>
2023-08-16 15:25:27 +0200

    feat(api): support batch event ingestion (#96)
    * feat: support batch event ingestion (#54)
    
    * refactor: add Sprintf usage and remove Context fron handler (#54)
    
    * feat(api): regenerate
    
    ---------
    
    Co-authored-by: Peter Marton <peter@tailfin.cloud>
    Co-authored-by: Andras Toth <4157749+tothandras@users.noreply.github.com>

--- internal/ingest/httpingest/httpingest.go
+++ internal/ingest/httpingest/httpingest.go
@@ -1,7 +1,9 @@
 package httpingest
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"time"
 
@@ -27,16 +29,64 @@ type Collector interface {
 func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	logger := h.getLogger()
 
-	var event event.Event
+	contentType := r.Header.Get("Content-Type")
+
+	var err error
+	switch contentType {
+	case "application/cloudevents+json":
+		err = h.processSingleRequest(w, r)
+	case "application/cloudevents-batch+json":
+		err = h.processBatchRequest(w, r)
+	default:
+		// this should never happen
+		err = errors.New("invalid content type: " + contentType)
+	}
+
+	if err != nil {
+		logger.ErrorCtx(r.Context(), "unable to process request", "error", err)
+		_ = render.Render(w, r, api.ErrInternalServerError(err))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h Handler) processBatchRequest(w http.ResponseWriter, r *http.Request) error {
+	var events api.IngestEventsApplicationCloudeventsBatchPlusJSONBody
+
+	err := json.NewDecoder(r.Body).Decode(&events)
+	if err != nil {
+		return err
+	}
+
+	for _, event := range events {
+		err = h.processEvent(r.Context(), event)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (h Handler) processSingleRequest(w http.ResponseWriter, r *http.Request) error {
+	var event api.IngestEventsApplicationCloudeventsPlusJSONRequestBody
 
 	err := json.NewDecoder(r.Body).Decode(&event)
 	if err != nil {
-		logger.ErrorCtx(r.Context(), "unable to parse event", "error", err)
-
-		_ = render.Render(w, r, api.ErrInternalServerError(err))
-
-		return
+		return err
 	}
+
+	err = h.processEvent(r.Context(), event)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h Handler) processEvent(context context.Context, event event.Event) error {
+	logger := h.getLogger()
 
 	logger = logger.With(
 		slog.String("event_id", event.ID()),
@@ -45,23 +95,17 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	)
 
 	if event.Time().IsZero() {
-		logger.DebugCtx(r.Context(), "event does not have a timestamp")
-
+		logger.DebugCtx(context, "event does not have a timestamp")
 		event.SetTime(time.Now().UTC())
 	}
 
-	err = h.Collector.Receive(event)
+	err := h.Collector.Receive(event)
 	if err != nil {
-		logger.ErrorCtx(r.Context(), "unable to forward event to collector", "error", err)
-
-		_ = render.Render(w, r, api.ErrInternalServerError(err))
-
-		return
+		return err
 	}
 
-	logger.InfoCtx(r.Context(), "event forwarded to downstream collector")
-
-	w.WriteHeader(http.StatusOK)
+	logger.InfoCtx(context, "event forwarded to downstream collector")
+	return nil
 }
 
 func (h Handler) getLogger() *slog.Logger {
--- internal/ingest/httpingest/httpingest_test.go
+++ internal/ingest/httpingest/httpingest_test.go
@@ -3,8 +3,10 @@ package httpingest
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"sync"
 	"testing"
 	"time"
@@ -50,7 +52,7 @@ func TestHandler(t *testing.T) {
 	err := json.NewEncoder(&buf).Encode(ev)
 	require.NoError(t, err)
 
-	resp, err := client.Post(server.URL, "", &buf)
+	resp, err := client.Post(server.URL, "application/cloudevents+json", &buf)
 	require.NoError(t, err)
 
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
@@ -63,4 +65,45 @@ func TestHandler(t *testing.T) {
 	assert.Equal(t, ev.Subject(), receivedEvent.Subject())
 	assert.Equal(t, ev.Source(), receivedEvent.Source())
 	assert.Equal(t, receivedEvent.Time(), ev.Time())
+}
+
+func TestBatchHandler(t *testing.T) {
+	collector := &inMemoryCollector{}
+	handler := Handler{
+		Collector: collector,
+	}
+	server := httptest.NewServer(handler)
+	client := server.Client()
+
+	now := time.Date(2023, 06, 15, 14, 33, 00, 00, time.UTC)
+
+	var events []event.Event
+	for i := 1; i <= 10; i++ {
+		id := strconv.Itoa(i)
+
+		event := event.New()
+		event.SetID(fmt.Sprintf("id%s", id))
+		event.SetSubject(fmt.Sprintf("sub%s", id))
+		event.SetSource(fmt.Sprintf("test%s", id))
+		event.SetTime(now.Add(time.Duration(i) * time.Second))
+		events = append(events, event)
+	}
+
+	var buf bytes.Buffer
+	err := json.NewEncoder(&buf).Encode(events)
+	require.NoError(t, err)
+
+	resp, err := client.Post(server.URL, "application/cloudevents-batch+json", &buf)
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	require.Len(t, collector.events, 10)
+	for i, event := range collector.events {
+		event := event
+		assert.Equal(t, events[i].ID(), event.ID())
+		assert.Equal(t, events[i].Subject(), event.Subject())
+		assert.Equal(t, events[i].Source(), event.Source())
+		assert.Equal(t, event.Time(), events[i].Time())
+	}
 }
