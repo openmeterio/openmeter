843e9c716fea542e6424950ff3b2a53acdf0e702
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-08-22 14:24:37 +0200

    chore: improve dev compose
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -47,6 +47,7 @@
               rdkafka # https://github.com/confluentinc/confluent-kafka-go#librdkafka
               cyrus_sasl
               pkg-config
+              # confluent-platform
 
               golangci-lint
               goreleaser
