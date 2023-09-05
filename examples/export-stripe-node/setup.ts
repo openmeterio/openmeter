2d31d61e75f6362552e242143362d8d830f8ec62
Peter Marton <email@martonpeter.com>
2023-09-05 14:13:51 +0200

    refactor(examples): node examples with indent two and latest sdk

--- examples/export-stripe-node/setup.ts
+++ examples/export-stripe-node/setup.ts
@@ -5,8 +5,8 @@ import Stripe from 'stripe'
 
 // Environment variables
 assert.ok(
-	process.env.STRIPE_KEY,
-	'STRIPE_KEY environment variables is required'
+  process.env.STRIPE_KEY,
+  'STRIPE_KEY environment variables is required'
 )
 
 const stripe = new Stripe(process.env.STRIPE_KEY, { apiVersion: '2022-11-15' })
@@ -15,59 +15,59 @@ const stripe = new Stripe(process.env.STRIPE_KEY, { apiVersion: '2022-11-15' })
 var meterSlug = 'm2'
 
 async function setup() {
-	// Create a Stripe Product
-	const product = await stripe.products.create({
-		name: 'AI Tokens',
-	})
-	console.log(
-		`Stripe product created: https://dashboard.stripe.com/test/products/${product.id}`
-	)
+  // Create a Stripe Product
+  const product = await stripe.products.create({
+    name: 'AI Tokens',
+  })
+  console.log(
+    `Stripe product created: https://dashboard.stripe.com/test/products/${product.id}`
+  )
 
-	// Create a metered Stripe Price
-	const price = await stripe.prices.create({
-		product: product.id,
-		// The meter ID this price belongs to
-		currency: 'usd',
-		recurring: {
-			interval: 'month',
-			usage_type: 'metered',
-		},
-		billing_scheme: 'per_unit',
-		unit_amount: 1, // cent
-	})
-	console.log(
-		`Stripe price created: https://dashboard.stripe.com/test/prices/${price.id}`
-	)
+  // Create a metered Stripe Price
+  const price = await stripe.prices.create({
+    product: product.id,
+    // The meter ID this price belongs to
+    currency: 'usd',
+    recurring: {
+      interval: 'month',
+      usage_type: 'metered',
+    },
+    billing_scheme: 'per_unit',
+    unit_amount: 1, // cent
+  })
+  console.log(
+    `Stripe price created: https://dashboard.stripe.com/test/prices/${price.id}`
+  )
 
-	// Create a Stripe customer
-	const customer = await stripe.customers.create({
-		name: 'My Awesome Customer',
-		metadata: {
-			// Useful to map to internal ID
-			external_key: 'my-awesome-user-id',
-		},
-	})
-	console.log(
-		`Stripe customer created: https://dashboard.stripe.com/test/customers/${customer.id}`
-	)
+  // Create a Stripe customer
+  const customer = await stripe.customers.create({
+    name: 'My Awesome Customer',
+    metadata: {
+      // Useful to map to internal ID
+      external_key: 'my-awesome-user-id',
+    },
+  })
+  console.log(
+    `Stripe customer created: https://dashboard.stripe.com/test/customers/${customer.id}`
+  )
 
-	// Start a new Stripe subscription for customer with the price created above
-	const subscription = await stripe.subscriptions.create({
-		customer: customer.id,
-		items: [
-			{
-				price: price.id,
-				metadata: {
-					om_meter_id: meterSlug,
-				},
-			},
-		],
-	})
-	console.log(
-		`Stripe subscription created: https://dashboard.stripe.com/test/subscriptions/${subscription.id}`
-	)
+  // Start a new Stripe subscription for customer with the price created above
+  const subscription = await stripe.subscriptions.create({
+    customer: customer.id,
+    items: [
+      {
+        price: price.id,
+        metadata: {
+          om_meter_id: meterSlug,
+        },
+      },
+    ],
+  })
+  console.log(
+    `Stripe subscription created: https://dashboard.stripe.com/test/subscriptions/${subscription.id}`
+  )
 }
 
 setup()
-	.then(() => console.info('setup done'))
-	.catch((err) => console.error('setup failed', err))
+  .then(() => console.info('setup done'))
+  .catch((err) => console.error('setup failed', err))
--- examples/ingest-fastify-node/app.ts
+++ examples/ingest-fastify-node/app.ts
@@ -6,90 +6,90 @@ import fastifySession from '@fastify/session'
 
 const openmeter = new OpenMeter({ baseUrl: 'http://localhost:8888' })
 const server = fastify({
-    logger: true,
+  logger: true,
 })
 
 server.register(fastifyCookie)
 server.register(fastifySession, {
-    secret: 'a secret with minimum length of 32 characters',
+  secret: 'a secret with minimum length of 32 characters',
 })
 
 // To make testing this example easier we list all the meter values for root request
 // This endpoint is not metered
 server.get('/', {
-    schema: {
-        querystring: {
-            subject: { type: 'string', nullable: true },
-            from: { type: 'string', format: 'date-time', nullable: true },
-            to: { type: 'string', format: 'date-time', nullable: true },
-            windowSize: { type: 'string', enum: ['MINUTE', 'HOUR', 'DAY'], nullable: true }
-        },
+  schema: {
+    querystring: {
+      subject: { type: 'string', nullable: true },
+      from: { type: 'string', format: 'date-time', nullable: true },
+      to: { type: 'string', format: 'date-time', nullable: true },
+      windowSize: { type: 'string', enum: ['MINUTE', 'HOUR', 'DAY'], nullable: true }
     },
-    handler: async (
-        req: FastifyRequest<{
-            Querystring: { subject?: string; from?: string; to?: string, windowSize?: WindowSize }
-        }>
-    ) => {
-        const values = await openmeter.meters.values('m1', {
-            subject: req.query.subject,
-            from: req.query.from ? new Date(req.query.from) : undefined,
-            to: req.query.to ? new Date(req.query.to) : undefined,
-            windowSize: req.query.windowSize
-        })
-        return values
-    },
+  },
+  handler: async (
+    req: FastifyRequest<{
+      Querystring: { subject?: string; from?: string; to?: string, windowSize?: WindowSize }
+    }>
+  ) => {
+    const values = await openmeter.meters.values('m1', {
+      subject: req.query.subject,
+      from: req.query.from ? new Date(req.query.from) : undefined,
+      to: req.query.to ? new Date(req.query.to) : undefined,
+      windowSize: req.query.windowSize
+    })
+    return values
+  },
 })
 
 // Metered APIs on /api
 server.register(
-    (instance, opts, next) => {
-        // Example metered API on GET /api
-        instance.get('/', () => {
-            return 'hello metered api'
-        })
+  (instance, opts, next) => {
+    // Example metered API on GET /api
+    instance.get('/', () => {
+      return 'hello metered api'
+    })
 
-        // Set session, see: https://github.com/fastify/session
-        instance.addHook('preHandler', (request, reply, next) => {
-            request.session.user = { id: 'my-test-id', name: 'Test User' }
-            next()
-        })
+    // Set session, see: https://github.com/fastify/session
+    instance.addHook('preHandler', (request, reply, next) => {
+      request.session.user = { id: 'my-test-id', name: 'Test User' }
+      next()
+    })
 
-        // Execute metering
-        instance.addHook('onResponse', async (request, reply) => {
-            const reqId = request.headers['x-request-id']
-            const id = typeof reqId === 'string' ? reqId : randomUUID()
+    // Execute metering
+    instance.addHook('onResponse', async (request, reply) => {
+      const reqId = request.headers['x-request-id']
+      const id = typeof reqId === 'string' ? reqId : randomUUID()
 
-            await openmeter.events.ingest({
-                specversion: '1.0',
-                id,
-                source: 'my-app',
-                type: 'api-calls',
-                subject: request.session.user.id,
-                data: {
-                    method: request.method,
-                    path: request.routerPath,
-                    duration_ms: reply.getResponseTime().toString(),
-                },
-            })
-        })
+      await openmeter.events.ingest({
+        specversion: '1.0',
+        id,
+        source: 'my-app',
+        type: 'api-calls',
+        subject: request.session.user.id,
+        data: {
+          method: request.method,
+          path: request.routerPath,
+          duration_ms: reply.getResponseTime().toString(),
+        },
+      })
+    })
 
-        next()
-    },
-    { prefix: 'api' }
+    next()
+  },
+  { prefix: 'api' }
 )
 
 server.listen({ port: 3000 }, (err) => {
-    if (err) {
-        console.error(err)
-        process.exit(1)
-    }
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
 })
 
 declare module 'fastify' {
-    interface Session {
-        user: {
-            id: string
-            name: string
-        }
+  interface Session {
+    user: {
+      id: string
+      name: string
     }
+  }
 }
