afc84893d681f6e8bb58d89443470f3b6bf42492
Peter Marton <email@martonpeter.com>
2023-10-18 12:30:23 -0700

    fix(openmeter): export dedupe

--- openmeter/ingest/dedupe.go
+++ openmeter/ingest/dedupe.go
@@ -2,7 +2,11 @@ package ingest
 
 import (
 	"github.com/openmeterio/openmeter/internal/dedupe"
+	"github.com/openmeterio/openmeter/internal/ingest"
 )
 
 // Deduplicator checks if an event is unique.
 type Deduplicator = dedupe.Deduplicator
+
+// DeduplicatingCollector implements event deduplication at event ingestion.
+type DeduplicatingCollector = ingest.DeduplicatingCollector
