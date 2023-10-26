e52328df78f1cd71c2e5be23e719817bbb83b28c
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-10-26 16:22:33 +0200

    ci: upgrade dagger to modules
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- ci/go.mod
+++ ci/go.mod
@@ -1,19 +1,15 @@
-module github.com/openmeterio/openmeter/ci
+module ci
 
-go 1.20
+go 1.21.2
 
 require (
-	dagger.io/dagger v0.8.4
-	github.com/magefile/mage v1.15.0
+	github.com/99designs/gqlgen v0.17.31
+	github.com/Khan/genqlient v0.6.0
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
+	golang.org/x/sync v0.3.0
 )
 
 require (
-	github.com/99designs/gqlgen v0.17.36 // indirect
-	github.com/Khan/genqlient v0.6.0 // indirect
-	github.com/adrg/xdg v0.4.0 // indirect
-	github.com/iancoleman/strcase v0.3.0 // indirect
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
-	github.com/vektah/gqlparser/v2 v2.5.8 // indirect
-	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	github.com/stretchr/testify v1.8.3 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.6 // indirect
 )
--- ci/main.go
+++ ci/main.go
@@ -1,11 +1,72 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"os"
-
-	"github.com/magefile/mage/mage"
+	"path/filepath"
 )
 
-func main() {
-	os.Exit(mage.Main())
+const (
+	goVersion           = "1.21.0"
+	golangciLintVersion = "1.54.2"
+)
+
+type Ci struct{}
+
+func (m *Ci) Check(ctx context.Context) error {
+	test, lint := m.Test(), m.Lint()
+
+	_, err := test.Sync(ctx)
+	if err != nil {
+		return err
+	}
+
+	_, err = lint.Sync(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *Ci) Test() *Container {
+	host := dag.Host()
+
+	return dag.Container().
+		From(fmt.Sprintf("golang:%s", goVersion)).
+		WithMountedCache("/root/.cache/go-build", dag.CacheVolume("go-build")).
+		WithMountedCache("/go/pkg/mod", dag.CacheVolume("go-mod")).
+		WithMountedDirectory("/src", host.Directory(root(), HostDirectoryOpts{
+			Exclude: []string{".direnv", ".devenv", "api/client/node/node_modules"},
+		})).
+		WithWorkdir("/src").
+		WithExec([]string{"go", "test", "-v", "./..."})
+}
+
+func (m *Ci) Lint() *Container {
+	host := dag.Host()
+
+	bin := dag.Container().
+		From(fmt.Sprintf("docker.io/golangci/golangci-lint:v%s", golangciLintVersion)).
+		File("/usr/bin/golangci-lint")
+
+	return dag.Container().
+		From(fmt.Sprintf("golang:%s", goVersion)).
+		WithMountedCache("/root/.cache/go-build", dag.CacheVolume("go-build")).
+		WithMountedCache("/go/pkg/mod", dag.CacheVolume("go-mod")).
+		WithMountedDirectory("/src", host.Directory(root(), HostDirectoryOpts{
+			Exclude: []string{".direnv", ".devenv", "api/client/node/node_modules"},
+		})).
+		WithWorkdir("/src").
+		WithFile("/usr/local/bin/golangci-lint", bin).
+		WithExec([]string{"golangci-lint", "run", "--verbose"})
+}
+
+func root() string {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return filepath.Join(wd, "..")
 }
