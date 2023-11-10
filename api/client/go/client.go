e137de9fcabb8f185bfad358bbb146760c4e566c
Peter Marton <peter@openmeter.io>
2023-11-10 08:22:52 -0800

    feat(client): go readme

--- api/client/go/client.go
+++ api/client/go/client.go
@@ -3,7 +3,17 @@ package openmeter
 
 import (
 	"context"
+	"net/http"
 )
+
+// Sugar syntax top of generated client
+func (c *Client) IngestEvent(ctx context.Context, event Event, reqEditors ...RequestEditorFn) (*http.Response, error) {
+	return c.IngestEventsWithApplicationCloudeventsPlusJSONBody(ctx, event, reqEditors...)
+}
+
+func (c *Client) IngestEventBatchWithResponse(ctx context.Context, events []Event, reqEditors ...RequestEditorFn) (*http.Response, error) {
+	return c.IngestEventsWithApplicationCloudeventsBatchPlusJSONBody(ctx, events, reqEditors...)
+}
 
 func (c *ClientWithResponses) IngestEventWithResponse(ctx context.Context, event Event, reqEditors ...RequestEditorFn) (*IngestEventsResponse, error) {
 	return c.IngestEventsWithApplicationCloudeventsPlusJSONBodyWithResponse(ctx, event, reqEditors...)
