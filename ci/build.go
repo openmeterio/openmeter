a3bae4b5897cf8c415c463bfc5f7a127f1516506
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-01-14 23:24:37 +0100

    ci: update CI modules
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- ci/build.go
+++ ci/build.go
@@ -2,23 +2,35 @@ package main
 
 import (
 	"context"
-	"path/filepath"
 	"strings"
+	"time"
 
 	"golang.org/x/sync/errgroup"
 )
 
+// Build individual artifacts. (Useful for testing and development)
 func (m *Ci) Build() *Build {
-	return &Build{}
+	return &Build{
+		Source: m.Source,
+	}
 }
 
-type Build struct{}
+type Build struct {
+	// +private
+	Source *Directory
+}
 
-func (m *Build) All(ctx context.Context) error {
+func (m *Build) All(
+	ctx context.Context,
+
+	// Target platform in "[os]/[platform]/[version]" format (e.g., "darwin/arm64/v7", "windows/amd64", "linux/arm64").
+	// +optional
+	platform Platform,
+) error {
 	var group errgroup.Group
 
 	group.Go(func() error {
-		err := m.Binary().All(ctx)
+		_, err := m.ContainerImage(platform).Sync(ctx)
 		if err != nil {
 			return err
 		}
@@ -27,16 +39,7 @@ func (m *Build) All(ctx context.Context) error {
 	})
 
 	group.Go(func() error {
-		_, err := m.ContainerImage().Sync(ctx)
-		if err != nil {
-			return err
-		}
-
-		return nil
-	})
-
-	group.Go(func() error {
-		_, err := m.HelmChart(OptEmpty[string]()).Sync(ctx)
+		_, err := m.HelmChart("").Sync(ctx)
 		if err != nil {
 			return err
 		}
@@ -47,25 +50,75 @@ func (m *Build) All(ctx context.Context) error {
 	return group.Wait()
 }
 
-func (m *Build) ContainerImage() *Container {
-	return dag.Container().
-		From("alpine:3.18.5@sha256:34871e7290500828b39e22294660bee86d966bc0017544e848dd9a255cdf59e0").
-		WithExec([]string{"apk", "add", "--update", "--no-cache", "ca-certificates", "tzdata", "bash"}). // TODO: use apko instead?
-		WithMountedFile("/usr/local/bin/openmeter", m.Binary().Api()).
-		WithMountedFile("/usr/local/bin/openmeter-sink-worker", m.Binary().SinkWorker())
+func (m *Build) containerImages(version string) []*Container {
+	platforms := []Platform{
+		"linux/amd64",
+		"linux/arm64",
+	}
+
+	variants := make([]*Container, 0, len(platforms))
+
+	for _, platform := range platforms {
+		variants = append(variants, m.containerImage(platform, version))
+	}
+
+	return variants
 }
 
+// Build a container image.
+func (m *Build) ContainerImage(
+	// Target platform in "[os]/[platform]/[version]" format (e.g., "darwin/arm64/v7", "windows/amd64", "linux/arm64").
+	// +optional
+	platform Platform,
+) *Container {
+	return m.containerImage(platform, "")
+}
+
+func (m *Build) containerImage(platform Platform, version string) *Container {
+	return dag.Container(ContainerOpts{Platform: platform}).
+		From(alpineBaseImage).
+		WithLabel("org.opencontainers.image.title", "openmeter").
+		WithLabel("org.opencontainers.image.description", "Cloud Metering for AI, Billing and FinOps. Collect and aggregate millions of usage events in real-time.").
+		WithLabel("org.opencontainers.image.url", "https://github.com/openmeterio/openmeter").
+		WithLabel("org.opencontainers.image.created", time.Now().String()). // TODO: embed commit timestamp
+		WithLabel("org.opencontainers.image.source", "https://github.com/openmeterio/openmeter").
+		WithLabel("org.opencontainers.image.licenses", "Apache-2.0").
+		With(func(c *Container) *Container {
+			if version != "" {
+				c = c.WithLabel("org.opencontainers.image.version", version)
+			}
+
+			return c
+		}).
+		WithExec([]string{"apk", "add", "--update", "--no-cache", "ca-certificates", "tzdata", "bash"}).
+		WithFile("/usr/local/bin/openmeter", m.Binary().api(platform, version)).
+		WithFile("/usr/local/bin/openmeter-sink-worker", m.Binary().sinkWorker(platform, version))
+}
+
+// Build binaries.
 func (m *Build) Binary() *Binary {
-	return &Binary{}
+	return &Binary{
+		Source: m.Source,
+	}
 }
 
-type Binary struct{}
+type Binary struct {
+	// +private
+	Source *Directory
+}
 
-func (m *Binary) All(ctx context.Context) error {
+// Build all binaries.
+func (m *Binary) All(
+	ctx context.Context,
+
+	// Target platform in "[os]/[platform]/[version]" format (e.g., "darwin/arm64/v7", "windows/amd64", "linux/arm64").
+	// +optional
+	platform Platform,
+) error {
 	var group errgroup.Group
 
 	group.Go(func() error {
-		_, err := m.Api().Sync(ctx)
+		_, err := m.Api(platform).Sync(ctx)
 		if err != nil {
 			return err
 		}
@@ -74,7 +127,7 @@ func (m *Binary) All(ctx context.Context) error {
 	})
 
 	group.Go(func() error {
-		_, err := m.SinkWorker().Sync(ctx)
+		_, err := m.SinkWorker(platform).Sync(ctx)
 		if err != nil {
 			return err
 		}
@@ -85,38 +138,97 @@ func (m *Binary) All(ctx context.Context) error {
 	return group.Wait()
 }
 
-func (m *Binary) Api() *File {
-	return buildDir().DockerBuild().File("/usr/local/bin/openmeter")
+// Build the API server binary.
+func (m *Binary) Api(
+	// Target platform in "[os]/[platform]/[version]" format (e.g., "darwin/arm64/v7", "windows/amd64", "linux/arm64").
+	// +optional
+	platform Platform,
+) *File {
+	return m.api(platform, "")
 }
 
-func (m *Binary) SinkWorker() *File {
-	return buildDir().DockerBuild().File("/usr/local/bin/openmeter-sink-worker")
+func (m *Binary) api(platform Platform, version string) *File {
+	return m.build(platform, version, "./cmd/server")
 }
 
-func (m *Build) HelmChart(version Optional[string]) *File {
-	chart := m.helmChartDir()
+// Build the sink worker binary.
+func (m *Binary) SinkWorker(
+	// Target platform in "[os]/[platform]/[version]" format (e.g., "darwin/arm64/v7", "windows/amd64", "linux/arm64").
+	// +optional
+	platform Platform,
+) *File {
+	return m.sinkWorker(platform, "")
+}
 
-	opts := HelmBasePackageOpts{
+func (m *Binary) sinkWorker(platform Platform, version string) *File {
+	return m.build(platform, version, "./cmd/sink-worker")
+}
+
+func (m *Binary) build(platform Platform, version string, pkg string) *File {
+	if version == "" {
+		version = "unknown"
+	}
+
+	goModule := buildContainer(platform)
+
+	binary := goModule.
+		WithSource(m.Source).
+		Build(GoWithSourceBuildOpts{
+			Pkg:      pkg,
+			Trimpath: true,
+			Tags:     []string{"musl"},
+			RawArgs: []string{
+				"-ldflags",
+				"-s -w -linkmode external -extldflags \"-static\" -X main.version=" + version,
+			},
+		})
+
+	return goModule.
+		Container().
+		WithFile("/out/binary", binary).
+		WithExec([]string{"xx-verify", "/out/binary"}).
+		File("/out/binary")
+}
+
+func buildContainer(platform Platform) *Go {
+	return dag.Go(GoOpts{
+		Container: dag.Go(GoOpts{Version: goVersion}).
+			WithEnvVariable("TARGETPLATFORM", string(platform)).
+			WithCgoEnabled().
+			Container().
+			WithDirectory("/", dag.Container().From(xxBaseImage).Rootfs()).
+			WithExec([]string{"apk", "add", "--update", "--no-cache", "ca-certificates", "make", "git", "curl", "clang", "lld"}).
+			WithExec([]string{"xx-apk", "add", "--update", "--no-cache", "musl-dev", "gcc"}).
+			WithExec([]string{"xx-go", "--wrap"}),
+	})
+}
+
+func (m *Build) HelmChart(
+	// Release version.
+	// +optional
+	version string,
+) *File {
+	chart := helmChartDir(m.Source)
+
+	opts := HelmPackageOpts{
 		DependencyUpdate: true,
 	}
 
-	if v, ok := version.Get(); ok {
-		opts.Version = strings.TrimPrefix(v, "v")
-		opts.AppVersion = v
+	if version != "" {
+		opts.Version = strings.TrimPrefix(version, "v")
+		opts.AppVersion = version
 	}
 
-	return dag.Helm().FromVersion(helmVersion).Package(chart, opts)
+	return dag.Helm(HelmOpts{Version: helmVersion}).Package(chart, opts)
 }
 
-func (m *Build) helmChartDir() *Directory {
-	chart := dag.Host().Directory(filepath.Join(root(), "deploy/charts/openmeter"), HostDirectoryOpts{
-		Exclude: []string{"charts"}, // exclude dependencies
-	})
+func helmChartDir(source *Directory) *Directory {
+	chart := source.Directory("deploy/charts/openmeter")
 
-	readme := dag.HelmDocs().FromVersion(helmDocsVersion).Generate(chart, HelmDocsBaseGenerateOpts{
+	readme := dag.HelmDocs(HelmDocsOpts{Version: helmDocsVersion}).Generate(chart, HelmDocsGenerateOpts{
 		Templates: []*File{
-			dag.Host().File(filepath.Join(root(), "deploy/charts/template.md")),
-			dag.Host().File(filepath.Join(root(), "deploy/charts/openmeter/README.tmpl.md")),
+			source.File("deploy/charts/template.md"),
+			source.File("deploy/charts/openmeter/README.tmpl.md"),
 		},
 		SortValuesOrder: "file",
 	})
