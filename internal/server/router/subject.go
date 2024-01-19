66b608c7a9a1b88256a26b63ea5b1ea383a81f6b
Peter Marton <peter@tailfin.cloud>
2024-01-19 10:30:54 -0800

    feat(api): add subjects (#534)

---
+++ internal/server/router/subject.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/openmeterio/openmeter/pkg/models"
+)
+
+func (a *Router) UpsertSubject(w http.ResponseWriter, r *http.Request) {
+	err := fmt.Errorf("not implemented: subject management is only available in OpenMeter Cloud")
+	models.NewStatusProblem(r.Context(), err, http.StatusNotImplemented).Respond(w, r)
+}
+
+func (a *Router) ListSubjects(w http.ResponseWriter, r *http.Request) {
+	err := fmt.Errorf("not implemented: subjects are only available in OpenMeter Cloud")
+	models.NewStatusProblem(r.Context(), err, http.StatusNotImplemented).Respond(w, r)
+}
