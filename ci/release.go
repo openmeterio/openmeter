e70a001d45b1fc7722fa7e2f9139ca57cdfa1266
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-01-24 09:39:52 +0100

    ci: refactor helm release
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- ci/release.go
+++ ci/release.go
@@ -10,32 +10,26 @@ func (m *Ci) Release(ctx context.Context, version string, githubActor string, gi
 	var group errgroup.Group
 
 	group.Go(func() error {
-		chart := m.Build().HelmChart("openmeter", version)
-
-		_, err := dag.Helm(HelmOpts{Version: helmVersion}).
-			Login("ghcr.io", githubActor, githubToken).
-			Push(chart, "oci://ghcr.io/openmeterio/helm-charts").
-			Sync(ctx)
-		if err != nil {
-			return err
-		}
-
-		return nil
+		return m.pushHelmChart(ctx, "openmeter", version, githubActor, githubToken)
 	})
 
 	group.Go(func() error {
-		chart := m.Build().HelmChart("benthos-collector", version)
-
-		_, err := dag.Helm(HelmOpts{Version: helmVersion}).
-			Login("ghcr.io", githubActor, githubToken).
-			Push(chart, "oci://ghcr.io/openmeterio/helm-charts").
-			Sync(ctx)
-		if err != nil {
-			return err
-		}
-
-		return nil
+		return m.pushHelmChart(ctx, "benthos-collector", version, githubActor, githubToken)
 	})
 
 	return group.Wait()
+}
+
+func (m *Ci) pushHelmChart(ctx context.Context, name string, version string, githubActor string, githubToken *Secret) error {
+	chart := m.Build().HelmChart(name, version)
+
+	_, err := dag.Helm(HelmOpts{Version: helmVersion}).
+		Login("ghcr.io", githubActor, githubToken).
+		Push(chart, "oci://ghcr.io/openmeterio/helm-charts").
+		Sync(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
