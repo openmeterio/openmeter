63532a5568e6510ef8cdea4400b6ac05272bf9a2
Peter Marton <peter@tailfin.cloud>
2024-03-05 13:23:44 -0800

    fix(meter): validate group by key (#665)

--- pkg/models/meter.go
+++ pkg/models/meter.go
@@ -4,11 +4,14 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"regexp"
 	"strings"
 	"time"
 
 	"github.com/oklog/ulid/v2"
 )
+
+var groupByKeyRegExp = regexp.MustCompile(`^[a-zA-Z_][0-9a-zA-Z_]*$`)
 
 type MeterAggregation string
 
@@ -228,6 +231,9 @@ func (m *Meter) Validate() error {
 		}
 		if strings.TrimSpace(key) == "" {
 			return fmt.Errorf("meter group by key cannot be empty")
+		}
+		if !groupByKeyRegExp.MatchString(key) {
+			return fmt.Errorf("meter group by key %s is invalid, only alphanumeric and underscore characters are allowed", key)
 		}
 		// keys must be unique
 		seen := make(map[string]struct{}, len(m.GroupBy))
