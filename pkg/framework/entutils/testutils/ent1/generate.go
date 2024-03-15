5a1572c187955ba1ce4dd0413b461e24fa16d794
Bence Csati <bcsati@cisco.com>
2024-03-15 11:21:28 +0100

    feat: add license headers
    Signed-off-by: Bence Csati <bcsati@cisco.com>
    
    feat: add more license headers
    
    Signed-off-by: Bence Csati <bcsati@cisco.com>

--- pkg/framework/entutils/testutils/ent1/generate.go
+++ pkg/framework/entutils/testutils/ent1/generate.go
@@ -1,3 +1,17 @@
+// Copyright © 2024 Tailfin Cloud Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package ent1
 
 //go:generate go run -mod=mod entgo.io/ent/cmd/ent generate --target ./db  --feature sql/upsert --feature sql/lock --feature sql/versioned-migration ./schema --template ../../expose.tpl --template ../../paginate.tpl
