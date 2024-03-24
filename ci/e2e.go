a59555222a77d07ca27cc7f0e7fd7f14900a09e7
galexi <spam@goth.wiki>
2024-03-24 22:26:28 +0100

    feat(ci): add dagger function for perf testing

--- ci/e2e.go
+++ ci/e2e.go
@@ -2,7 +2,51 @@ package main
 
 import (
 	"fmt"
+	"path"
+	"time"
 )
+
+const (
+	perfTestDefaultConfig = "dagger.config.yaml"
+)
+
+func (m *Ci) Perf(configName Optional[string]) *Container {
+	localStack := NewLocalStack(m.Source, m.Source.File(path.Join("perf", "configs", configName.GetOr(perfTestDefaultConfig))))
+
+	// build k6 tests with node20 and pnpm
+	testBuilder := dag.Container().
+		From("node:20-alpine").
+		WithMountedDirectory("/mnt/src", m.Source).
+		WithWorkdir("/mnt/src/perf/k6").
+		WithExec([]string{"pnpm", "install"}).
+		WithExec([]string{"pnpm", "build"})
+
+	k6Container := dag.Container().
+		From("grafana/k6:latest").
+		WithDirectory("/tests/dist", testBuilder.Directory("/mnt/src/perf/k6/dist")).
+		WithFile("/tests/run-all.sh", m.Source.File("perf/k6/run-all.sh")).
+		WithWorkdir("/tests").
+		WithServiceBinding("api", localStack.Api).
+		WithServiceBinding("clickhouse", localStack.Clickhouse).
+		WithEnvVariable("CLICKHOUSE_BASE_URL", "http://clickhouse:8123").
+		WithEnvVariable("OPENMETER_BASE_URL", "http://api:8888").
+		WithEnvVariable("OPENMETER_TELEMETRY_URL", "http://api:10000").
+		WithEntrypoint([]string{"/bin/ash", "-c"})
+
+	// seeding the system
+	dag.Container().
+		From("jeffail/benthos:latest").
+		WithMountedDirectory("/mnt/src", m.Source).
+		WithServiceBinding("api", localStack.Api).
+		WithEnvVariable("OPENMETER_BASE_URL", "http://api:8888").
+		WithEnvVariable("SEEDER_COUNT", "100").
+		WithExec([]string{"-c", "/mnt/src/perf/configs/seed.benthos.yaml"})
+
+	// TODO: add querying clickhouse to get the number of records instead
+	time.Sleep(10 * time.Second)
+
+	return k6Container.WithExec([]string{"./run-all.sh"})
+}
 
 func (m *Ci) Etoe(test Optional[string]) *Container {
 	localStack := NewLocalStack(m.Source, m.Source.File("e2e/config.yaml"))
