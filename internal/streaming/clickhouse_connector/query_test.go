3919b40a56ecccb4de8b6bc8098cdba7f9338b2c
Zoltan Tudlik <zoltan.tudlik@gmail.com>
2024-03-28 08:55:54 +0100

    chore(query): remove duplicated test case from TestCreateMeterView

--- internal/streaming/clickhouse_connector/query_test.go
+++ internal/streaming/clickhouse_connector/query_test.go
@@ -108,19 +108,6 @@ func TestCreateMeterView(t *testing.T) {
 			wantSQL:  "CREATE MATERIALIZED VIEW IF NOT EXISTS openmeter.om_my_namespace_meter1 (subject String, windowstart DateTime, windowend DateTime, value AggregateFunction(count, Float64)) ENGINE = AggregatingMergeTree() ORDER BY (windowstart, windowend, subject) AS SELECT subject, tumbleStart(time, toIntervalMinute(1)) AS windowstart, tumbleEnd(time, toIntervalMinute(1)) AS windowend, countState(*) AS value FROM openmeter.om_events WHERE openmeter.om_events.namespace = 'my_namespace' AND empty(openmeter.om_events.validation_error) = 1 AND openmeter.om_events.type = 'myevent' GROUP BY windowstart, windowend, subject",
 			wantArgs: nil,
 		},
-		{
-			query: createMeterView{
-				Database:      "openmeter",
-				Namespace:     "my_namespace",
-				MeterSlug:     "meter1",
-				Aggregation:   models.MeterAggregationCount,
-				EventType:     "myevent",
-				ValueProperty: "",
-				GroupBy:       map[string]string{},
-			},
-			wantSQL:  "CREATE MATERIALIZED VIEW IF NOT EXISTS openmeter.om_my_namespace_meter1 (subject String, windowstart DateTime, windowend DateTime, value AggregateFunction(count, Float64)) ENGINE = AggregatingMergeTree() ORDER BY (windowstart, windowend, subject) AS SELECT subject, tumbleStart(time, toIntervalMinute(1)) AS windowstart, tumbleEnd(time, toIntervalMinute(1)) AS windowend, countState(*) AS value FROM openmeter.om_events WHERE openmeter.om_events.namespace = 'my_namespace' AND empty(openmeter.om_events.validation_error) = 1 AND openmeter.om_events.type = 'myevent' GROUP BY windowstart, windowend, subject",
-			wantArgs: nil,
-		},
 	}
 
 	for _, tt := range tests {
