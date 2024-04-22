dbd52a381e583cfda7df7d86eb540e537e64cad9
Peter Marton <peter@openmeter.io>
2024-04-21 18:44:50 -0700

    test(credit): linter and flaky times

--- internal/credit/postgres_connector/grant.go
+++ internal/credit/postgres_connector/grant.go
@@ -399,12 +399,3 @@ func mapResetEntity(entry *db.CreditEntry) (credit_model.Reset, error) {
 
 	return reset, nil
 }
-
-// rollback calls to tx.Rollback and wraps the given error
-// with the rollback error if occurred.
-func rollback(tx *db.Tx, err error) error {
-	if rerr := tx.Rollback(); rerr != nil {
-		err = fmt.Errorf("%w: %v", err, rerr)
-	}
-	return err
-}
