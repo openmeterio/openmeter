0883f296e296e7cf1cbf94316940cc6ce4588db8
Peter Marton <peter@tailfin.cloud>
2024-05-14 19:05:45 +0200

    feat(openmeter): export new kafka collector (#902)

--- openmeter/ingest/kafkaingest/collector.go
+++ openmeter/ingest/kafkaingest/collector.go
@@ -5,8 +5,10 @@ import (
 	"log/slog"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"go.opentelemetry.io/otel/metric"
 
 	"github.com/openmeterio/openmeter/internal/ingest/kafkaingest"
+	"github.com/openmeterio/openmeter/openmeter/ingest/kafkaingest/serializer"
 )
 
 // Collector is a receiver of events that handles sending those events to a downstream Kafka broker.
@@ -14,4 +16,13 @@ type Collector = kafkaingest.Collector
 
 func KafkaProducerGroup(ctx context.Context, producer *kafka.Producer, logger *slog.Logger) (execute func() error, interrupt func(error)) {
 	return kafkaingest.KafkaProducerGroup(ctx, producer, logger)
+}
+
+func NewCollector(
+	producer *kafka.Producer,
+	serializer serializer.Serializer,
+	namespacedTopicTemplate string,
+	metricMeter metric.Meter,
+) (*Collector, error) {
+	return kafkaingest.NewCollector(producer, serializer, namespacedTopicTemplate, metricMeter)
 }
