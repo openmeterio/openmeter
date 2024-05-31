a09b3feb56ad91cee28c6085dfed93f82257deef
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-05-31 16:34:48 +0200

    ci: update dagger

--- ci/main.go
+++ ci/main.go
@@ -8,11 +8,11 @@ import (
 
 const (
 	// Alpine is required for our current build (due to Kafka and CGO), but it doesn't seem to work well with golangci-lint
-	goVersion      = "1.22.2"
-	goBuildVersion = goVersion + "-alpine3.19@sha256:cdc86d9f363e8786845bea2040312b4efa321b828acdeb26f393faa864d887b0"
+	goVersion      = "1.22.3"
+	goBuildVersion = goVersion + "-alpine3.19@sha256:f1fe698725f6ed14eb944dc587591f134632ed47fc0732ec27c7642adbe90618"
 	xxBaseImage    = "tonistiigi/xx:1.4.0@sha256:0cd3f05c72d6c9b038eb135f91376ee1169ef3a330d34e418e65e2a5c2e9c0d4"
 
-	golangciLintVersion = "v1.58.2"
+	golangciLintVersion = "v1.59.0"
 	spectralVersion     = "6.11"
 	kafkaVersion        = "3.6"
 	clickhouseVersion   = "23.3.9.55"
@@ -20,7 +20,7 @@ const (
 	postgresVersion     = "15.3"
 
 	helmDocsVersion = "v1.13.1"
-	helmVersion     = "3.14.4"
+	helmVersion     = "3.15.1"
 
 	alpineBaseImage = "alpine:3.20.0@sha256:77726ef6b57ddf65bb551896826ec38bc3e53f75cdde31354fbffb4f25238ebd"
 )
