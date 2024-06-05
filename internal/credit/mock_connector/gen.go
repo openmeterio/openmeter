5a5ccd1da1383af557945d48df78d576be24a213
Alex Goth <alex.goth@openmeter.io>
2024-06-05 11:08:33 +0200

    fix: fix mockgen script

--- internal/credit/mock_connector/gen.go
+++ internal/credit/mock_connector/gen.go
@@ -1,2 +1,2 @@
-//go:generate github.com/golang/mock/mockgen -destination=./mock.gen.go -package=credit_mock github.com/openmeterio/openmeter/internal/credit Connector
+//go:generate mockgen -destination=./mock.gen.go -package=credit_mock github.com/openmeterio/openmeter/internal/credit Connector
 package credit_mock
