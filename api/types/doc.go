3d23277111b9664bdf13484f59fc54c9380bbf61
Bence Csati <bcsati@cisco.com>
2024-07-01 10:57:13 +0200

    feat: add more license headers
    Signed-off-by: Bence Csati <bcsati@cisco.com>

--- api/types/doc.go
+++ api/types/doc.go
@@ -1,3 +1,17 @@
+// Copyright © 2024 Tailfin Cloud Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Some parts of the OpenAPI spec are not supported by oapi-codegen, such as the "oneOf" and "anyOf" keywords.
 // In these cases, you can define the types manually in the types package and reference them via `x-go-type` so the go client works properly.
 package types
