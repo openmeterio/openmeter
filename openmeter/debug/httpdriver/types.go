78bd2472922188cc51caba7faf229272204786d5
Peter Marton <peter@openmeter.io>
2024-07-04 07:52:34 -0700

    feat(debug): exports

---
+++ openmeter/debug/httpdriver/types.go
@@ -0,0 +1,12 @@
+package httpdriver
+
+import "github.com/openmeterio/openmeter/internal/debug/httpdriver"
+
+// requests
+type GetMetricsHandlerRequest = httpdriver.GetMetricsHandlerRequest
+
+// responses
+type GetMetricsHandlerResponse = httpdriver.GetMetricsHandlerResponse
+
+// params
+type GetMetricsHandlerParams = httpdriver.GetMetricsHandlerParams
