055f52f0d35c107dd9647bf06b038160e17352a6
Alex Goth <alex.goth@openmeter.io>
2024-07-23 13:50:24 +0200

    feat: combine ent schema definitions in central place

---
+++ internal/ent/schema/feature.go
@@ -0,0 +1,38 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+
+	"github.com/openmeterio/openmeter/pkg/framework/entutils"
+)
+
+type Feature struct {
+	ent.Schema
+}
+
+func (Feature) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		entutils.IDMixin{},
+		entutils.TimeMixin{},
+		entutils.MetadataAnnotationsMixin{},
+	}
+}
+
+func (Feature) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("namespace").NotEmpty().Immutable(),
+		field.String("name").NotEmpty(),
+		field.String("key").NotEmpty().Immutable(),
+		field.String("meter_slug").Optional().Nillable().Immutable(),
+		field.JSON("meter_group_by_filters", map[string]string{}).Optional(),
+		field.Time("archived_at").Optional().Nillable(),
+	}
+}
+
+func (Feature) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("namespace", "id"),
+	}
+}
