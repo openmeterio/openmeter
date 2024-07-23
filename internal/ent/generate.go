82c5c6a12c2bc62ff5da9cfebd6e5270bb318170
Alex Goth <alex.goth@openmeter.io>
2024-07-23 13:50:24 +0200

    feat: combine ent schema definitions in central place

--- internal/ent/generate.go
+++ internal/ent/generate.go
@@ -1,3 +1,3 @@
-package entdb
+package ent
 
 //go:generate go run -mod=mod entgo.io/ent/cmd/ent generate --target ./db  --feature sql/upsert --feature sql/lock --feature sql/versioned-migration --template ../../pkg/framework/entutils/expose.tpl --template ../../pkg/framework/entutils/paginate.tpl ./schema
