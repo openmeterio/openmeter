cd512735f8e348fcbadf6a30555c5df7f5f28b05
Peter Turi <peter.turi@openmeter.io>
2024-07-30 12:59:35 +0200

    feat: initialize entitlements in balance worker
    Given that this would copy paste most of the code from server's main
    this patch moves some client initializaiton code into internal for
    better reusability.

---
+++ internal/cmd/README.md
@@ -0,0 +1,4 @@
+# Command initialization helpers
+
+These packages can be used to quickly create clients from configuration objects, so
+that we can prevent code duplication in the mains.
---
+++ internal/common/entitlement/connectors.go
@@ -0,0 +1,96 @@
+package entitlement
+
+import (
+	"log/slog"
+	"time"
+
+	"github.com/openmeterio/openmeter/internal/ent/db"
+	"github.com/openmeterio/openmeter/internal/meter"
+	"github.com/openmeterio/openmeter/openmeter/credit"
+	creditpgadapter "github.com/openmeterio/openmeter/openmeter/credit/postgresadapter"
+	"github.com/openmeterio/openmeter/openmeter/entitlement"
+	booleanentitlement "github.com/openmeterio/openmeter/openmeter/entitlement/boolean"
+	meteredentitlement "github.com/openmeterio/openmeter/openmeter/entitlement/metered"
+	entitlementpgadapter "github.com/openmeterio/openmeter/openmeter/entitlement/postgresadapter"
+	staticentitlement "github.com/openmeterio/openmeter/openmeter/entitlement/static"
+	"github.com/openmeterio/openmeter/openmeter/event/publisher"
+	"github.com/openmeterio/openmeter/openmeter/productcatalog"
+	productcatalogpgadapter "github.com/openmeterio/openmeter/openmeter/productcatalog/postgresadapter"
+	"github.com/openmeterio/openmeter/openmeter/streaming"
+)
+
+type Connectors struct {
+	Feature            productcatalog.FeatureConnector
+	EntitlementOwner   credit.OwnerConnector
+	CreditBalance      credit.BalanceConnector
+	Grant              credit.GrantConnector
+	MeteredEntitlement meteredentitlement.Connector
+	Entitlement        entitlement.EntitlementConnector
+}
+
+type ConnectorsOptions struct {
+	DatabaseClient     *db.Client
+	StreamingConnector streaming.Connector
+	Logger             *slog.Logger
+	MeterRepository    meter.Repository
+	Publisher          publisher.TopicPublisher
+}
+
+func GetConnectors(opts ConnectorsOptions) *Connectors {
+	// Initialize database adapters
+	featureDBAdapter := productcatalogpgadapter.NewPostgresFeatureDBAdapter(opts.DatabaseClient, opts.Logger)
+	entitlementDBAdapter := entitlementpgadapter.NewPostgresEntitlementDBAdapter(opts.DatabaseClient)
+	usageResetDBAdapter := entitlementpgadapter.NewPostgresUsageResetDBAdapter(opts.DatabaseClient)
+	grantDBAdapter := creditpgadapter.NewPostgresGrantDBAdapter(opts.DatabaseClient)
+	balanceSnashotDBAdapter := creditpgadapter.NewPostgresBalanceSnapshotDBAdapter(opts.DatabaseClient)
+
+	// Initialize connectors
+	featureConnector := productcatalog.NewFeatureConnector(featureDBAdapter, opts.MeterRepository)
+	entitlementOwnerConnector := meteredentitlement.NewEntitlementGrantOwnerAdapter(
+		featureDBAdapter,
+		entitlementDBAdapter,
+		usageResetDBAdapter,
+		opts.MeterRepository,
+		opts.Logger,
+	)
+	creditBalanceConnector := credit.NewBalanceConnector(
+		grantDBAdapter,
+		balanceSnashotDBAdapter,
+		entitlementOwnerConnector,
+		opts.StreamingConnector,
+		opts.Logger,
+	)
+	grantConnector := credit.NewGrantConnector(
+		entitlementOwnerConnector,
+		grantDBAdapter,
+		balanceSnashotDBAdapter,
+		time.Minute,
+		opts.Publisher,
+	)
+	meteredEntitlementConnector := meteredentitlement.NewMeteredEntitlementConnector(
+		opts.StreamingConnector,
+		entitlementOwnerConnector,
+		creditBalanceConnector,
+		grantConnector,
+		entitlementDBAdapter,
+		opts.Publisher,
+	)
+	entitlementConnector := entitlement.NewEntitlementConnector(
+		entitlementDBAdapter,
+		featureConnector,
+		opts.MeterRepository,
+		meteredEntitlementConnector,
+		staticentitlement.NewStaticEntitlementConnector(),
+		booleanentitlement.NewBooleanEntitlementConnector(),
+		opts.Publisher,
+	)
+
+	return &Connectors{
+		Feature:            featureConnector,
+		EntitlementOwner:   entitlementOwnerConnector,
+		CreditBalance:      creditBalanceConnector,
+		Grant:              grantConnector,
+		MeteredEntitlement: meteredEntitlementConnector,
+		Entitlement:        entitlementConnector,
+	}
+}
