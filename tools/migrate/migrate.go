3838aaa965880d0dd4184c4b136761986c7cd23b
Alex Goth <alex.goth@openmeter.io>
2024-08-09 14:35:00 +0200

    refactor: expose migrate interface

--- tools/migrate/migrate.go
+++ tools/migrate/migrate.go
@@ -1,26 +1,36 @@
-// A lightweigh migration tool to replace `ent.Schema.Create` calls.
+// A very lightweigh migration tool to replace `ent.Schema.Create` calls.
 package migrate
 
 import (
 	"embed"
+	"io/fs"
 
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	"github.com/golang-migrate/migrate/v4/source/iofs"
 )
 
-//go:embed migrations
-var fs embed.FS
+type Migrate = migrate.Migrate
 
-func Up(conn string) error {
+//go:embed migrations
+var omMigrations embed.FS
+
+// NewMigrate creates a new migrate instance.
+// fs is expected to contain a migrations directory with the migration files.
+func NewMigrate(conn string, fs fs.FS) (*Migrate, error) {
 	d, err := iofs.New(fs, "migrations")
 	if err != nil {
-		return err
+		return nil, err
 	}
-	m, err := migrate.NewWithSourceInstance("iofs", d, conn)
+	return migrate.NewWithSourceInstance("iofs", d, conn)
+}
+
+func Up(conn string) error {
+	m, err := NewMigrate(conn, omMigrations)
 	if err != nil {
 		return err
 	}
+
 	defer m.Close()
 	err = m.Up()
 	if err != nil {
