6c5b14ecd596096356d8878a2960e309b53dff1d
Peter Turi <peter.turi@openmeter.io>
2024-08-14 13:26:10 +0200

    chore: expose events for cloud usage

---
+++ openmeter/event/models/models.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"github.com/openmeterio/openmeter/internal/event/models"
+)
+
+type (
+	FeatureKeyAndID = models.FeatureKeyAndID
+	NamespaceID     = models.NamespaceID
+
+	SubjectKeyAndID = models.SubjectKeyAndID
+	Subject         = models.Subject
+)
