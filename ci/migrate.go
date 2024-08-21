028441e523295fd1e96ff4e289a377f76f3ae21c
Alex Goth <alex.goth@openmeter.io>
2024-08-21 16:08:09 +0200

    refactor: use purpose built images and get rid of nix in dagger

--- ci/migrate.go
+++ ci/migrate.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/openmeterio/openmeter/ci/internal/dagger"
 	"github.com/sourcegraph/conc/pool"
@@ -20,38 +21,78 @@ type Migrate struct {
 func (m *Migrate) Check(
 	ctx context.Context,
 ) error {
-	nix := nix(m.Source)
+	app := goModuleCross("").
+		WithSource(m.Source).
+		Container().
+		WithEnvVariable("GOFLAGS", "-tags=musl")
+
+	bin := dag.Container(dagger.ContainerOpts{
+		Platform: "linux/amd64",
+	}).From("arigaio/atlas:0.25.0").File("atlas")
+
+	atlas := app.
+		WithFile("/bin/atlas", bin).
+		WithDirectory("internal/ent", m.Source.Directory("internal/ent")).
+		WithDirectory("tools/migrate/migrations", m.Source.Directory("tools/migrate/migrations")).
+		WithFile("atlas.hcl", m.Source.File("atlas.hcl"))
+
 	p := pool.New().WithErrors().WithContext(ctx)
 
 	// Always validate schema is generated
-	p.Go(syncFunc(nix.
-		WithExec([]string{"sh", "-c", "nix develop --impure .#dagger -c go generate ./internal/ent/..."}).
-		WithExec([]string{"sh", "-c", "nix develop --impure .#dagger -c sh -c 'if [[ -n $(git status --porcelain ./internal/ent) ]]; then echo \"Ent schema wasnt generated\"; exit 1; fi' "}),
-	))
+	p.Go(func(ctx context.Context) error {
+		result := app.
+			WithExec([]string{"go", "generate", "-x", "-tags=musl", "-ldflags", "linkmode=external", "./internal/ent/..."}).
+			Directory("internal/ent")
+
+		source := m.Source.Directory("internal/ent")
+
+		err := diff(ctx, source, result)
+		if err != nil {
+			return fmt.Errorf("schema is not in sync with generated code")
+		}
+		return nil
+	})
+
 	// Always validate migrations are in sync with schema
-	p.Go(syncFunc(nix.
-		WithServiceBinding("postgres", postgresNamed("no-diff")).
-		WithExec([]string{"sh", "-c", "nix develop --impure .#dagger -c atlas migrate --env ci diff test"}).
-		WithExec([]string{"sh", "-c", "nix develop --impure .#dagger -c sh -c 'if [[ -n $(git status --porcelain ./tools/migrate/migrations) ]]; then echo \"Migrations directory is dirty\"; exit 1; fi' "}),
-	))
+	p.Go(func(ctx context.Context) error {
+		result := atlas.
+			WithServiceBinding("postgres", postgresNamed("no-diff")).
+			WithExec([]string{"atlas", "migrate", "--env", "ci", "diff", "test"}).
+			Directory("tools/migrate/migrations")
+
+		source := m.Source.Directory("tools/migrate/migrations")
+		err := diff(ctx, source, result)
+		if err != nil {
+			return fmt.Errorf("migrations are not in sync with schema")
+		}
+
+		return nil
+	})
+
 	// Always lint last 10 migrations
-	p.Go(syncFunc(nix.
-		WithServiceBinding("postgres", postgresNamed("last-10")).
-		WithExec([]string{"sh", "-c", "nix develop --impure .#dagger -c atlas migrate --env ci lint --latest 10"}),
+	p.Go(syncFunc(
+		atlas.
+			WithServiceBinding("postgres", postgresNamed("last-10")).
+			WithExec([]string{"atlas", "migrate", "--env", "ci", "lint", "--latest", "10"}),
 	))
+
 	// Validate checksum is intact
-	p.Go(syncFunc(nix.
-		WithServiceBinding("postgres", postgresNamed("validate")).
-		WithExec([]string{"sh", "-c", "nix develop --impure .#dagger -c atlas migrate --env ci validate"}),
+	p.Go(syncFunc(
+		atlas.
+			WithServiceBinding("postgres", postgresNamed("validate")).
+			WithExec([]string{"atlas", "migrate", "--env", "ci", "validate"}),
 	))
 
 	return p.Wait()
 }
 
-func nix(src *dagger.Directory) *dagger.Container {
-	return dag.Nix().SetupNix(dagger.NixSetupNixOpts{
-		Src: src,
-	}).
-		// Note: we have to use `sh -c` as otherwise devenv nix assertion fails: https://github.com/cachix/devenv/blob/b285601679c7686f623791ad93a8e0debc322633/src/modules/top-level.nix#L229
-		WithExec([]string{"sh", "-c", "nix develop --impure .#dagger"}) // Prepare environment
+func diff(ctx context.Context, d1, d2 *dagger.Directory) error {
+	_, err := dag.Container(dagger.ContainerOpts{Platform: ""}).
+		From(alpineBaseImage).
+		WithExec([]string{"apk", "add", "--update", "--no-cache", "ca-certificates", "tzdata", "bash"}).
+		WithDirectory("src", d1).
+		WithDirectory("res", d2).
+		WithExec([]string{"diff", "-u", "-r", "src", "res"}).
+		Sync(ctx)
+	return err
 }
