46e603d445121bd97f4611a75c68128752d26a84
Alex Goth <alex.goth@openmeter.io>
2024-08-28 11:13:13 +0200

    chore: use golangci-lint for vscode

--- openmeter/registry/startup/db.go
+++ openmeter/registry/startup/db.go
@@ -13,17 +13,17 @@ func DB(ctx context.Context, cfg config.PostgresConfig, db *db.Client) error {
 	if !cfg.AutoMigrate.Enabled() {
 		return nil
 	}
-	
+
 	switch cfg.AutoMigrate {
-		case config.AutoMigrateEnt:
-			if err := db.Schema.Create(ctx); err != nil {
-				return fmt.Errorf("failed to migrate db: %w", err)
-			}
-		case config.AutoMigrateMigration:
-			if err := migrate.Up(cfg.URL); err != nil {
-				return fmt.Errorf("failed to migrate db: %w", err)
-			}
+	case config.AutoMigrateEnt:
+		if err := db.Schema.Create(ctx); err != nil {
+			return fmt.Errorf("failed to migrate db: %w", err)
+		}
+	case config.AutoMigrateMigration:
+		if err := migrate.Up(cfg.URL); err != nil {
+			return fmt.Errorf("failed to migrate db: %w", err)
+		}
 	}
-		
+
 	return nil
 }
