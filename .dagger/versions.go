e9bdfb15c15b9b83921c5621fb9ef2b8f0c98afe
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-09-23 14:11:28 +0200

    chore: update build dependencies
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- .dagger/versions.go
+++ .dagger/versions.go
@@ -1,8 +1,8 @@
 package main
 
 const (
-	golangciLintVersion = "v1.60.2"
-	helmVersion         = "3.15.4"
+	golangciLintVersion = "v1.61.0"
+	helmVersion         = "3.16.1"
 	helmDocsVersion     = "v1.14.2"
-	spectralVersion     = "6.11.1"
+	spectralVersion     = "6.13.1"
 )
--- .dagger/versions_pinned.go
+++ .dagger/versions_pinned.go
@@ -2,7 +2,7 @@ package main
 
 const (
 	// Alpine is required for our current build (due to Kafka and CGO), but it doesn't seem to work well with golangci-lint
-	goVersion = "1.23.0"
+	goVersion = "1.23.1"
 
 	kafkaVersion      = "3.6"
 	clickhouseVersion = "24.5.5.78"
@@ -13,10 +13,10 @@ const (
 	// TODO: add update mechanism for versions below
 
 	// Alpine is required for our current build (due to Kafka and CGO), but it doesn't seem to work well with golangci-lint
-	goBuildVersion = goVersion + "-alpine3.20@sha256:d0b31558e6b3e4cc59f6011d79905835108c919143ebecc58f35965bf79948f4"
+	goBuildVersion = goVersion + "-alpine3.20@sha256:ac67716dd016429be8d4c2c53a248d7bcdf06d34127d3dc451bda6aa5a87bc06"
 	xxBaseImage    = "tonistiigi/xx:1.5.0@sha256:0c6a569797744e45955f39d4f7538ac344bfb7ebf0a54006a0a4297b153ccf0f"
 
-	alpineBaseImage = "alpine:3.20.2@sha256:0a4eaa0eecf5f8c050e5bba433f58c052be7587ee8af3e8b3910ef9ab5fbe9f5"
+	alpineBaseImage = "alpine:3.20.3@sha256:beefdbd8a1da6d2915566fde36db9db0b524eb737fc57cd1367effd16dc0d06d"
 
 	atlasImage = "arigaio/atlas:0.26.1@sha256:f77152f5458255410d2e59ad80a0fc661524067a90d297a0b62e6bda23437893"
 )
