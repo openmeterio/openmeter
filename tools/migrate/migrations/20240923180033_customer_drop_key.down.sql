6fd9bd1f6fe09b89c9e39c9b0aa4954859d52dda
Alex Goth <alex.goth@openmeter.io>
2024-09-24 10:43:16 +0200

    fix: migration contents

--- tools/migrate/migrations/20240923180033_customer_drop_key.down.sql
+++ tools/migrate/migrations/20240923180033_customer_drop_key.down.sql
@@ -1,10 +1,24 @@
 -- reverse: create index "customersubjects_namespace_subject_key" to table: "customer_subjects"
 DROP INDEX "customersubjects_namespace_subject_key";
+
 -- reverse: create index "customersubjects_namespace" to table: "customer_subjects"
 DROP INDEX "customersubjects_namespace";
+
 -- reverse: modify "customer_subjects" table
-ALTER TABLE "customer_subjects" DROP CONSTRAINT "customer_subjects_customers_subjects", DROP COLUMN "namespace", ADD
- CONSTRAINT "customer_subjects_customers_subjects" FOREIGN KEY ("customer_id") REFERENCES "customers" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE
+    "customer_subjects" DROP CONSTRAINT "customer_subjects_customers_subjects",
+    DROP COLUMN "namespace",
+ADD
+    CONSTRAINT "customer_subjects_customers_subjects" FOREIGN KEY ("customer_id") REFERENCES "customers" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION;
+
 -- reverse: modify "customers" table
+--
+--
+-- manual interventions as we're breaking lint rules
 -- atlas:nolint MF103
-ALTER TABLE "customers" ADD COLUMN "key" character varying NOT NULL;
+ALTER TABLE
+    "customers"
+ADD
+    COLUMN "key" character varying NOT NULL;
+
+CREATE UNIQUE INDEX "customer_namespace_key_deleted_at" ON "customers" ("namespace", "key", "deleted_at");
--- tools/migrate/migrations/20240923180033_customer_drop_key.up.sql
+++ tools/migrate/migrations/20240923180033_customer_drop_key.up.sql
@@ -1,13 +1,24 @@
 -- modify "customers" table
 -- atlas:nolint DS103
-ALTER TABLE "customers" DROP COLUMN "key";
+ALTER TABLE
+    "customers" DROP COLUMN "key";
+
 -- modify "customer_subjects" table
 -- atlas:nolint MF103
-ALTER TABLE "customer_subjects" DROP CONSTRAINT "customer_subjects_customers_subjects", ADD COLUMN "namespace" character varying NOT NULL, ADD
- CONSTRAINT "customer_subjects_customers_subjects" FOREIGN KEY ("customer_id") REFERENCES "customers" ("id") ON UPDATE NO ACTION ON DELETE CASCADE;
+ALTER TABLE
+    "customer_subjects" DROP CONSTRAINT "customer_subjects_customers_subjects",
+ADD
+    COLUMN "namespace" character varying NOT NULL,
+ADD
+    CONSTRAINT "customer_subjects_customers_subjects" FOREIGN KEY ("customer_id") REFERENCES "customers" ("id") ON UPDATE NO ACTION ON DELETE CASCADE;
+
 -- create index "customersubjects_namespace" to table: "customer_subjects"
 -- atlas:nolint MF103
 CREATE INDEX "customersubjects_namespace" ON "customer_subjects" ("namespace");
+
 -- create index "customersubjects_namespace_subject_key" to table: "customer_subjects"
+--
+--
+-- manual interventions as we're breaking lint rules
 -- atlas:nolint MF101
 CREATE UNIQUE INDEX "customersubjects_namespace_subject_key" ON "customer_subjects" ("namespace", "subject_key");
