c2dd2868fb0c8c5cc68206a33409341250631463
Peter Marton <peter@openmeter.io>
2024-10-07 10:55:52 -0700

    feat(app): skip non om initiated payment intents

--- openmeter/appstripe/httpdriver/handler.go
+++ openmeter/appstripe/httpdriver/handler.go
@@ -1,13 +1,8 @@
 package httpdriver
 
 import (
-	"context"
-	"errors"
-	"net/http"
-
 	"github.com/openmeterio/openmeter/openmeter/appstripe"
 	"github.com/openmeterio/openmeter/openmeter/namespace/namespacedriver"
-	"github.com/openmeterio/openmeter/pkg/framework/commonhttp"
 	"github.com/openmeterio/openmeter/pkg/framework/transport/httptransport"
 )
 
@@ -25,15 +20,6 @@ type handler struct {
 	service          appstripe.Service
 	namespaceDecoder namespacedriver.NamespaceDecoder
 	options          []httptransport.HandlerOption
-}
-
-func (h *handler) resolveNamespace(ctx context.Context) (string, error) {
-	ns, ok := h.namespaceDecoder.GetNamespace(ctx)
-	if !ok {
-		return "", commonhttp.NewHTTPError(http.StatusInternalServerError, errors.New("internal server error"))
-	}
-
-	return ns, nil
 }
 
 func New(
