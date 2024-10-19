b4a2fb44531395d1cd541f676634b68d47442dbd
Peter Marton <peter@openmeter.io>
2024-10-19 09:43:39 -0700

    feat(sink): meter event

--- openmeter/sink/storage.go
+++ openmeter/sink/storage.go
@@ -3,11 +3,9 @@ package sink
 import (
 	"context"
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
-	"github.com/huandu/go-sqlbuilder"
 
 	sinkmodels "github.com/openmeterio/openmeter/openmeter/sink/models"
 	"github.com/openmeterio/openmeter/openmeter/streaming/clickhouse_connector"
@@ -51,67 +49,18 @@ type ClickHouseStorage struct {
 	config ClickHouseStorageConfig
 }
 
+// BatchInsert inserts multiple messages into ClickHouse.
 func (c *ClickHouseStorage) BatchInsert(ctx context.Context, messages []sinkmodels.SinkMessage) error {
-	query := InsertEventsQuery{
-		Clock:         realClock{},
-		Database:      c.config.Database,
-		Messages:      messages,
-		QuerySettings: c.config.QuerySettings,
-	}
-	sql, args, err := query.ToSQL()
-	if err != nil {
-		return err
-	}
+	var rawEvents []clickhouse_connector.CHEvent
+	var meterEvents []clickhouse_connector.CHMeterEvent
 
-	// By default, ClickHouse is writing data synchronously.
-	// See https://clickhouse.com/docs/en/cloud/bestpractices/asynchronous-inserts
-	if c.config.AsyncInsert {
-		// With the `wait_for_async_insert` setting, you can configure
-		// if you want an insert statement to return with an acknowledgment
-		// either immediately after the data got inserted into the buffer.
-		err = c.config.ClickHouse.AsyncInsert(ctx, sql, c.config.AsyncInsertWait, args...)
-	} else {
-		err = c.config.ClickHouse.Exec(ctx, sql, args...)
-	}
-
-	if err != nil {
-		return fmt.Errorf("failed to batch insert events: %w", err)
-	}
-
-	return nil
-}
-
-type InsertEventsQuery struct {
-	Clock         Clock
-	Database      string
-	Messages      []sinkmodels.SinkMessage
-	QuerySettings map[string]string
-}
-
-func (q InsertEventsQuery) ToSQL() (string, []interface{}, error) {
-	tableName := clickhouse_connector.GetEventsTableName(q.Database)
-
-	query := sqlbuilder.ClickHouse.NewInsertBuilder()
-	query.InsertInto(tableName)
-	query.Cols("namespace", "validation_error", "id", "type", "source", "subject", "time", "data", "ingested_at", "stored_at")
-
-	// Add settings
-	var settings []string
-	for key, value := range q.QuerySettings {
-		settings = append(settings, fmt.Sprintf("%s = %s", key, value))
-	}
-
-	if len(settings) > 0 {
-		query.SQL(fmt.Sprintf("SETTINGS %s", strings.Join(settings, ", ")))
-	}
-
-	for _, message := range q.Messages {
+	for _, message := range messages {
 		var eventErr string
 		if message.Status.Error != nil {
 			eventErr = message.Status.Error.Error()
 		}
 
-		storedAt := q.Clock.Now()
+		storedAt := time.Now()
 		ingestedAt := storedAt
 
 		if message.KafkaMessage != nil {
@@ -128,33 +77,85 @@ func (q InsertEventsQuery) ToSQL() (string, []interface{}, error) {
 			}
 		}
 
-		query.Values(
-			message.Namespace,
-			eventErr,
-			message.Serialized.Id,
-			message.Serialized.Type,
-			message.Serialized.Source,
-			message.Serialized.Subject,
-			message.Serialized.Time,
-			message.Serialized.Data,
-			ingestedAt,
-			storedAt,
-		)
+		rawEvent := clickhouse_connector.CHEvent{
+			Namespace:       message.Namespace,
+			ValidationError: eventErr,
+			ID:              message.Serialized.Id,
+			Type:            message.Serialized.Type,
+			Source:          message.Serialized.Source,
+			Subject:         message.Serialized.Subject,
+			Time:            message.Serialized.Time,
+			Data:            message.Serialized.Data,
+			IngestedAt:      ingestedAt,
+			StoredAt:        storedAt,
+		}
+
+		rawEvents = append(rawEvents, rawEvent)
+
+		// Meter events per meter
+		for _, meterEvent := range message.MeterEvents {
+			meterEvent := clickhouse_connector.CHMeterEvent{
+				Namespace:   message.Namespace,
+				Time:        rawEvent.Time,
+				Meter:       meterEvent.Meter.ID,
+				Subject:     rawEvent.Subject,
+				Value:       meterEvent.Value,
+				GroupBy:     meterEvent.GroupBy,
+				EventID:     rawEvent.ID,
+				EventSource: rawEvent.Source,
+				EventType:   rawEvent.Type,
+				StoredAt:    rawEvent.StoredAt,
+				IngestedAt:  rawEvent.IngestedAt,
+			}
+
+			meterEvents = append(meterEvents, meterEvent)
+		}
 	}
 
-	sql, args := query.Build()
-	return sql, args, nil
-}
+	var err error
 
-// Clock is an interface for getting the current time.
-// It is used to make the code testable.
-type Clock interface {
-	Now() time.Time
-}
+	// Insert raw events
+	query := clickhouse_connector.InsertEventsQuery{
+		Database:      c.config.Database,
+		Events:        rawEvents,
+		QuerySettings: c.config.QuerySettings,
+	}
+	sql, args := query.ToSQL()
 
-// realClock implements Clock using the system clock.
-type realClock struct{}
+	// By default, ClickHouse is writing data synchronously.
+	// See https://clickhouse.com/docs/en/cloud/bestpractices/asynchronous-inserts
+	if c.config.AsyncInsert {
+		// With the `wait_for_async_insert` setting, you can configure
+		// if you want an insert statement to return with an acknowledgment
+		// either immediately after the data got inserted into the buffer.
+		err = c.config.ClickHouse.AsyncInsert(ctx, sql, c.config.AsyncInsertWait, args...)
+	} else {
+		err = c.config.ClickHouse.Exec(ctx, sql, args...)
+	}
 
-func (realClock) Now() time.Time {
-	return time.Now()
+	if err != nil {
+		return fmt.Errorf("failed to batch insert raw events: %w", err)
+	}
+
+	// Insert meter events
+	if len(meterEvents) > 0 {
+		query := clickhouse_connector.InsertMeterEventsQuery{
+			Database:      c.config.Database,
+			MeterEvents:   meterEvents,
+			QuerySettings: c.config.QuerySettings,
+		}
+		sql, args := query.ToSQL()
+
+		if c.config.AsyncInsert {
+			err = c.config.ClickHouse.AsyncInsert(ctx, sql, c.config.AsyncInsertWait, args...)
+		} else {
+			err = c.config.ClickHouse.Exec(ctx, sql, args...)
+		}
+
+		if err != nil {
+			return fmt.Errorf("failed to batch insert meter events: %w", err)
+		}
+	}
+
+	return nil
 }
---
+++ openmeter/streaming/clickhouse_connector/meter_event_query.go
@@ -0,0 +1,131 @@
+package clickhouse_connector
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/huandu/go-sqlbuilder"
+)
+
+const (
+	MeterEventTableName = "meter_events"
+)
+
+// Meter Event represents a single meter event in ClickHouse
+type CHMeterEvent struct {
+	// Identifiers
+	Namespace string    `ch:"namespace"`
+	Time      time.Time `ch:"time"`
+	Meter     string    `ch:"meter"`
+	Subject   string    `ch:"subject"`
+
+	// Usage
+	Value   float64           `ch:"value"`
+	GroupBy map[string]string `ch:"group_by"`
+
+	// Metadata
+	EventID     string    `ch:"event_id"`
+	EventSource string    `ch:"event_source"`
+	EventType   string    `ch:"event_type"`
+	IngestedAt  time.Time `ch:"ingested_at"`
+	StoredAt    time.Time `ch:"stored_at"`
+}
+
+// Create Meter Event Table
+type createMeterEventTable struct {
+	Database string
+}
+
+func (d createMeterEventTable) toSQL() string {
+	tableName := GetMeterEventsTableName(d.Database)
+
+	sb := sqlbuilder.ClickHouse.NewCreateTableBuilder()
+	sb.CreateTable(tableName)
+	sb.IfNotExists()
+
+	// Identifiers
+	sb.Define("namespace", "String")
+	sb.Define("time", "DateTime")
+	sb.Define("meter", "LowCardinality(String)")
+	sb.Define("subject", "String")
+
+	// Usage
+	sb.Define("value", "Decimal(14, 4)")
+	sb.Define("group_by", "Map(String, String)")
+
+	// Metadata
+	sb.Define("event_id", "String")
+	sb.Define("event_type", "LowCardinality(String)")
+	sb.Define("event_source", "String")
+	sb.Define("ingested_at", "DateTime")
+	sb.Define("stored_at", "DateTime")
+	sb.SQL("ENGINE = MergeTree")
+	sb.SQL("PARTITION BY toYYYYMM(time)")
+	sb.SQL("ORDER BY (namespace, time, meter, subject)")
+
+	sql, _ := sb.Build()
+	return sql
+}
+
+// Insert Meter Events Query
+type InsertMeterEventsQuery struct {
+	Database      string
+	MeterEvents   []CHMeterEvent
+	QuerySettings map[string]string
+}
+
+func (q InsertMeterEventsQuery) ToSQL() (string, []interface{}) {
+	tableName := GetMeterEventsTableName(q.Database)
+
+	query := sqlbuilder.ClickHouse.NewInsertBuilder()
+	query.InsertInto(tableName)
+	query.Cols(
+		"namespace",
+		"time",
+		"meter",
+		"subject",
+		"value",
+		"group_by",
+		"event_id",
+		"event_source",
+		"event_type",
+		"ingested_at",
+		"stored_at",
+	)
+
+	// Add settings
+	var settings []string
+	for key, value := range q.QuerySettings {
+		settings = append(settings, fmt.Sprintf("%s = %s", key, value))
+	}
+
+	if len(settings) > 0 {
+		query.SQL(fmt.Sprintf("SETTINGS %s", strings.Join(settings, ", ")))
+	}
+
+	for _, meterEvent := range q.MeterEvents {
+		query.Values(
+			meterEvent.Namespace,
+			meterEvent.Time,
+			meterEvent.Meter,
+			meterEvent.Subject,
+			meterEvent.Value,
+			meterEvent.GroupBy,
+			meterEvent.EventID,
+			meterEvent.EventSource,
+			meterEvent.EventType,
+			meterEvent.IngestedAt,
+			meterEvent.StoredAt,
+		)
+	}
+
+	sql, args := query.Build()
+	return sql, args
+}
+
+// Get Meter Events Table Name
+func GetMeterEventsTableName(database string) string {
+	return fmt.Sprintf("%s.%s%s", sqlbuilder.Escape(database), tablePrefix, MeterEventTableName)
+}
