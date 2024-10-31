a98c4f7f8e50a8152e9ffaedf16cb1272111446a
Peter Marton <peter@openmeter.io>
2024-10-31 15:07:21 -0700

    test(telemetry): add

---
+++ app/common/telemetry_test.go
@@ -0,0 +1,46 @@
+package common
+
+import (
+	"context"
+	"log/slog"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+)
+
+func TestLevelHandler(t *testing.T) {
+	mockHandler := &MockHandler{}
+	logger := slog.New(NewLevelHandler(mockHandler, slog.LevelInfo))
+
+	mockHandler.On("Enabled", mock.Anything, slog.LevelInfo).Return(true)
+	mockHandler.On("Enabled", mock.Anything, slog.LevelWarn).Return(true)
+	mockHandler.On("Enabled", mock.Anything, slog.LevelError).Return(true)
+
+	logger.Debug("debug")
+	logger.Info("info")
+	logger.Warn("warn")
+	logger.Error("error")
+
+	mockHandler.AssertExpectations(t)
+}
+
+type MockHandler struct {
+	mock.Mock
+}
+
+func (h *MockHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	args := h.Called(ctx, level)
+	return args.Bool(0)
+}
+
+func (h *MockHandler) WithGroup(name string) slog.Handler {
+	return h
+}
+
+func (h *MockHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return h
+}
+
+func (h *MockHandler) Handle(ctx context.Context, record slog.Record) error {
+	return nil
+}
