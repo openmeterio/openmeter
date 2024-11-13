fa8e531c2406dd5d94968ff647b43a7740f4b7bb
Peter Marton <peter@openmeter.io>
2024-11-12 18:54:25 -0800

    feat(server): reusable logger

--- pkg/server/logger.go
+++ pkg/server/logger.go
@@ -10,19 +10,24 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.27.0"
 )
 
-type StructuredLogger struct {
+var (
+	_ middleware.LogFormatter = (*RequestLogger)(nil)
+	_ middleware.LogEntry     = (*RequestLoggerEntry)(nil)
+)
+
+func NewRequestLoggerMiddleware(handler slog.Handler) func(next http.Handler) http.Handler {
+	return middleware.RequestLogger(&RequestLogger{Logger: handler})
+}
+
+type RequestLogger struct {
 	Logger slog.Handler
 }
 
-func NewStructuredLogger(handler slog.Handler) func(next http.Handler) http.Handler {
-	return middleware.RequestLogger(&StructuredLogger{Logger: handler})
+func (l *RequestLogger) NewLogEntry(r *http.Request) middleware.LogEntry {
+	return &RequestLoggerEntry{logger: slog.New(l.Logger), request: r}
 }
 
-func (l *StructuredLogger) NewLogEntry(r *http.Request) middleware.LogEntry {
-	return &StructuredLoggerEntry{logger: slog.New(l.Logger), request: r}
-}
-
-type StructuredLoggerEntry struct {
+type RequestLoggerEntry struct {
 	// logger is the underlying logger.
 	logger *slog.Logger
 
@@ -30,7 +35,7 @@ type StructuredLoggerEntry struct {
 	request *http.Request
 }
 
-func (e *StructuredLoggerEntry) Write(status, bytes int, header http.Header, elapsed time.Duration, extra interface{}) {
+func (e *RequestLoggerEntry) Write(status, bytes int, header http.Header, elapsed time.Duration, extra interface{}) {
 	e.logger.LogAttrs(e.request.Context(), slog.LevelDebug, "request complete",
 		slog.Int(string(semconv.HTTPResponseStatusCodeKey), status),
 		slog.Int(string(semconv.HTTPResponseSizeKey), bytes),
@@ -38,7 +43,7 @@ func (e *StructuredLoggerEntry) Write(status, bytes int, header http.Header, ela
 	)
 }
 
-func (e *StructuredLoggerEntry) Panic(v interface{}, stack []byte) {
+func (e *RequestLoggerEntry) Panic(v interface{}, stack []byte) {
 	e.logger.LogAttrs(e.request.Context(), slog.LevelError, "request panicked",
 		slog.String("stack", string(stack)),
 		slog.String("panic", fmt.Sprintf("%+v", v)),
