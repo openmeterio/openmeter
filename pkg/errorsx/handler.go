b24e07e31cbc497c2fcb4b9da78dddeceba855a3
Peter Marton <peter@openmeter.io>
2024-11-13 13:30:23 -0800

    refactor(handlers): get rid of wrapping

--- pkg/errorsx/handler.go
+++ pkg/errorsx/handler.go
@@ -6,37 +6,15 @@ import (
 	"log/slog"
 )
 
+var (
+	_ Handler = (*SlogHandler)(nil)
+	_ Handler = (*NopHandler)(nil)
+)
+
 // Handler handles an error.
 type Handler interface {
 	Handle(err error)
 	HandleContext(ctx context.Context, err error)
-}
-
-// AppHandler contains some custom logic to handle an error.
-type AppHandler struct {
-	Handler Handler
-}
-
-func NewAppHandler(handler Handler) AppHandler {
-	return AppHandler{Handler: handler}
-}
-
-func (h AppHandler) Handle(err error) {
-	// ignore context cancellation errors: they generally occur due to the client canceling the request
-	if errors.Is(err, context.Canceled) {
-		return
-	}
-
-	h.Handler.Handle(err)
-}
-
-func (h AppHandler) HandleContext(ctx context.Context, err error) {
-	// ignore context cancellation errors: they generally occur due to the client canceling the request
-	if errors.Is(err, context.Canceled) {
-		return
-	}
-
-	h.Handler.HandleContext(ctx, err)
 }
 
 // SlogHandler is a Handler that logs errors using slog.
@@ -49,43 +27,48 @@ func NewSlogHandler(logger *slog.Logger) SlogHandler {
 }
 
 func (h SlogHandler) Handle(err error) {
+	// Context canceled errors are logged as warnings.
+	if errors.Is(err, context.Canceled) {
+		h.Logger.Warn(err.Error())
+		return
+	}
+
+	// Warn errors are logged as warnings.
 	if wErr, ok := ErrorAs[*warnError](err); ok {
 		h.Logger.Warn(wErr.Error())
-	} else {
-		h.Logger.Error(err.Error())
+		return
 	}
+
+	// All other errors are logged as errors.
+	h.Logger.Error(err.Error())
 }
 
 func (h SlogHandler) HandleContext(ctx context.Context, err error) {
+	// Context canceled errors are logged as warnings.
+	if errors.Is(err, context.Canceled) {
+		h.Logger.WarnContext(ctx, err.Error())
+		return
+	}
+
+	// Warn errors are logged as warnings.
 	if wErr, ok := ErrorAs[*warnError](err); ok {
 		h.Logger.WarnContext(ctx, wErr.Error())
-	} else {
-		h.Logger.ErrorContext(ctx, err.Error())
+		return
 	}
+
+	// All other errors are logged as errors.
+	h.Logger.ErrorContext(ctx, err.Error())
 }
 
 // NopHandler ignores all errors.
 type NopHandler struct{}
 
+func NewNopHandler() NopHandler {
+	return NopHandler{}
+}
+
 func (h NopHandler) Handle(err error) {
 }
 
 func (h NopHandler) HandleContext(ctx context.Context, err error) {
-}
-
-// ContextHandler always accepts a context.
-type ContextHandler struct {
-	Handler Handler
-}
-
-func NewContextHandler(handler Handler) ContextHandler {
-	return ContextHandler{Handler: handler}
-}
-
-func (h ContextHandler) Handle(ctx context.Context, err error) {
-	h.Handler.HandleContext(ctx, err)
-}
-
-func (h ContextHandler) HandleContext(ctx context.Context, err error) {
-	h.Handler.HandleContext(ctx, err)
 }
