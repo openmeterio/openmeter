4d282f95a33c5e3df9ac330a597c37089dc28699
Alex Goth <alex.goth@openmeter.io>
2024-12-10 16:05:10 +0100

    test(subscription): implement custom subscription creation

--- e2e/productcatalog_test.go
+++ e2e/productcatalog_test.go
@@ -24,12 +24,12 @@ func TestPlan(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	// Let's set up a customer
+	// Let's set up two customers
 	customerAPIRes, err := client.CreateCustomerWithResponse(ctx, api.CreateCustomerJSONRequestBody{
-		Name:         "Test Customer",
+		Name:         "Test Customer 1",
 		Currency:     lo.ToPtr(api.CurrencyCode("USD")),
 		Description:  lo.ToPtr("Test Customer Description"),
-		PrimaryEmail: lo.ToPtr("customer@mail.com"),
+		PrimaryEmail: lo.ToPtr("customer1@mail.com"),
 		Timezone:     lo.ToPtr(time.UTC.String()),
 		BillingAddress: &api.Address{
 			City:        lo.ToPtr("City"),
@@ -41,13 +41,37 @@ func TestPlan(t *testing.T) {
 			PostalCode:  lo.ToPtr("12345"),
 		},
 		UsageAttribution: api.CustomerUsageAttribution{
-			SubjectKeys: []string{"test_customer_subject"},
+			SubjectKeys: []string{"test_customer_subject_1"},
 		},
 	})
 	require.Nil(t, err)
 
-	customer := customerAPIRes.JSON201
-	require.NotNil(t, customer)
+	customer1 := customerAPIRes.JSON201
+	require.NotNil(t, customer1)
+
+	customerAPIRes, err = client.CreateCustomerWithResponse(ctx, api.CreateCustomerJSONRequestBody{
+		Name:         "Test Customer 2",
+		Currency:     lo.ToPtr(api.CurrencyCode("USD")),
+		Description:  lo.ToPtr("Test Customer Description"),
+		PrimaryEmail: lo.ToPtr("customer2@mail.com"),
+		Timezone:     lo.ToPtr(time.UTC.String()),
+		BillingAddress: &api.Address{
+			City:        lo.ToPtr("City"),
+			Country:     lo.ToPtr("US"),
+			Line1:       lo.ToPtr("Line 1"),
+			Line2:       lo.ToPtr("Line 2"),
+			State:       lo.ToPtr("State"),
+			PhoneNumber: lo.ToPtr("1234567890"),
+			PostalCode:  lo.ToPtr("12345"),
+		},
+		UsageAttribution: api.CustomerUsageAttribution{
+			SubjectKeys: []string{"test_customer_subject_2"},
+		},
+	})
+	require.Nil(t, err)
+
+	customer2 := customerAPIRes.JSON201
+	require.NotNil(t, customer1)
 
 	// Now, let's create dedicated features for the plan
 	featureAPIRes, err := client.CreateFeatureWithResponse(ctx, api.CreateFeatureJSONRequestBody{
@@ -71,115 +95,118 @@ func TestPlan(t *testing.T) {
 
 	var planId string
 
+	// Lets build a PlanCreate input
+	p1RC1 := api.RateCard{}
+	err = p1RC1.FromRateCardFlatFee(api.RateCardFlatFee{
+		Name:        "Test Plan Phase 1 Rate Card 1",
+		Description: lo.ToPtr("Has a one time flat price like an installation fee"),
+		Key:         "test_plan_phase_1_rate_card_1",
+		TaxConfig: &api.TaxConfig{
+			Stripe: &api.StripeTaxConfig{
+				Code: "txcd_10000000",
+			},
+		},
+		Price: &api.FlatPriceWithPaymentTerm{
+			Amount:      "1000",
+			PaymentTerm: lo.ToPtr(api.PricePaymentTerm("in_advance")),
+			Type:        api.FlatPriceWithPaymentTermType("flat"),
+		},
+		BillingCadence: nil,
+		Type:           api.RateCardFlatFeeType("flat"),
+	})
+	require.Nil(t, err)
+
+	p1RC2 := api.RateCard{}
+	err = p1RC2.FromRateCardFlatFee(api.RateCardFlatFee{
+		Name:        "Test Plan Phase 1 Rate Card 2",
+		Description: lo.ToPtr("Has a monthly recurring price to grant access to a feature"),
+		Key:         PlanFeatureKey,
+		FeatureKey:  lo.ToPtr(PlanFeatureKey),
+		TaxConfig: &api.TaxConfig{
+			Stripe: &api.StripeTaxConfig{
+				Code: "txcd_10000000",
+			},
+		},
+		Price: &api.FlatPriceWithPaymentTerm{
+			Amount:      "1000",
+			PaymentTerm: lo.ToPtr(api.PricePaymentTerm("in_advance")),
+			Type:        api.FlatPriceWithPaymentTermType("flat"),
+		},
+		BillingCadence: lo.ToPtr("P1M"),
+		Type:           api.RateCardFlatFeeType("flat"),
+	})
+	require.Nil(t, err)
+
+	p2RC1 := api.RateCard{}
+	err = p2RC1.FromRateCardFlatFee(api.RateCardFlatFee{
+		Name:        "Test Plan Phase 2 Rate Card 1",
+		Description: lo.ToPtr("Keeps access to the same feature as in phase 1"),
+		Key:         PlanFeatureKey,
+		FeatureKey:  lo.ToPtr(PlanFeatureKey),
+		TaxConfig: &api.TaxConfig{
+			Stripe: &api.StripeTaxConfig{
+				Code: "txcd_10000000",
+			},
+		},
+		Price: &api.FlatPriceWithPaymentTerm{
+			Amount:      "1000",
+			PaymentTerm: lo.ToPtr(api.PricePaymentTerm("in_advance")),
+			Type:        api.FlatPriceWithPaymentTermType("flat"),
+		},
+		BillingCadence: lo.ToPtr("P1M"),
+		Type:           api.RateCardFlatFeeType("flat"),
+	})
+	require.Nil(t, err)
+
+	p2RC2P := api.RateCardUsageBasedPrice{}
+	err = p2RC2P.FromUnitPriceWithCommitments(api.UnitPriceWithCommitments{
+		Amount: "0.1",
+		Type:   api.UnitPriceWithCommitmentsType("unit"),
+	})
+	require.Nil(t, err)
+
+	p2RC2 := api.RateCard{}
+	err = p2RC2.FromRateCardUsageBased(api.RateCardUsageBased{
+		Name:        "Test Plan Phase 2 Rate Card 2",
+		Description: lo.ToPtr("Adds a usage based price for the metered feature"),
+		Key:         PlanMeteredFeatureKey,
+		FeatureKey:  lo.ToPtr(PlanMeteredFeatureKey),
+		TaxConfig: &api.TaxConfig{
+			Stripe: &api.StripeTaxConfig{
+				Code: "txcd_10000000",
+			},
+		},
+		BillingCadence: "P1M",
+		Price:          &p2RC2P,
+		Type:           api.RateCardUsageBasedType("usage_based"),
+	})
+	require.Nil(t, err)
+
+	planCreate := api.PlanCreate{
+		Currency:    api.CurrencyCode("USD"),
+		Name:        "Test Plan",
+		Description: lo.ToPtr("Test Plan Description"),
+		Key:         PlanKey,
+		Phases: []api.PlanPhase{
+			{
+				Name:        "Test Plan Phase 1",
+				Key:         "test_plan_phase_1",
+				Description: lo.ToPtr("Test Plan Phase 1 Description"),
+				StartAfter:  nil,
+				RateCards:   []api.RateCard{p1RC1, p1RC2},
+			},
+			{
+				Name:        "Test Plan Phase 2",
+				Key:         "test_plan_phase_2",
+				Description: lo.ToPtr("Test Plan Phase 1 Description"),
+				StartAfter:  lo.ToPtr("P2M"),
+				RateCards:   []api.RateCard{p2RC1, p2RC2},
+			},
+		},
+	}
+
 	t.Run("Should create a plan on happy path", func(t *testing.T) {
-		p1RC1 := api.RateCard{}
-		err := p1RC1.FromRateCardFlatFee(api.RateCardFlatFee{
-			Name:        "Test Plan Phase 1 Rate Card 1",
-			Description: lo.ToPtr("Has a one time flat price like an installation fee"),
-			Key:         "test_plan_phase_1_rate_card_1",
-			TaxConfig: &api.TaxConfig{
-				Stripe: &api.StripeTaxConfig{
-					Code: "txcd_10000000",
-				},
-			},
-			Price: &api.FlatPriceWithPaymentTerm{
-				Amount:      "1000",
-				PaymentTerm: lo.ToPtr(api.PricePaymentTerm("in_advance")),
-				Type:        api.FlatPriceWithPaymentTermType("flat"),
-			},
-			BillingCadence: nil,
-			Type:           api.RateCardFlatFeeType("flat"),
-		})
-		require.Nil(t, err)
-
-		p1RC2 := api.RateCard{}
-		err = p1RC2.FromRateCardFlatFee(api.RateCardFlatFee{
-			Name:        "Test Plan Phase 1 Rate Card 2",
-			Description: lo.ToPtr("Has a monthly recurring price to grant access to a feature"),
-			Key:         PlanFeatureKey,
-			FeatureKey:  lo.ToPtr(PlanFeatureKey),
-			TaxConfig: &api.TaxConfig{
-				Stripe: &api.StripeTaxConfig{
-					Code: "txcd_10000000",
-				},
-			},
-			Price: &api.FlatPriceWithPaymentTerm{
-				Amount:      "1000",
-				PaymentTerm: lo.ToPtr(api.PricePaymentTerm("in_advance")),
-				Type:        api.FlatPriceWithPaymentTermType("flat"),
-			},
-			BillingCadence: lo.ToPtr("P1M"),
-			Type:           api.RateCardFlatFeeType("flat"),
-		})
-		require.Nil(t, err)
-
-		p2RC1 := api.RateCard{}
-		err = p2RC1.FromRateCardFlatFee(api.RateCardFlatFee{
-			Name:        "Test Plan Phase 2 Rate Card 1",
-			Description: lo.ToPtr("Keeps access to the same feature as in phase 1"),
-			Key:         PlanFeatureKey,
-			FeatureKey:  lo.ToPtr(PlanFeatureKey),
-			TaxConfig: &api.TaxConfig{
-				Stripe: &api.StripeTaxConfig{
-					Code: "txcd_10000000",
-				},
-			},
-			Price: &api.FlatPriceWithPaymentTerm{
-				Amount:      "1000",
-				PaymentTerm: lo.ToPtr(api.PricePaymentTerm("in_advance")),
-				Type:        api.FlatPriceWithPaymentTermType("flat"),
-			},
-			BillingCadence: lo.ToPtr("P1M"),
-			Type:           api.RateCardFlatFeeType("flat"),
-		})
-		require.Nil(t, err)
-
-		p2RC2P := api.RateCardUsageBasedPrice{}
-		err = p2RC2P.FromUnitPriceWithCommitments(api.UnitPriceWithCommitments{
-			Amount: "0.1",
-			Type:   api.UnitPriceWithCommitmentsType("unit"),
-		})
-		require.Nil(t, err)
-
-		p2RC2 := api.RateCard{}
-		err = p2RC2.FromRateCardUsageBased(api.RateCardUsageBased{
-			Name:        "Test Plan Phase 2 Rate Card 2",
-			Description: lo.ToPtr("Adds a usage based price for the metered feature"),
-			Key:         PlanMeteredFeatureKey,
-			FeatureKey:  lo.ToPtr(PlanMeteredFeatureKey),
-			TaxConfig: &api.TaxConfig{
-				Stripe: &api.StripeTaxConfig{
-					Code: "txcd_10000000",
-				},
-			},
-			BillingCadence: "P1M",
-			Price:          &p2RC2P,
-			Type:           api.RateCardUsageBasedType("usage_based"),
-		})
-		require.Nil(t, err)
-
-		planAPIRes, err := client.CreatePlanWithResponse(ctx, api.CreatePlanJSONRequestBody{
-			Currency:    api.CurrencyCode("USD"),
-			Name:        "Test Plan",
-			Description: lo.ToPtr("Test Plan Description"),
-			Key:         PlanKey,
-			Phases: []api.PlanPhase{
-				{
-					Name:        "Test Plan Phase 1",
-					Key:         "test_plan_phase_1",
-					Description: lo.ToPtr("Test Plan Phase 1 Description"),
-					StartAfter:  nil, // Ommittable for first phase
-					RateCards:   []api.RateCard{p1RC1, p1RC2},
-				},
-				{
-					Name:        "Test Plan Phase 2",
-					Key:         "test_plan_phase_2",
-					Description: lo.ToPtr("Test Plan Phase 1 Description"),
-					StartAfter:  lo.ToPtr("P2M"),
-					RateCards:   []api.RateCard{p2RC1, p2RC2},
-				},
-			},
-		})
+		planAPIRes, err := client.CreatePlanWithResponse(ctx, planCreate)
 		require.Nil(t, err)
 		require.Equal(t, 201, planAPIRes.StatusCode())
 
@@ -209,14 +236,38 @@ func TestPlan(t *testing.T) {
 
 	var subscriptionId string
 
+	t.Run("Should create a custom subscription", func(t *testing.T) {
+		require.NotNil(t, customer1)
+		require.NotNil(t, customer1.Id)
+
+		create := api.SubscriptionCreate{}
+		err := create.FromCustomSubscriptionCreate(api.CustomSubscriptionCreate{
+			ActiveFrom: startTime,
+			CustomerId: *customer2.Id,
+			CustomPlan: planCreate, // For simplicity we can reuse the same plan input, we know its valid
+		})
+		require.Nil(t, err)
+
+		apiRes, err := client.CreateSubscriptionWithResponse(ctx, create)
+		require.Nil(t, err)
+
+		assert.Equal(t, 201, apiRes.StatusCode(), "received the following body: %s", apiRes.Body)
+
+		subscription := apiRes.JSON201
+		require.NotNil(t, subscription)
+		require.NotNil(t, subscription.Id)
+		assert.Equal(t, api.SubscriptionStatusActive, *subscription.Status)
+		assert.Nil(t, subscription.Plan)
+	})
+
 	t.Run("Should create a subscription based on the plan", func(t *testing.T) {
-		require.NotNil(t, customer)
-		require.NotNil(t, customer.Id)
+		require.NotNil(t, customer1)
+		require.NotNil(t, customer1.Id)
 
 		create := api.SubscriptionCreate{}
 		err := create.FromPlanSubscriptionCreate(api.PlanSubscriptionCreate{
 			ActiveFrom:  startTime,
-			CustomerId:  *customer.Id,
+			CustomerId:  *customer1.Id,
 			Name:        "Test Subscription",
 			Description: lo.ToPtr("Test Subscription Description"),
 			Plan: api.PlanReferenceInput{
@@ -226,7 +277,6 @@ func TestPlan(t *testing.T) {
 		})
 		require.Nil(t, err)
 
-		// FIXME: why is this the generated type CreateSubscriptionJSONRequestBody?
 		apiRes, err := client.CreateSubscriptionWithResponse(ctx, create)
 		require.Nil(t, err)
 
