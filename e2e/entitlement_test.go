9045cd368ba732f7f2483141fdd5ee45c0438533
Alex Goth <alex.goth@openmeter.io>
2025-01-06 10:50:26 +0100

    fix(e2e): explicit wait times

--- e2e/entitlement_test.go
+++ e2e/entitlement_test.go
@@ -99,7 +99,7 @@ func TestEntitlementWithUniqueCountAggregation(t *testing.T) {
 		now := time.Now()
 
 		for i := 0; i < uniqueEventCount*2; i++ {
-			timestamp := gofakeit.DateRange(now, now.Add(time.Second*5))
+			timestamp := gofakeit.DateRange(now, now.Add(time.Second))
 
 			ev := cloudevents.New()
 			ev.SetID(gofakeit.UUID())
@@ -119,7 +119,9 @@ func TestEntitlementWithUniqueCountAggregation(t *testing.T) {
 
 		// Wait for events to be processed
 		assert.EventuallyWithT(t, func(t *assert.CollectT) {
-			resp, err := client.QueryMeterWithResponse(ctx, meterSlug, nil)
+			resp, err := client.QueryMeterWithResponse(ctx, meterSlug, &api.QueryMeterParams{
+				To: convert.ToPointer(time.Now().Truncate(time.Minute)),
+			})
 			require.NoError(t, err)
 			require.Equal(t, http.StatusOK, resp.StatusCode())
 
@@ -129,7 +131,9 @@ func TestEntitlementWithUniqueCountAggregation(t *testing.T) {
 	})
 
 	t.Run("Should calculate usage correctly", func(t *testing.T) {
-		resp, err := client.GetEntitlementValueWithResponse(ctx, subject, entitlementId, nil)
+		resp, err := client.GetEntitlementValueWithResponse(ctx, subject, entitlementId, &api.GetEntitlementValueParams{
+			Time: convert.ToPointer(time.Now().Truncate(time.Minute)),
+		})
 		require.NoError(t, err)
 		require.Equal(t, http.StatusOK, resp.StatusCode())
 
