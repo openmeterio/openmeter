6960cdd19e8bbe4e5b4c3aaa079fb115486653d5
Alex Goth <alex.goth@openmeter.io>
2025-01-16 13:27:43 +0100

    refactor(entitlement): filter itemhistory from view response

--- openmeter/productcatalog/subscription/http/get.go
+++ openmeter/productcatalog/subscription/http/get.go
@@ -52,7 +52,7 @@ func (h *handler) GetSubscription() GetSubscriptionHandler {
 				return def, err
 			}
 
-			return MapSubscriptionViewToAPI(view)
+			return MapSubscriptionViewToAPI(view.WithoutItemHistory())
 		},
 		commonhttp.JSONResponseEncoderWithStatus[GetSubscriptionResponse](http.StatusOK),
 		httptransport.AppendOptions(
