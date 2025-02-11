017bc7f100be616f53fc560edf8537976399b14f
Peter Marton <peter@openmeter.io>
2025-02-10 16:13:51 -0800

    feat(errors): generic

--- pkg/models/error.go
+++ pkg/models/error.go
@@ -2,6 +2,11 @@ package models
 
 import "fmt"
 
+// TODO: these should be picked up in a general server error handler
+
+// MeterNotFoundError represents an error when a meter is not found
+var _ error = (*MeterNotFoundError)(nil)
+
 type MeterNotFoundError struct {
 	MeterSlug string
 }
@@ -9,6 +14,9 @@ type MeterNotFoundError struct {
 func (e *MeterNotFoundError) Error() string {
 	return fmt.Sprintf("meter not found: %s", e.MeterSlug)
 }
+
+// NamespaceNotFoundError represents an error when a namespace is not found
+var _ error = (*NamespaceNotFoundError)(nil)
 
 type NamespaceNotFoundError struct {
 	Namespace string
@@ -18,7 +26,9 @@ func (e *NamespaceNotFoundError) Error() string {
 	return fmt.Sprintf("namespace not found: %s", e.Namespace)
 }
 
-// TODO: these should be picked up in a general server error handler
+// GenericServerError represents an error when a server error occurs
+var _ error = (*GenericUserError)(nil)
+
 type GenericUserError struct {
 	Inner error
 }
@@ -30,6 +40,44 @@ func (e *GenericUserError) Error() string {
 func (e *GenericUserError) Unwrap() error {
 	return e.Inner
 }
+
+// GenericNotFoundError represents an error when a resource is not found
+var _ error = (*GenericNotFoundError)(nil)
+
+type GenericNotFoundError genericError
+
+func (e GenericNotFoundError) Error() string {
+	return e.Err.Error()
+}
+
+func (e GenericNotFoundError) Unwrap() error {
+	return e.Err
+}
+
+// GenericBadGateway represents an error when a resource is not found
+var _ error = (*GenericBadGateway)(nil)
+
+type GenericBadGateway genericError
+
+func (e GenericBadGateway) Error() string {
+	return e.Err.Error()
+}
+
+func (e GenericBadGateway) Unwrap() error {
+	return e.Err
+}
+
+// GenericValidationError represents an error when a resource is not found
+var _ error = (*GenericValidationError)(nil)
+
+type GenericValidationError genericError
+
+func (e GenericValidationError) Error() string {
+	return e.Err.Error()
+}
+
+// GenericConflictError is a generic error for conflict errors
+var _ error = (*GenericConflictError)(nil)
 
 type GenericConflictError struct {
 	Inner error
@@ -43,6 +91,9 @@ func (e *GenericConflictError) Unwrap() error {
 	return e.Inner
 }
 
+// GenericForbiddenError is a generic error for forbidden errors
+var _ error = (*GenericForbiddenError)(nil)
+
 type GenericForbiddenError struct {
 	Inner error
 }
@@ -53,4 +104,9 @@ func (e *GenericForbiddenError) Error() string {
 
 func (e *GenericForbiddenError) Unwrap() error {
 	return e.Inner
+}
+
+// genericError represents a generic error
+type genericError struct {
+	Err error
 }
