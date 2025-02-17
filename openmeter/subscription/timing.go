393562dc8ea352bac6abd4ad0ae181f695249215
Alex Goth <alex.goth@openmeter.io>
2025-02-17 16:19:58 +0100

    fix(subs): fix "phase xy has no billables" errors

--- openmeter/subscription/timing.go
+++ openmeter/subscription/timing.go
@@ -81,6 +81,10 @@ func (c Timing) ResolveForSpec(spec SubscriptionSpec) (time.Time, error) {
 				return def, &models.GenericUserError{Inner: fmt.Errorf("billing isn't active for the subscription, there isn't a next_billing_cycle")}
 			}
 
+			if !currentPhase.HasBillables() {
+				return def, &models.GenericUserError{Inner: fmt.Errorf("current phase has no billables, there isn't a next_billing_cycle")}
+			}
+
 			period, err := spec.GetAlignedBillingPeriodAt(currentPhase.PhaseKey, clock.Now())
 
 			return period.To, err
