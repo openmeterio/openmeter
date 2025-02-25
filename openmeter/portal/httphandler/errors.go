075a100e02dbdbe816e0333a852bbf6ad268d318
Peter Marton <peter@openmeter.io>
2025-02-24 19:33:27 -0800

    fix(portal): meeter not found

--- openmeter/portal/httphandler/errors.go
+++ openmeter/portal/httphandler/errors.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/openmeterio/openmeter/openmeter/meter"
 	"github.com/openmeterio/openmeter/openmeter/portal"
 	"github.com/openmeterio/openmeter/pkg/framework/commonhttp"
 	"github.com/openmeterio/openmeter/pkg/framework/transport/httptransport"
@@ -11,6 +12,7 @@ import (
 
 func errorEncoder() httptransport.ErrorEncoder {
 	return func(ctx context.Context, err error, w http.ResponseWriter, r *http.Request) bool {
-		return commonhttp.HandleErrorIfTypeMatches[*portal.NotImplementedError](ctx, http.StatusNotImplemented, err, w)
+		return commonhttp.HandleErrorIfTypeMatches[*portal.NotImplementedError](ctx, http.StatusNotImplemented, err, w) ||
+			commonhttp.HandleErrorIfTypeMatches[*meter.MeterNotFoundError](ctx, http.StatusNotFound, err, w)
 	}
 }
