6d1e5b27a3febd5573c34344d8ea3e556e7b9061
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2025-03-06 19:05:57 +0100

    feat: support ignoring migrations (#2382)

---
+++ tools/migrate/testdata/ignore/20240903155435_entitlement-expired-index.down.sql
@@ -0,0 +1,3 @@
+-- migration:ignore
+-- reverse: create index "entitlement_current_usage_period_end_deleted_at" to table: "entitlements"
+DROP INDEX "entitlement_current_usage_period_end_deleted_at";
