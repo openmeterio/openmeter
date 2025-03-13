b4ef2535de5137b7da0ba44d7d921b0819598371
Alex Goth <alex.goth@openmeter.io>
2025-03-13 14:03:06 +0100

    feat: subscription deletion

--- openmeter/server/router/subscription.go
+++ openmeter/server/router/subscription.go
@@ -60,3 +60,10 @@ func (a *Router) RestoreSubscription(w http.ResponseWriter, r *http.Request, sub
 		ID: subscriptionId,
 	}).ServeHTTP(w, r)
 }
+
+// (DELETE /api/v1/subscriptions/{subscriptionId})
+func (a *Router) DeleteSubscription(w http.ResponseWriter, r *http.Request, subscriptionId string) {
+	a.subscriptionHandler.DeleteSubscription().With(subscriptionhttpdriver.DeleteSubscriptionParams{
+		ID: subscriptionId,
+	}).ServeHTTP(w, r)
+}
