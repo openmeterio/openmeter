8fe5fbde104664ea424433d2cd484009420d6928
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2025-04-09 09:34:55 +0200

    feat: add custom validator

--- pkg/models/validator.go
+++ pkg/models/validator.go
@@ -1,6 +1,26 @@
 package models
 
+import "errors"
+
 type Validator interface {
 	// Validate returns an error if the instance of the Validator is invalid.
 	Validate() error
+}
+
+type ValidatorFunc[T any] func(T) error
+
+type CustomValidator[T any] interface {
+	ValidateWith(...ValidatorFunc[T]) error
+}
+
+func Validate[T any](v T, validators ...ValidatorFunc[T]) error {
+	var errs []error
+
+	for _, validator := range validators {
+		if err := validator(v); err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	return NewNillableGenericValidationError(errors.Join(errs...))
 }
