06c850ddda883dfac2fa5c49334ec26b564c9dda
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2025-04-16 07:06:59 +0200

    refactor: use discard handler for test logger

--- openmeter/testutils/logger.go
+++ openmeter/testutils/logger.go
@@ -1,7 +1,6 @@
 package testutils
 
 import (
-	"context"
 	"log/slog"
 	"testing"
 )
@@ -11,16 +10,8 @@ func NewLogger(t testing.TB) *slog.Logger {
 	return slog.Default()
 }
 
-// discardHandler is a slog.Handler implementation which does not emit log messages
-// See: https://go-review.googlesource.com/c/go/+/548335/5/src/log/slog/example_discard_test.go#14
-type discardHandler struct {
-	slog.JSONHandler
-}
-
-func (d *discardHandler) Enabled(context.Context, slog.Level) bool { return false }
-
 func NewDiscardLogger(t testing.TB) *slog.Logger {
 	t.Helper()
 
-	return slog.New(&discardHandler{})
+	return slog.New(slog.DiscardHandler)
 }
