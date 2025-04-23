3be2df1101a61a617592b0a4f781b5916d40eaea
Alex Goth <alex.goth@openmeter.io>
2025-04-23 14:23:56 +0200

    feat(subsadd): write extend restore on subsaddratecard

--- openmeter/subscription/addon/extend.go
+++ openmeter/subscription/addon/extend.go
@@ -1,6 +1,7 @@
 package subscriptionaddon
 
 import (
+	"errors"
 	"fmt"
 	"reflect"
 
@@ -77,6 +78,76 @@ func (a SubscriptionAddonRateCard) Apply(target productcatalog.RateCard) error {
 
 // Restore restores the addon rate card to the target rate card
 func (a SubscriptionAddonRateCard) Restore(target productcatalog.RateCard) error {
-	// TODO: implement
-	return models.NewGenericNotImplementedError(fmt.Errorf("not implemented"))
+	// Target has has to be implemented by a pointer otherwise we can't use it as a receiver. Let's check that
+	typ := reflect.TypeOf(target)
+	if typ == nil {
+		return fmt.Errorf("target must not be nil")
+	}
+
+	if typ.Kind() != reflect.Ptr {
+		return fmt.Errorf("target must be a pointer")
+	}
+
+	if a.AddonRateCard.AsMeta().Price == nil && a.AddonRateCard.AsMeta().EntitlementTemplate == nil {
+		return nil
+	}
+
+	if err := validateRateCards(a.AddonRateCard.RateCard, target); err != nil {
+		return err
+	}
+
+	return target.ChangeMeta(func(m productcatalog.RateCardMeta) (productcatalog.RateCardMeta, error) {
+		aMeta := a.AddonRateCard.AsMeta()
+		tMeta := m.Clone()
+
+		// Let's update the price
+		if aMeta.Price != nil {
+			switch {
+			case tMeta.Price == nil:
+				return m, fmt.Errorf("target price is nil, cannot restore price without addon")
+			case tMeta.Price.Type() == productcatalog.FlatPriceType:
+				tFlat, _ := tMeta.Price.AsFlat()
+				aFlat, _ := aMeta.Price.AsFlat()
+
+				newAmount := tFlat.Amount.Sub(aFlat.Amount)
+				if newAmount.IsNegative() {
+					return m, fmt.Errorf("restoring flat price would yield a negative amount: %s - %s = %s", tFlat.Amount, aFlat.Amount, newAmount)
+				}
+
+				m.Price = productcatalog.NewPriceFrom(productcatalog.FlatPrice{
+					Amount:      newAmount,
+					PaymentTerm: tFlat.PaymentTerm,
+				})
+			default:
+				return m, fmt.Errorf("not supported price type: %s", tMeta.Price.Type())
+			}
+		}
+
+		// Let's update the entitlement template
+		if aMeta.EntitlementTemplate != nil {
+			switch {
+			case tMeta.EntitlementTemplate == nil:
+				return m, fmt.Errorf("target entitlement template is nil, cannot restore entitlement template without addon")
+			case tMeta.EntitlementTemplate.Type().String() == entitlement.EntitlementTypeBoolean.String():
+				// TODO: figure this out, we have a data loss situation here
+				return m, models.NewGenericNotImplementedError(errors.New("boolean entitlement templates are not supported"))
+			case tMeta.EntitlementTemplate.Type().String() == entitlement.EntitlementTypeMetered.String():
+				tMetered, _ := tMeta.EntitlementTemplate.AsMetered()
+				aMetered, _ := aMeta.EntitlementTemplate.AsMetered()
+
+				newIssueAfterReset := lo.FromPtrOr(tMetered.IssueAfterReset, 0) - lo.FromPtrOr(aMetered.IssueAfterReset, 0)
+
+				if newIssueAfterReset < 0 {
+					return m, fmt.Errorf("restoring entitlement template would yield a negative issue after reset: %.0f - %.0f = %.0f", lo.FromPtrOr(tMetered.IssueAfterReset, 0), lo.FromPtrOr(aMetered.IssueAfterReset, 0), newIssueAfterReset)
+				}
+
+				tMetered.IssueAfterReset = lo.ToPtr(newIssueAfterReset)
+				m.EntitlementTemplate = productcatalog.NewEntitlementTemplateFrom(tMetered)
+			default:
+				return m, fmt.Errorf("not supported entitlement template type: %s", tMeta.EntitlementTemplate.Type())
+			}
+		}
+
+		return m, nil
+	})
 }
--- openmeter/subscription/addon/extend_test.go
+++ openmeter/subscription/addon/extend_test.go
@@ -14,9 +14,10 @@ import (
 	subscriptiontestutils "github.com/openmeterio/openmeter/openmeter/subscription/testutils"
 	"github.com/openmeterio/openmeter/openmeter/testutils"
 	"github.com/openmeterio/openmeter/pkg/isodate"
+	"github.com/openmeterio/openmeter/pkg/models"
 )
 
-func TestExtendApply(t *testing.T) {
+func TestValidations(t *testing.T) {
 	someMeta := productcatalog.RateCardMeta{
 		Name:        "Test Addon Rate Card 4",
 		Description: lo.ToPtr("Test Addon Rate Card 4 Description"),
@@ -33,18 +34,35 @@ func TestExtendApply(t *testing.T) {
 			RateCardMeta: someMeta.Clone(),
 		})
 
-		err := rc.Apply(nil)
-		require.Error(t, err)
-		require.ErrorContains(t, err, "target must not be nil")
+		t.Run("Apply", func(t *testing.T) {
+			err := rc.Apply(nil)
+			require.Error(t, err)
+			require.ErrorContains(t, err, "target must not be nil")
+		})
+
+		t.Run("Restore", func(t *testing.T) {
+			err := rc.Restore(nil)
+			require.Error(t, err)
+			require.ErrorContains(t, err, "target must not be nil")
+		})
 	})
 
 	t.Run("Should error if provided RateCard is not a pointer", func(t *testing.T) {
 		rc := getTestAddonRateCard(&productcatalog.FlatFeeRateCard{
 			RateCardMeta: someMeta.Clone(),
 		})
-		err := rc.Apply(nonPointerRateCard{})
-		require.Error(t, err)
-		require.ErrorContains(t, err, "target must be a pointer")
+
+		t.Run("Apply", func(t *testing.T) {
+			err := rc.Apply(nonPointerRateCard{})
+			require.Error(t, err)
+			require.ErrorContains(t, err, "target must be a pointer")
+		})
+
+		t.Run("Restore", func(t *testing.T) {
+			err := rc.Restore(nonPointerRateCard{})
+			require.Error(t, err)
+			require.ErrorContains(t, err, "target must be a pointer")
+		})
 	})
 
 	t.Run("Should error if target RateCard has different price type", func(t *testing.T) {
@@ -57,11 +75,21 @@ func TestExtendApply(t *testing.T) {
 			Amount: alpacadecimal.NewFromInt(100),
 		})
 
-		err := rc.Apply(&productcatalog.UsageBasedRateCard{
-			RateCardMeta: meta,
+		t.Run("Apply", func(t *testing.T) {
+			err := rc.Apply(&productcatalog.UsageBasedRateCard{
+				RateCardMeta: meta,
+			})
+			require.Error(t, err)
+			require.ErrorContains(t, err, "target and addon rate card price types do not match")
 		})
-		require.Error(t, err)
-		require.ErrorContains(t, err, "target and addon rate card price types do not match")
+
+		t.Run("Restore", func(t *testing.T) {
+			err := rc.Restore(&productcatalog.UsageBasedRateCard{
+				RateCardMeta: meta,
+			})
+			require.Error(t, err)
+			require.ErrorContains(t, err, "target and addon rate card price types do not match")
+		})
 	})
 
 	t.Run("Should error if target RateCard has different entitlement type", func(t *testing.T) {
@@ -75,11 +103,21 @@ func TestExtendApply(t *testing.T) {
 		meta = someMeta.Clone()
 		meta.EntitlementTemplate = productcatalog.NewEntitlementTemplateFrom(productcatalog.StaticEntitlementTemplate{})
 
-		err := rc.Apply(&productcatalog.UsageBasedRateCard{
-			RateCardMeta: meta,
+		t.Run("Apply", func(t *testing.T) {
+			err := rc.Apply(&productcatalog.UsageBasedRateCard{
+				RateCardMeta: meta,
+			})
+			require.Error(t, err)
+			require.ErrorContains(t, err, "target and addon rate card entitlement types do not match")
 		})
-		require.Error(t, err)
-		require.ErrorContains(t, err, "target and addon rate card entitlement types do not match")
+
+		t.Run("Restore", func(t *testing.T) {
+			err := rc.Restore(&productcatalog.UsageBasedRateCard{
+				RateCardMeta: meta,
+			})
+			require.Error(t, err)
+			require.ErrorContains(t, err, "target and addon rate card entitlement types do not match")
+		})
 	})
 
 	t.Run("Should error if target RateCard has a different key", func(t *testing.T) {
@@ -90,32 +128,21 @@ func TestExtendApply(t *testing.T) {
 		meta := someMeta.Clone()
 		meta.Key = "different-key"
 
-		err := rc.Apply(&productcatalog.FlatFeeRateCard{
-			RateCardMeta: meta,
-		})
-		require.Error(t, err)
-		require.ErrorContains(t, err, "target and addon rate card keys do not match")
-	})
-
-	t.Run("Should not change target RateCard if addon RateCard has no entitlement & no price", func(t *testing.T) {
-		meta := someMeta.Clone()
-		meta.EntitlementTemplate = nil
-		meta.Price = nil
-
-		rc := getTestAddonRateCard(&productcatalog.FlatFeeRateCard{
-			RateCardMeta: meta,
+		t.Run("Apply", func(t *testing.T) {
+			err := rc.Apply(&productcatalog.FlatFeeRateCard{
+				RateCardMeta: meta,
+			})
+			require.Error(t, err)
+			require.ErrorContains(t, err, "target and addon rate card keys do not match")
 		})
 
-		target := &productcatalog.FlatFeeRateCard{
-			RateCardMeta: someMeta.Clone(),
-		}
-
-		targetClone := target.Clone()
-
-		err := rc.Apply(target)
-
-		require.NoError(t, err)
-		require.Equal(t, targetClone, target)
+		t.Run("Restore", func(t *testing.T) {
+			err := rc.Restore(&productcatalog.FlatFeeRateCard{
+				RateCardMeta: meta,
+			})
+			require.Error(t, err)
+			require.ErrorContains(t, err, "target and addon rate card keys do not match")
+		})
 	})
 
 	t.Run("Should error if target Price has different payment term", func(t *testing.T) {
@@ -138,10 +165,53 @@ func TestExtendApply(t *testing.T) {
 			RateCardMeta: meta.Clone(),
 		}
 
+		t.Run("Apply", func(t *testing.T) {
+			err := rc.Apply(target)
+
+			require.Error(t, err)
+			require.ErrorContains(t, err, "target and addon rate card price payment terms do not match")
+		})
+
+		t.Run("Restore", func(t *testing.T) {
+			err := rc.Restore(target)
+
+			require.Error(t, err)
+			require.ErrorContains(t, err, "target and addon rate card price payment terms do not match")
+		})
+	})
+}
+
+func TestExtendApply(t *testing.T) {
+	someMeta := productcatalog.RateCardMeta{
+		Name:        "Test Addon Rate Card 4",
+		Description: lo.ToPtr("Test Addon Rate Card 4 Description"),
+		Key:         subscriptiontestutils.ExampleFeatureKey,
+		FeatureKey:  lo.ToPtr(subscriptiontestutils.ExampleFeatureKey),
+		Price: productcatalog.NewPriceFrom(productcatalog.FlatPrice{
+			Amount:      alpacadecimal.NewFromInt(100),
+			PaymentTerm: productcatalog.InAdvancePaymentTerm,
+		}),
+	}
+
+	t.Run("Should not change target RateCard if addon RateCard has no entitlement & no price", func(t *testing.T) {
+		meta := someMeta.Clone()
+		meta.EntitlementTemplate = nil
+		meta.Price = nil
+
+		rc := getTestAddonRateCard(&productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta,
+		})
+
+		target := &productcatalog.FlatFeeRateCard{
+			RateCardMeta: someMeta.Clone(),
+		}
+
+		targetClone := target.Clone()
+
 		err := rc.Apply(target)
 
-		require.Error(t, err)
-		require.ErrorContains(t, err, "target and addon rate card price payment terms do not match")
+		require.NoError(t, err)
+		require.Equal(t, targetClone, target)
 	})
 
 	t.Run("Should keep FlatPrice of target if addon has no price", func(t *testing.T) {
@@ -319,6 +389,223 @@ func TestExtendApply(t *testing.T) {
 		require.NoError(t, err)
 		require.NotNil(t, me.IssueAfterReset)
 		require.Equal(t, 200.0, *me.IssueAfterReset)
+	})
+}
+
+func TestExtendRestore(t *testing.T) {
+	someMeta := productcatalog.RateCardMeta{
+		Name:        "Test Addon Rate Card 4",
+		Description: lo.ToPtr("Test Addon Rate Card 4 Description"),
+		Key:         subscriptiontestutils.ExampleFeatureKey,
+		FeatureKey:  lo.ToPtr(subscriptiontestutils.ExampleFeatureKey),
+		Price: productcatalog.NewPriceFrom(productcatalog.FlatPrice{
+			Amount:      alpacadecimal.NewFromInt(100),
+			PaymentTerm: productcatalog.InAdvancePaymentTerm,
+		}),
+	}
+
+	t.Run("Should not change target RateCard if addon RateCard has no entitlement & no price", func(t *testing.T) {
+		meta := someMeta.Clone()
+		meta.EntitlementTemplate = nil
+		meta.Price = nil
+
+		rc := getTestAddonRateCard(&productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta,
+		})
+
+		target := &productcatalog.FlatFeeRateCard{
+			RateCardMeta: someMeta.Clone(),
+		}
+
+		targetClone := target.Clone()
+
+		err := rc.Restore(target)
+
+		require.NoError(t, err)
+		require.Equal(t, targetClone, target)
+	})
+
+	t.Run("Should error if target Price is nil but addon has a flat price", func(t *testing.T) {
+		meta := someMeta.Clone()
+		meta.EntitlementTemplate = nil
+
+		rc := getTestAddonRateCard(&productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta.Clone(),
+		})
+
+		meta.Price = nil
+
+		target := &productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta.Clone(),
+		}
+
+		err := rc.Restore(target)
+
+		require.Error(t, err)
+		require.ErrorContains(t, err, "target price is nil, cannot restore price without addon")
+	})
+
+	t.Run("Should deduct flat price of addon from target", func(t *testing.T) {
+		meta := someMeta.Clone()
+		meta.EntitlementTemplate = nil
+		meta.Price = productcatalog.NewPriceFrom(productcatalog.FlatPrice{
+			Amount:      alpacadecimal.NewFromInt(10),
+			PaymentTerm: productcatalog.InAdvancePaymentTerm,
+		})
+
+		rc := getTestAddonRateCard(&productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta.Clone(),
+		})
+
+		target := &productcatalog.FlatFeeRateCard{
+			RateCardMeta: someMeta.Clone(),
+		}
+
+		err := rc.Restore(target)
+
+		require.NoError(t, err)
+		fp, err := target.AsMeta().Price.AsFlat()
+		require.NoError(t, err)
+		require.Equal(t, alpacadecimal.NewFromInt(90), fp.Amount)
+	})
+
+	t.Run("Should allow 0 resulting flat price", func(t *testing.T) {
+		meta := someMeta.Clone()
+		meta.EntitlementTemplate = nil
+
+		rc := getTestAddonRateCard(&productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta.Clone(),
+		})
+
+		target := &productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta.Clone(),
+		}
+
+		err := rc.Restore(target)
+
+		require.NoError(t, err)
+		fp, err := target.AsMeta().Price.AsFlat()
+		require.NoError(t, err)
+		require.Equal(t, alpacadecimal.NewFromInt(0), fp.Amount)
+	})
+
+	t.Run("Should error if target has no entitlement but addon has a metered entitlement", func(t *testing.T) {
+		meta := someMeta.Clone()
+		meta.EntitlementTemplate = productcatalog.NewEntitlementTemplateFrom(productcatalog.MeteredEntitlementTemplate{
+			IssueAfterReset: lo.ToPtr(100.0),
+			UsagePeriod:     testutils.GetISODuration(t, "P1M"),
+		})
+
+		rc := getTestAddonRateCard(&productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta.Clone(),
+		})
+
+		meta.EntitlementTemplate = nil
+
+		target := &productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta.Clone(),
+		}
+
+		err := rc.Restore(target)
+
+		require.Error(t, err)
+		require.ErrorContains(t, err, "target entitlement template is nil, cannot restore entitlement template without addon")
+	})
+
+	// TODO: figure this out, we have a data loss situation here
+	t.Run("Should return NOT IMPLEMENTED when trying to restore a boolean entitlement template", func(t *testing.T) {
+		meta := someMeta.Clone()
+		meta.EntitlementTemplate = productcatalog.NewEntitlementTemplateFrom(productcatalog.BooleanEntitlementTemplate{})
+
+		rc := getTestAddonRateCard(&productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta.Clone(),
+		})
+
+		target := &productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta.Clone(),
+		}
+
+		err := rc.Restore(target)
+
+		require.Error(t, err)
+		require.ErrorAs(t, err, lo.ToPtr(&models.GenericNotImplementedError{}))
+	})
+
+	t.Run("Should deduct issueAfterReset of addon from target", func(t *testing.T) {
+		meta := someMeta.Clone()
+		meta.EntitlementTemplate = productcatalog.NewEntitlementTemplateFrom(productcatalog.MeteredEntitlementTemplate{
+			IssueAfterReset: lo.ToPtr(10.0),
+			UsagePeriod:     testutils.GetISODuration(t, "P1M"),
+		})
+
+		rc := getTestAddonRateCard(&productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta.Clone(),
+		})
+
+		meta.EntitlementTemplate = productcatalog.NewEntitlementTemplateFrom(productcatalog.MeteredEntitlementTemplate{
+			IssueAfterReset: lo.ToPtr(100.0),
+			UsagePeriod:     testutils.GetISODuration(t, "P1M"),
+		})
+
+		target := &productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta.Clone(),
+		}
+
+		err := rc.Restore(target)
+
+		require.NoError(t, err)
+		me, err := target.AsMeta().EntitlementTemplate.AsMetered()
+		require.NoError(t, err)
+		require.Equal(t, 90.0, *me.IssueAfterReset)
+	})
+
+	t.Run("Should not allow negative issueAfterReset", func(t *testing.T) {
+		meta := someMeta.Clone()
+		meta.EntitlementTemplate = productcatalog.NewEntitlementTemplateFrom(productcatalog.MeteredEntitlementTemplate{
+			IssueAfterReset: lo.ToPtr(100.0),
+			UsagePeriod:     testutils.GetISODuration(t, "P1M"),
+		})
+
+		rc := getTestAddonRateCard(&productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta.Clone(),
+		})
+
+		meta.EntitlementTemplate = productcatalog.NewEntitlementTemplateFrom(productcatalog.MeteredEntitlementTemplate{
+			IssueAfterReset: lo.ToPtr(50.0),
+			UsagePeriod:     testutils.GetISODuration(t, "P1M"),
+		})
+
+		target := &productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta.Clone(),
+		}
+
+		err := rc.Restore(target)
+
+		require.Error(t, err)
+		require.ErrorContains(t, err, "restoring entitlement template would yield a negative issue after reset: 50 - 100 = -50")
+	})
+
+	t.Run("Should allow 0 resulting issueAfterReset", func(t *testing.T) {
+		meta := someMeta.Clone()
+		meta.EntitlementTemplate = productcatalog.NewEntitlementTemplateFrom(productcatalog.MeteredEntitlementTemplate{
+			IssueAfterReset: lo.ToPtr(100.0),
+			UsagePeriod:     testutils.GetISODuration(t, "P1M"),
+		})
+
+		rc := getTestAddonRateCard(&productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta.Clone(),
+		})
+
+		target := &productcatalog.FlatFeeRateCard{
+			RateCardMeta: meta.Clone(),
+		}
+
+		err := rc.Restore(target)
+
+		require.NoError(t, err)
+		me, err := target.AsMeta().EntitlementTemplate.AsMetered()
+		require.NoError(t, err)
+		require.Equal(t, 0.0, *me.IssueAfterReset)
 	})
 }
 
