3516ced50d70fc81c74ebe0132b1168ff8e0c19d
Peter Turi <peter.turi@openmeter.io>
2025-04-25 21:49:03 +0200

    fix: invoice app serialization

--- openmeter/billing/events.go
+++ openmeter/billing/events.go
@@ -1,8 +1,10 @@
 package billing
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/openmeterio/openmeter/openmeter/app"
 	"github.com/openmeterio/openmeter/openmeter/event/metadata"
 )
 
@@ -10,7 +12,48 @@ const (
 	EventSubsystem metadata.EventSubsystem = "billing"
 )
 
-type EventInvoice Invoice
+type InvoiceAppBases struct {
+	Tax      app.AppBase `json:"tax"`
+	Payment  app.AppBase `json:"payment"`
+	Invocing app.AppBase `json:"invocing"`
+}
+
+func (a InvoiceAppBases) Validate() error {
+	var errs []error
+
+	if err := a.Tax.Validate(); err != nil {
+		errs = append(errs, fmt.Errorf("tax: %w", err))
+	}
+
+	if err := a.Payment.Validate(); err != nil {
+		errs = append(errs, fmt.Errorf("payment: %w", err))
+	}
+
+	if err := a.Invocing.Validate(); err != nil {
+		errs = append(errs, fmt.Errorf("invocing: %w", err))
+	}
+
+	return errors.Join(errs...)
+}
+
+type EventInvoice struct {
+	Invoice  Invoice         `json:"invoice"`
+	AppBases InvoiceAppBases `json:"app_bases,omitempty"`
+}
+
+func (e EventInvoice) Validate() error {
+	var errs []error
+
+	if err := e.Invoice.Validate(); err != nil {
+		errs = append(errs, err)
+	}
+
+	if err := e.AppBases.Validate(); err != nil {
+		errs = append(errs, err)
+	}
+
+	return errors.Join(errs...)
+}
 
 func NewEventInvoice(invoice Invoice) EventInvoice {
 	// This causes a stack overflow
@@ -21,7 +64,20 @@ func NewEventInvoice(invoice Invoice) EventInvoice {
 	// an up-to-date app based on the payload.Workflow.AppReferences
 	payload.Workflow.Apps = nil
 
-	return EventInvoice(payload)
+	appBases := InvoiceAppBases{}
+	// TODO[later]: Apps are always present, so we should only use the struct without a pointer
+	if invoice.Workflow.Apps != nil {
+		appBases = InvoiceAppBases{
+			Tax:      invoice.Workflow.Apps.Tax.GetAppBase(),
+			Payment:  invoice.Workflow.Apps.Payment.GetAppBase(),
+			Invocing: invoice.Workflow.Apps.Invoicing.GetAppBase(),
+		}
+	}
+
+	return EventInvoice{
+		Invoice:  payload,
+		AppBases: appBases,
+	}
 }
 
 type InvoiceCreatedEvent struct {
@@ -36,14 +92,14 @@ func (e InvoiceCreatedEvent) EventName() string {
 	return metadata.GetEventName(metadata.EventType{
 		Subsystem: EventSubsystem,
 		Name:      "invoice.created",
-		Version:   "v1",
+		Version:   "v2",
 	})
 }
 
 func (e InvoiceCreatedEvent) EventMetadata() metadata.EventMetadata {
 	return metadata.EventMetadata{
-		Source:  metadata.ComposeResourcePath(e.Namespace, metadata.EntityInvoice, e.ID),
-		Subject: metadata.ComposeResourcePath(e.Namespace, metadata.EntityCustomer, e.Customer.CustomerID),
+		Source:  metadata.ComposeResourcePath(e.Invoice.Namespace, metadata.EntityInvoice, e.Invoice.ID),
+		Subject: metadata.ComposeResourcePath(e.Invoice.Namespace, metadata.EntityCustomer, e.Invoice.Customer.CustomerID),
 	}
 }
 
