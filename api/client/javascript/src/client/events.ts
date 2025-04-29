18db0816e39c0ad4f02a83c9f5a58cd90411aa23
Andras Toth <4157749+tothandras@users.noreply.github.com>
2025-04-29 13:35:19 +0200

    feat(client): remove crypto dependency from Node SDK

--- api/client/javascript/src/client/events.ts
+++ api/client/javascript/src/client/events.ts
@@ -1,4 +1,3 @@
-import crypto from 'crypto'
 import { transformResponse } from './utils.js'
 import type { RequestOptions } from './common.js'
 import type { operations, paths, Event } from './schemas.js'
@@ -18,8 +17,8 @@ export class Events {
    * @returns The ingested events
    */
   public async ingest(events: Event | Event[], options?: RequestOptions) {
-    const body = (Array.isArray(events) ? events : [events]).map(
-      setDefaultsForEvent
+    const body = await Promise.all(
+      (Array.isArray(events) ? events : [events]).map(setDefaultsForEvent)
     )
 
     const resp = await this.client.POST('/api/v1/events', {
@@ -54,16 +53,62 @@ export class Events {
   }
 }
 
-export function setDefaultsForEvent(ev: Event): Event {
+/**
+ * Sets the defaults for an event
+ * @param ev - The event to set the defaults for
+ * @returns The event with the defaults set
+ */
+export async function setDefaultsForEvent(ev: Event): Promise<Event> {
   return {
-    data: ev.data,
-    datacontenttype: ev.datacontenttype,
-    dataschema: ev.dataschema,
-    id: ev.id ?? crypto.randomUUID(),
+    ...ev,
+    id: ev.id ?? (await generateId()),
     source: ev.source ?? '@openmeter/sdk',
     specversion: ev.specversion ?? '1.0',
-    subject: ev.subject,
-    time: ev.time,
-    type: ev.type,
+    time: ev.time ?? new Date(),
   }
+}
+
+let _randomUUID: (() => string) | undefined
+
+// One-off attempt to load node:crypto and capture randomUUID (if present)
+async function loadUUIDProvider() {
+  if (_randomUUID !== undefined) {
+    // already tried
+    return _randomUUID
+  }
+
+  try {
+    const c = await import('node:crypto')
+    if (typeof c.randomUUID === 'function') {
+      // available
+      _randomUUID = c.randomUUID.bind(c)
+    }
+  } catch {
+    // not-available
+  }
+
+  return _randomUUID
+}
+
+/**
+ * Generates a random ID
+ * @returns A random ID
+ */
+async function generateId() {
+  const randomUUID = await loadUUIDProvider()
+  if (randomUUID) {
+    return randomUUID()
+  }
+
+  // Fallback to semi-random ID
+  const bytes = new Uint8Array(16)
+  for (let i = 0; i < 16; i++) {
+    bytes[i] = (Math.random() * 256) | 0
+  }
+
+  bytes[6] = (bytes[6] & 0x0f) | 0x40
+  bytes[8] = (bytes[8] & 0x3f) | 0x80
+
+  const hex = [...bytes].map((b) => b.toString(16).padStart(2, '0')).join('')
+  return `${hex.slice(0, 8)}-${hex.slice(8, 12)}-${hex.slice(12, 16)}-${hex.slice(16, 20)}-${hex.slice(20)}`
 }
