06ee09a0328e8e56cf5b84250e65345ddbe7c578
Peter Turi <peter.turi@openmeter.io>
2025-04-30 16:02:58 +0200

    refactor: migrate to use hashes for dedupe key
    This allows us to have 57% savings in redis memory footprint.

--- app/config/dedupe.go
+++ app/config/dedupe.go
@@ -164,6 +164,7 @@ type DedupeDriverRedisConfiguration struct {
 	redis.Config `mapstructure:",squash"`
 
 	Expiration time.Duration
+	Mode       redisdedupe.DedupeMode
 }
 
 func (DedupeDriverRedisConfiguration) DriverName() string {
@@ -180,6 +181,7 @@ func (c DedupeDriverRedisConfiguration) NewDeduplicator() (dedupe.Deduplicator, 
 	return redisdedupe.Deduplicator{
 		Redis:      redisClient,
 		Expiration: c.Expiration,
+		Mode:       c.Mode,
 	}, nil
 }
 
@@ -188,6 +190,10 @@ func (c DedupeDriverRedisConfiguration) Validate() error {
 
 	if err := c.Config.Validate(); err != nil {
 		errs = append(errs, errorsx.WithPrefix(err, "redis"))
+	}
+
+	if err := c.Mode.Validate(); err != nil {
+		errs = append(errs, errorsx.WithPrefix(err, "mode"))
 	}
 
 	return errors.Join(errs...)
