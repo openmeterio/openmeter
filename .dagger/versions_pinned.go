289649e150fa617f5a8613b127a166081091ca84
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2025-05-08 11:56:55 +0200

    chore(deps): bump Go version to 1.24.1 (#2788)

--- .dagger/versions_pinned.go
+++ .dagger/versions_pinned.go
@@ -1,9 +1,6 @@
 package main
 
 const (
-	// Alpine is required for our current build (due to Kafka and CGO), but it doesn't seem to work well with golangci-lint
-	goVersion = "1.23.4"
-
 	kafkaVersion      = "3.6"
 	clickhouseVersion = "24.5.5.78"
 	redisVersion      = "7.0.12"
@@ -13,8 +10,8 @@ const (
 	// TODO: add update mechanism for versions below
 
 	// Alpine is required for our current build (due to Kafka and CGO), but it doesn't seem to work well with golangci-lint
-	goBuildVersion = "1.23.4-alpine3.21@sha256:6c5c9590f169f77c8046e45c611d3b28fe477789acd8d3762d23d4744de69812"
+	goBuildVersion = "1.24.2-alpine3.21@sha256:7772cb5322baa875edd74705556d08f0eeca7b9c4b5367754ce3f2f00041ccee"
 	xxBaseImage    = "tonistiigi/xx:1.6.1@sha256:923441d7c25f1e2eb5789f82d987693c47b8ed987c4ab3b075d6ed2b5d6779a3"
 
-	alpineBaseImage = "alpine:3.21.0@sha256:21dc6063fd678b478f57c0e13f47560d0ea4eeba26dfc947b2a4f81f686b9f45"
+	alpineBaseImage = "alpine:3.21@sha256:a8560b36e8b8210634f77d9f7f9efd7ffa463e380b75e2e74aff4511df3ef88c"
 )
