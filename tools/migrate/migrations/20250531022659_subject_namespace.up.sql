b95060f64dfb102a0936c1c89a0253126f6879dd
Peter Marton <peter@openmeter.io>
2025-05-30 20:23:25 -0700

    fix(subject): migrations

--- tools/migrate/migrations/20250531022659_subject_namespace.up.sql
+++ tools/migrate/migrations/20250531022659_subject_namespace.up.sql
@@ -4,12 +4,11 @@ DROP INDEX "subject_key_organization_subjects";
 -- modify "subjects" table
 -- note that alter table doesn't accept multiple column changes with renaming
 -- atlas:nolint BC102
-ALTER TABLE
-    "subjects" RENAME COLUMN "organization_subjects" TO "namespace";
+ALTER TABLE "subjects" RENAME COLUMN "organization_subjects" TO "namespace";
 
 -- atlas:nolint DS103 BC102
-ALTER TABLE
-    "subjects" DROP COLUMN "current_period_start",
+ALTER TABLE "subjects"
+    DROP COLUMN "current_period_start",
     DROP COLUMN "current_period_end";
 
 -- create index "subject_namespace" to table: "subjects"
