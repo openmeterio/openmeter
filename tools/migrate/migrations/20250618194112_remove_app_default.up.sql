eba3f0bdbed5bda987b6ffd3716e449bc0d7086e
Peter Marton <peter@openmeter.io>
2025-06-18 13:07:21 -0700

    fix(migration): up and down

--- tools/migrate/migrations/20250618194112_remove_app_default.up.sql
+++ tools/migrate/migrations/20250618194112_remove_app_default.up.sql
@@ -1,2 +1,7 @@
+-- This migration removes the "is_default" column from the "apps" table
+
+-- drop index "app_namespace_type_is_default" from table: "apps"
+DROP INDEX "app_namespace_type_is_default";
+
 -- modify "apps" table
 ALTER TABLE "apps" DROP COLUMN "is_default";
