57e698d102c659a8920321005a43a06dd4c1853f
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2025-07-07 10:07:13 +0200

    chore: bump librdkafka in nix env

--- flake.nix
+++ flake.nix
@@ -75,14 +75,14 @@
               # Check actual version via:
               # $ pkg-config --modversion rdkafka++
               # Getting sha256 hash for git ref:
-              # $ nix-shell -p nix-prefetch-git jq --run "nix hash convert sha256:\$(nix-prefetch-git --url https://github.com/confluentinc/librdkafka.git --quiet --rev v2.10.1 | jq -r '.sha256')"
+              # $ nix-shell -p nix-prefetch-git jq --run "nix hash convert sha256:\$(nix-prefetch-git --url https://github.com/confluentinc/librdkafka.git --quiet --rev v2.11.0 | jq -r '.sha256')"
               (rdkafka.overrideAttrs (_: rec {
-                version = "2.10.1";
+                version = "2.11.0";
                 src = fetchFromGitHub {
                   owner = "confluentinc";
                   repo = "librdkafka";
                   rev = "v${version}";
-                  sha256 = "sha256-+ACn+1fjWEnUB32gUCoMpnq+6YBu+rufPT8LY920DBk=";
+                  sha256 = "sha256-37lCQ+CFeTRQwL6FCl79RSGw+nRKr0DeuXob9CjiVnk=";
                 };
               }))
 
