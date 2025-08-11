3bd379645e5f1966e748c4230b188aea01cb561c
Peter Turi <peter.turi@openmeter.io>
2025-08-11 14:10:38 +0200

    fix: remove test, fix golangcilint version missmatch

--- .dagger/test.go
+++ .dagger/test.go
@@ -6,29 +6,6 @@ import (
 	"github.com/openmeterio/openmeter/.dagger/internal/dagger"
 )
 
-func (m *Openmeter) Test() *dagger.Container {
-	// TODO: customize user and password
-	postgres := dag.Postgres(dagger.PostgresOpts{
-		Version: postgresVersion,
-	}).WithDatabase("svix")
-
-	svix := dag.Svix(dagger.SvixOpts{
-		Version:   svixVersion,
-		Postgres:  postgres.AsSvixPostgres(),
-		Database:  "svix",
-		JwtSecret: dag.SetSecret("svix-jwt-secret", SvixJWTSingingSecret),
-	})
-
-	return goModuleCross("").
-		WithSource(m.Source).
-		WithEnvVariable("POSTGRES_HOST", "postgres").
-		WithEnvVariable("SVIX_HOST", "svix").
-		WithEnvVariable("SVIX_JWT_SECRET", SvixJWTSingingSecret).
-		WithServiceBinding("postgres", postgres.Service()).
-		WithServiceBinding("svix", svix.Service()).
-		Exec([]string{"go", "run", "-mod=mod", "gotest.tools/gotestsum", "--", "-tags", "musl", "-v", "./..."})
-}
-
 func (m *Openmeter) QuickstartTest(
 	service *dagger.Service,
 
