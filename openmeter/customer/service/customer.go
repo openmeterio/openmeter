ed9d7ad497a6589f0dbdef1821e8b81a5f0e1adc
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2025-08-29 10:16:18 +0200

    refactor: tx handling customer service

--- openmeter/customer/service/customer.go
+++ openmeter/customer/service/customer.go
@@ -14,7 +14,9 @@ var _ customer.Service = (*Service)(nil)
 
 // ListCustomers lists customers
 func (s *Service) ListCustomers(ctx context.Context, input customer.ListCustomersInput) (pagination.PagedResponse[customer.Customer], error) {
-	return s.adapter.ListCustomers(ctx, input)
+	return transaction.Run(ctx, s.adapter, func(ctx context.Context) (pagination.PagedResponse[customer.Customer], error) {
+		return s.adapter.ListCustomers(ctx, input)
+	})
 }
 
 // CreateCustomer creates a customer
@@ -24,19 +26,21 @@ func (s *Service) CreateCustomer(ctx context.Context, input customer.CreateCusto
 		return nil, models.NewGenericValidationError(err)
 	}
 
-	// Create the customer
-	createdCustomer, err := s.adapter.CreateCustomer(ctx, input)
-	if err != nil {
-		return nil, err
-	}
+	return transaction.Run(ctx, s.adapter, func(ctx context.Context) (*customer.Customer, error) {
+		// Create the customer
+		createdCustomer, err := s.adapter.CreateCustomer(ctx, input)
+		if err != nil {
+			return nil, err
+		}
 
-	// Publish the customer created event
-	customerCreatedEvent := customer.NewCustomerCreateEvent(ctx, createdCustomer)
-	if err := s.publisher.Publish(ctx, customerCreatedEvent); err != nil {
-		return nil, fmt.Errorf("failed to publish customer created event: %w", err)
-	}
+		// Publish the customer created event
+		customerCreatedEvent := customer.NewCustomerCreateEvent(ctx, createdCustomer)
+		if err := s.publisher.Publish(ctx, customerCreatedEvent); err != nil {
+			return nil, fmt.Errorf("failed to publish customer created event: %w", err)
+		}
 
-	return createdCustomer, nil
+		return createdCustomer, nil
+	})
 }
 
 // DeleteCustomer deletes a customer
@@ -46,40 +50,46 @@ func (s *Service) DeleteCustomer(ctx context.Context, input customer.DeleteCusto
 		return models.NewGenericValidationError(err)
 	}
 
-	// Delete the customer
-	err := s.adapter.DeleteCustomer(ctx, input)
-	if err != nil {
-		return err
-	}
+	return transaction.RunWithNoValue(ctx, s.adapter, func(ctx context.Context) error {
+		// Delete the customer
+		err := s.adapter.DeleteCustomer(ctx, input)
+		if err != nil {
+			return err
+		}
 
-	// Get the deleted customer
-	deletedCustomer, err := s.GetCustomer(ctx, customer.GetCustomerInput{
-		CustomerID: &customer.CustomerID{
-			Namespace: input.Namespace,
-			ID:        input.ID,
-		},
+		// Get the deleted customer
+		deletedCustomer, err := s.GetCustomer(ctx, customer.GetCustomerInput{
+			CustomerID: &customer.CustomerID{
+				Namespace: input.Namespace,
+				ID:        input.ID,
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// Publish the customer deleted event
+		customerDeletedEvent := customer.NewCustomerDeleteEvent(ctx, deletedCustomer)
+		if err := s.publisher.Publish(ctx, customerDeletedEvent); err != nil {
+			return fmt.Errorf("failed to publish customer deleted event: %w", err)
+		}
+
+		return nil
 	})
-	if err != nil {
-		return err
-	}
-
-	// Publish the customer deleted event
-	customerDeletedEvent := customer.NewCustomerDeleteEvent(ctx, deletedCustomer)
-	if err := s.publisher.Publish(ctx, customerDeletedEvent); err != nil {
-		return fmt.Errorf("failed to publish customer deleted event: %w", err)
-	}
-
-	return nil
 }
 
 // GetCustomer gets a customer
 func (s *Service) GetCustomer(ctx context.Context, input customer.GetCustomerInput) (*customer.Customer, error) {
-	return s.adapter.GetCustomer(ctx, input)
+	return transaction.Run(ctx, s.adapter, func(ctx context.Context) (*customer.Customer, error) {
+		return s.adapter.GetCustomer(ctx, input)
+	})
 }
 
 // GetCustomerByUsageAttribution gets a customer by usage attribution
 func (s *Service) GetCustomerByUsageAttribution(ctx context.Context, input customer.GetCustomerByUsageAttributionInput) (*customer.Customer, error) {
-	return s.adapter.GetCustomerByUsageAttribution(ctx, input)
+	return transaction.Run(ctx, s.adapter, func(ctx context.Context) (*customer.Customer, error) {
+		return s.adapter.GetCustomerByUsageAttribution(ctx, input)
+	})
 }
 
 // UpdateCustomer updates a customer
@@ -89,19 +99,21 @@ func (s *Service) UpdateCustomer(ctx context.Context, input customer.UpdateCusto
 		return nil, models.NewGenericValidationError(err)
 	}
 
-	// Update the customer
-	updatedCustomer, err := s.adapter.UpdateCustomer(ctx, input)
-	if err != nil {
-		return nil, err
-	}
+	return transaction.Run(ctx, s.adapter, func(ctx context.Context) (*customer.Customer, error) {
+		// Update the customer
+		updatedCustomer, err := s.adapter.UpdateCustomer(ctx, input)
+		if err != nil {
+			return nil, err
+		}
 
-	// Publish the customer updated event
-	customerUpdatedEvent := customer.NewCustomerUpdateEvent(ctx, updatedCustomer)
-	if err := s.publisher.Publish(ctx, customerUpdatedEvent); err != nil {
-		return nil, fmt.Errorf("failed to publish customer updated event: %w", err)
-	}
+		// Publish the customer updated event
+		customerUpdatedEvent := customer.NewCustomerUpdateEvent(ctx, updatedCustomer)
+		if err := s.publisher.Publish(ctx, customerUpdatedEvent); err != nil {
+			return nil, fmt.Errorf("failed to publish customer updated event: %w", err)
+		}
 
-	return updatedCustomer, nil
+		return updatedCustomer, nil
+	})
 }
 
 // CustomerExists checks if a customer exists
