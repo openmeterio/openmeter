bdc71011de485ee65a55be87eef9feafe52ed8f9
Peter Turi <peter.turi@openmeter.io>
2025-09-07 08:46:30 +0200

    chore: fix missing nil check

--- openmeter/notification/httpdriver/mapping.go
+++ openmeter/notification/httpdriver/mapping.go
@@ -546,6 +546,11 @@ func FromEventAsEntitlementResetPayload(e notification.Event) (api.NotificationE
 		return api.NotificationEventResetPayload{}, fmt.Errorf("entitlement reset is nil")
 	}
 
+	var customer *api.Customer
+	if e.Payload.BalanceThreshold != nil {
+		customer = lo.ToPtr(e.Payload.BalanceThreshold.Customer)
+	}
+
 	return api.NotificationEventResetPayload{
 		Id:        e.ID,
 		Timestamp: e.CreatedAt,
@@ -555,7 +560,7 @@ func FromEventAsEntitlementResetPayload(e notification.Event) (api.NotificationE
 			Entitlement: e.Payload.EntitlementReset.Entitlement,
 			Feature:     e.Payload.EntitlementReset.Feature,
 			Subject:     e.Payload.EntitlementReset.Subject,
-			Customer:    lo.ToPtr(e.Payload.BalanceThreshold.Customer),
+			Customer:    customer,
 		},
 	}, nil
 }
