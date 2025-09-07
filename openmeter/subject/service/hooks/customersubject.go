763290492961c41f82838d2aadc69384cfecd0fb
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2025-09-07 10:18:45 +0200

    feat: add tracing to customer subject hook

--- openmeter/subject/service/hooks/customersubject.go
+++ openmeter/subject/service/hooks/customersubject.go
@@ -7,6 +7,9 @@ import (
 	"log/slog"
 
 	"github.com/samber/lo"
+	"go.opentelemetry.io/otel/attribute"
+	otelcodes "go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
 
 	"github.com/openmeterio/openmeter/openmeter/customer"
 	"github.com/openmeterio/openmeter/openmeter/subject"
@@ -42,14 +45,37 @@ type customerSubjectHook struct {
 
 	provisioner *SubjectProvisioner
 	logger      *slog.Logger
+	tracer      trace.Tracer
 }
 
 func (s customerSubjectHook) PostCreate(ctx context.Context, cus *customer.Customer) error {
-	return s.provisioner.EnsureSubjects(ctx, cus)
+	ctx, span := s.tracer.Start(ctx, "customer_subject_hook.post_create")
+	defer span.End()
+
+	err := s.provisioner.EnsureSubjects(ctx, cus)
+	if err != nil {
+		span.SetStatus(otelcodes.Error, "failed to provision subjects for customer")
+		span.RecordError(err)
+	} else {
+		span.SetStatus(otelcodes.Ok, "subjects provisioned for customer")
+	}
+
+	return err
 }
 
 func (s customerSubjectHook) PostUpdate(ctx context.Context, cus *customer.Customer) error {
-	return s.provisioner.EnsureSubjects(ctx, cus)
+	ctx, span := s.tracer.Start(ctx, "customer_subject_hook.post_update")
+	defer span.End()
+
+	err := s.provisioner.EnsureSubjects(ctx, cus)
+	if err != nil {
+		span.SetStatus(otelcodes.Error, "failed to provision subjects for customer")
+		span.RecordError(err)
+	} else {
+		span.SetStatus(otelcodes.Ok, "subjects provisioned for customer")
+	}
+
+	return err
 }
 
 func NewCustomerSubjectHook(config CustomerSubjectHookConfig) (CustomerSubjectHook, error) {
@@ -65,6 +91,7 @@ func NewCustomerSubjectHook(config CustomerSubjectHookConfig) (CustomerSubjectHo
 	return &customerSubjectHook{
 		provisioner: provisioner,
 		logger:      config.Logger.With("subsystem", "subject_customer_provisioner"),
+		tracer:      config.Tracer,
 	}, nil
 }
 
@@ -75,6 +102,7 @@ var _ models.Validator = (*SubjectProvisionerConfig)(nil)
 type SubjectProvisionerConfig struct {
 	Subject subject.Service
 	Logger  *slog.Logger
+	Tracer  trace.Tracer
 }
 
 func (c SubjectProvisionerConfig) Validate() error {
@@ -88,6 +116,10 @@ func (c SubjectProvisionerConfig) Validate() error {
 		errs = append(errs, fmt.Errorf("logger is required"))
 	}
 
+	if c.Tracer == nil {
+		errs = append(errs, fmt.Errorf("tracer is required"))
+	}
+
 	return errors.Join(errs...)
 }
 
@@ -99,12 +131,14 @@ func NewSubjectProvisioner(config SubjectProvisionerConfig) (*SubjectProvisioner
 	return &SubjectProvisioner{
 		subject: config.Subject,
 		logger:  config.Logger.With("subsystem", "subject.provisioner"),
+		tracer:  config.Tracer,
 	}, nil
 }
 
 type SubjectProvisioner struct {
 	subject subject.Service
 	logger  *slog.Logger
+	tracer  trace.Tracer
 }
 
 // EnsureSubjects ensures that Subjects are provisioned for each usage attribution entry.
@@ -117,35 +151,31 @@ func (p SubjectProvisioner) EnsureSubjects(ctx context.Context, cus *customer.Cu
 		return errors.New("failed to provision subject for customer: customer is nil")
 	}
 
+	var err error
+
+	ctx, span := p.tracer.Start(ctx, "subject_provisioner.ensure_subjects")
+	defer func() {
+		if err != nil {
+			span.SetStatus(otelcodes.Error, err.Error())
+			span.RecordError(err)
+		} else {
+			span.SetStatus(otelcodes.Ok, "subjects provisioned for customer")
+		}
+
+		span.End()
+	}()
+
+	span.SetAttributes(
+		attribute.String("customer.id", cus.ID),
+		attribute.String("customer.key", lo.FromPtrOr(cus.Key, "nil")),
+	)
+
 	var errs []error
 
 	for _, subKey := range cus.UsageAttribution.SubjectKeys {
-		// Check if the subject exists
-		sub, err := p.subject.GetByIdOrKey(ctx, cus.Namespace, subKey)
+		sub, err := p.EnsureSubject(ctx, cus, subKey)
 		if err != nil {
-			if models.IsGenericNotFoundError(err) {
-				// Create Subject if it does not exist
-				_, err = p.subject.Create(NewContextWithSkipSubjectCustomer(ctx),
-					subject.CreateInput{
-						Namespace: cus.Namespace,
-						Key:       subKey,
-						Metadata: lo.ToPtr(map[string]interface{}{
-							"createdBy":  "subject.provisioner",
-							"customerId": cus.ID,
-						}),
-					})
-				if err != nil {
-					errs = append(errs,
-						fmt.Errorf("failed to create subject for customer [namespace=%s customer.id=%s customer.usage_attribution_key: %s]: %w",
-							cus.Namespace, cus.ID, subKey, err),
-					)
-				}
-
-				continue
-			}
-
 			errs = append(errs, err)
-
 			continue
 		}
 
@@ -159,5 +189,69 @@ func (p SubjectProvisioner) EnsureSubjects(ctx context.Context, cus *customer.Cu
 		}
 	}
 
-	return errors.Join(errs...)
+	err = errors.Join(errs...)
+
+	return err
+}
+
+func (p SubjectProvisioner) EnsureSubject(ctx context.Context, cus *customer.Customer, subjectKey string) (*subject.Subject, error) {
+	if cus == nil {
+		return nil, errors.New("failed to provision subject for customer: customer is nil")
+	}
+
+	var err error
+
+	ctx, span := p.tracer.Start(ctx, "subject_provisioner.ensure_subject")
+	defer func() {
+		if err != nil {
+			span.SetStatus(otelcodes.Error, err.Error())
+			span.RecordError(err)
+		} else {
+			span.SetStatus(otelcodes.Ok, "subject provisioned for customer")
+		}
+
+		span.End()
+	}()
+
+	span.SetAttributes(
+		attribute.String("customer.id", cus.ID),
+		attribute.String("customer.key", lo.FromPtrOr(cus.Key, "nil")),
+	)
+
+	// Check if the subject exists
+	sub, err := p.subject.GetByIdOrKey(ctx, cus.Namespace, subjectKey)
+	if err != nil {
+		if models.IsGenericNotFoundError(err) {
+			// Create Subject if it does not exist
+			sub, err = p.subject.Create(NewContextWithSkipSubjectCustomer(ctx),
+				subject.CreateInput{
+					Namespace: cus.Namespace,
+					Key:       subjectKey,
+					Metadata: lo.ToPtr(map[string]interface{}{
+						"createdBy":  "subject.provisioner",
+						"customerId": cus.ID,
+					}),
+				})
+			if err != nil {
+				return nil, fmt.Errorf("failed to create subject for customer [namespace=%s customer.id=%s customer.usage_attribution_key: %s]: %w",
+					cus.Namespace, cus.ID, subjectKey, err)
+			}
+
+			span.AddEvent("created subject", trace.WithAttributes(
+				attribute.String("subject.id", sub.Id),
+				attribute.String("subject.key", sub.Key),
+			))
+
+			return &sub, nil
+		}
+
+		return nil, err
+	}
+
+	span.AddEvent("found subject", trace.WithAttributes(
+		attribute.String("subject.id", sub.Id),
+		attribute.String("subject.key", sub.Key),
+	))
+
+	return &sub, nil
 }
