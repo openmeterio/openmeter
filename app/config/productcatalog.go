6aac4086d340468a7af546911682c25e58727fa8
Alex Goth <64845621+GAlexIHU@users.noreply.github.com>
2025-09-24 14:04:20 +0200

    chore(config): add multi-subscription config

--- app/config/productcatalog.go
+++ app/config/productcatalog.go
@@ -2,11 +2,17 @@ package config
 
 import "github.com/spf13/viper"
 
-type ProductCatalogConfiguration struct{}
+type ProductCatalogConfiguration struct {
+	Subscription SubscriptionConfiguration
+}
 
 func (c ProductCatalogConfiguration) Validate() error {
 	return nil
 }
 
 func ConfigureProductCatalog(v *viper.Viper) {
+}
+
+type SubscriptionConfiguration struct {
+	MultiSubscriptionNamespaces []string
 }
