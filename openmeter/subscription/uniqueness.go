b89e27031a127b01fbfd7b1de34c6d09f0a81590
Alex Goth <64845621+GAlexIHU@users.noreply.github.com>
2025-10-02 18:34:48 +0200

    feat(subs): uniqueconstraint validations

--- openmeter/subscription/uniqueness.go
+++ openmeter/subscription/uniqueness.go
@@ -1,20 +1,151 @@
 package subscription
 
 import (
-	"github.com/openmeterio/openmeter/pkg/models"
-)
+	"errors"
+	"fmt"
 
-// TODO[galexi]: Implement this
-func ValidateUniqueConstraintByFeatures(subs []SubscriptionSpec) error {
-	return nil
-}
+	"github.com/samber/lo"
+
+	"github.com/openmeterio/openmeter/pkg/models"
+	"github.com/openmeterio/openmeter/pkg/slicesx"
+)
 
 func ValidateUniqueConstraintBySubscriptions(subs []SubscriptionSpec) error {
 	if overlaps := models.NewSortedCadenceList(subs).GetOverlaps(); len(overlaps) > 0 {
 		return ErrOnlySingleSubscriptionAllowed.WithAttrs(models.Attributes{
+			// FIXME[galexi]: improve on this
 			"overlaps": overlaps,
 		})
 	}
 
 	return nil
+}
+
+func ValidateUniqueConstraintByFeatures(subs []SubscriptionSpec) error {
+	return featureLevelUniqueConstraintValidator{}.Validate(subs)
+}
+
+type SubscriptionUniqueConstraintErrorDetailSide struct {
+	Item    SubscriptionItemSpec `json:"-"` // useful internally but let's not expose it to the client
+	Path    SpecPath             `json:"path"`
+	PlanRef PlanRef              `json:"plan_ref"`
+}
+
+type SubscriptionUniqueConstraintErrorDetail struct {
+	Left  SubscriptionUniqueConstraintErrorDetailSide `json:"left"`
+	Right SubscriptionUniqueConstraintErrorDetailSide `json:"right"`
+}
+
+// let's localize all logic on this struct to avoid scope pollution
+type featureLevelUniqueConstraintValidator struct{}
+
+func (v featureLevelUniqueConstraintValidator) Validate(subs []SubscriptionSpec) error {
+	billableItems := v.collectRelevantItems(subs, v.itemIsBillable)
+	timelinesForBillableItems, err := v.buildRelevantTimelines(billableItems)
+	if err != nil {
+		return err
+	}
+
+	var errs []error
+	for _, timeline := range timelinesForBillableItems {
+		if overlaps := timeline.GetOverlaps(); len(overlaps) > 0 {
+			for _, overlap := range overlaps {
+				errs = append(errs, ErrOnlySingleBillableItemAllowedAtATime.WithAttrs(models.Attributes{
+					ErrCodeOnlySingleBillableItemAllowedAtATime: SubscriptionUniqueConstraintErrorDetail{
+						Left: SubscriptionUniqueConstraintErrorDetailSide{
+							Item:    lo.FromPtr(overlap.Item1.Item.SubscriptionItemSpec),
+							Path:    overlap.Item1.Item.GetPath(),
+							PlanRef: lo.FromPtr(overlap.Item1.Item.SubscriptionSpec.Plan),
+						},
+						Right: SubscriptionUniqueConstraintErrorDetailSide{
+							Item:    lo.FromPtr(overlap.Item2.Item.SubscriptionItemSpec),
+							Path:    overlap.Item2.Item.GetPath(),
+							PlanRef: lo.FromPtr(overlap.Item2.Item.SubscriptionSpec.Plan),
+						},
+					},
+				}))
+			}
+		}
+	}
+
+	return errors.Join(errs...)
+}
+
+func (v featureLevelUniqueConstraintValidator) buildRelevantTimelines(itemMap map[string][]itemSpecWithCircularReferences) (map[string]models.CadenceList[validationTimelineEntry], error) {
+	timelines := make(map[string]models.CadenceList[validationTimelineEntry])
+
+	for itemKey, items := range itemMap {
+		validationTimelineEntries, err := slicesx.MapWithErr(items, func(item itemSpecWithCircularReferences) (validationTimelineEntry, error) {
+			phaseCadence, err := item.SubscriptionSpec.GetPhaseCadence(item.SubscriptionPhaseSpec.PhaseKey)
+			if err != nil {
+				return validationTimelineEntry{}, fmt.Errorf("failed to get phase cadence for item %s: %w", itemKey, err)
+			}
+
+			itemCadence := item.SubscriptionItemSpec.GetCadence(phaseCadence)
+
+			return validationTimelineEntry{
+				Item:    &item,
+				Cadence: itemCadence,
+			}, nil
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		timelines[itemKey] = models.NewSortedCadenceList(validationTimelineEntries)
+	}
+
+	return timelines, nil
+}
+
+func (v featureLevelUniqueConstraintValidator) collectRelevantItems(subs []SubscriptionSpec, condition func(item *SubscriptionItemSpec) bool) map[string][]itemSpecWithCircularReferences {
+	relevantItems := make(map[string][]itemSpecWithCircularReferences)
+
+	for _, sub := range subs {
+		for _, phase := range sub.Phases {
+			for itemKey, items := range phase.ItemsByKey {
+				for idx, item := range items {
+					if condition(item) {
+						relevantItems[itemKey] = append(relevantItems[itemKey], itemSpecWithCircularReferences{
+							SubscriptionItemVersion: idx,
+							SubscriptionItemSpec:    item,
+							SubscriptionPhaseSpec:   phase,
+							SubscriptionSpec:        &sub,
+						})
+					}
+				}
+			}
+		}
+	}
+
+	return relevantItems
+}
+
+func (v featureLevelUniqueConstraintValidator) itemIsBillable(item *SubscriptionItemSpec) bool {
+	if item == nil {
+		return false
+	}
+
+	return item.RateCard.AsMeta().IsBillable()
+}
+
+// This will be a circular structure so be careful when using it
+type itemSpecWithCircularReferences struct {
+	SubscriptionItemVersion int
+	SubscriptionItemSpec    *SubscriptionItemSpec
+	SubscriptionPhaseSpec   *SubscriptionPhaseSpec
+	SubscriptionSpec        *SubscriptionSpec
+}
+
+func (i itemSpecWithCircularReferences) GetPath() SpecPath {
+	return NewItemVersionPath(i.SubscriptionPhaseSpec.PhaseKey, i.SubscriptionItemSpec.ItemKey, i.SubscriptionItemVersion)
+}
+
+type validationTimelineEntry struct {
+	Item    *itemSpecWithCircularReferences
+	Cadence models.CadencedModel
+}
+
+func (i validationTimelineEntry) GetCadence() models.CadencedModel {
+	return i.Cadence
 }
--- openmeter/subscription/uniqueness_test.go
+++ openmeter/subscription/uniqueness_test.go
@@ -4,38 +4,21 @@ import (
 	"testing"
 	"time"
 
+	"github.com/alpacahq/alpacadecimal"
 	"github.com/invopop/gobl/currency"
 	"github.com/samber/lo"
 	"github.com/stretchr/testify/require"
 
+	"github.com/openmeterio/openmeter/openmeter/productcatalog"
 	"github.com/openmeterio/openmeter/openmeter/subscription"
+	subscriptiontestutils "github.com/openmeterio/openmeter/openmeter/subscription/testutils"
+	"github.com/openmeterio/openmeter/pkg/clock"
 	"github.com/openmeterio/openmeter/pkg/currencyx"
 	"github.com/openmeterio/openmeter/pkg/datetime"
 	"github.com/openmeterio/openmeter/pkg/models"
 )
 
 func TestValidateUniqueConstraintBySubscriptions(t *testing.T) {
-	getSimpleSub := func(cad models.CadencedModel) subscription.SubscriptionSpec {
-		sp := subscription.SubscriptionSpec{
-			CreateSubscriptionPlanInput: subscription.CreateSubscriptionPlanInput{
-				Plan: &subscription.PlanRef{
-					Key: "test_plan",
-				},
-				BillingCadence: datetime.NewISODuration(0, 1, 0, 0, 0, 0, 0),
-			},
-			CreateSubscriptionCustomerInput: subscription.CreateSubscriptionCustomerInput{
-				CustomerId:    "test_customer",
-				ActiveFrom:    cad.ActiveFrom,
-				ActiveTo:      cad.ActiveTo,
-				Name:          "test_subscription",
-				BillingAnchor: cad.ActiveFrom,
-				Currency:      currencyx.Code(currency.USD),
-			},
-		}
-
-		return sp
-	}
-
 	t.Run("Should not error if they're far apart", func(t *testing.T) {
 		s1 := getSimpleSub(models.CadencedModel{
 			ActiveFrom: time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC),
@@ -122,4 +105,280 @@ func TestValidateUniqueConstraintBySubscriptions(t *testing.T) {
 		err := subscription.ValidateUniqueConstraintBySubscriptions([]subscription.SubscriptionSpec{s1, s2, s3, s4, s5})
 		require.Error(t, err)
 	})
+}
+
+func TestValidateUniqueConstraintByFeatures(t *testing.T) {
+	t.Run("Should not error if on a single suscription passed in", func(t *testing.T) {
+		s1 := getSimpleSub(models.CadencedModel{
+			ActiveFrom: time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC),
+			ActiveTo:   lo.ToPtr(time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC)),
+		})
+
+		require.NoError(t, subscription.ValidateUniqueConstraintByFeatures([]subscription.SubscriptionSpec{s1}))
+	})
+
+	t.Run("Should not error if two empty subscriptions are overlapping", func(t *testing.T) {
+		s1 := getSimpleSub(models.CadencedModel{
+			ActiveFrom: time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC),
+			ActiveTo:   lo.ToPtr(time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC)),
+		})
+
+		s2 := getSimpleSub(models.CadencedModel{
+			ActiveFrom: time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC),
+			ActiveTo:   lo.ToPtr(time.Date(2025, 1, 4, 0, 0, 0, 0, time.UTC)),
+		})
+
+		require.NoError(t, subscription.ValidateUniqueConstraintByFeatures([]subscription.SubscriptionSpec{s1, s2}))
+	})
+
+	t.Run("Should not error if two if two subscriptions share the same feature without entitlement or price", func(t *testing.T) {
+		clock.FreezeTime(time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC))
+		defer clock.UnFreeze()
+
+		// First the setup
+		overlappingFeatureKey := "feature1"
+		overlappingFeatureID := "01K6JCPG631MH1EKEQB2YMDBJW"
+
+		builder1 := subscriptiontestutils.BuildTestSubscriptionSpec(t)
+		builder1 = builder1.AddPhase(nil, &productcatalog.FlatFeeRateCard{
+			RateCardMeta: productcatalog.RateCardMeta{
+				Name:       "overlapping feature",
+				Key:        overlappingFeatureKey,
+				FeatureKey: &overlappingFeatureKey,
+				FeatureID:  &overlappingFeatureID,
+			},
+		})
+		s1, err := builder1.Build()
+		require.NoError(t, err)
+
+		builder2 := subscriptiontestutils.BuildTestSubscriptionSpec(t)
+		builder2 = builder2.AddPhase(nil, &productcatalog.FlatFeeRateCard{
+			RateCardMeta: productcatalog.RateCardMeta{
+				Name:       "overlapping feature",
+				Key:        overlappingFeatureKey,
+				FeatureKey: &overlappingFeatureKey,
+				FeatureID:  &overlappingFeatureID,
+			},
+		})
+		s2, err := builder2.Build()
+		require.NoError(t, err)
+
+		t.Run("Should not error if the subscriptions are overlapping", func(t *testing.T) {
+			s1.ActiveFrom = time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC)
+			s1.ActiveTo = lo.ToPtr(time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC))
+			s2.ActiveFrom = time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC)
+			s2.ActiveTo = lo.ToPtr(time.Date(2025, 1, 4, 0, 0, 0, 0, time.UTC))
+
+			require.NoError(t, subscription.ValidateUniqueConstraintByFeatures([]subscription.SubscriptionSpec{s1, s2}))
+		})
+
+		t.Run("Should not error if the subscriptions are adjacent", func(t *testing.T) {
+			s1.ActiveFrom = time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC)
+			s1.ActiveTo = lo.ToPtr(time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC))
+			s2.ActiveFrom = time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC)
+			s2.ActiveTo = lo.ToPtr(time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC))
+
+			require.NoError(t, subscription.ValidateUniqueConstraintByFeatures([]subscription.SubscriptionSpec{s1, s2}))
+		})
+
+		t.Run("Should not error if the subscriptions are not overlapping", func(t *testing.T) {
+			s1.ActiveFrom = time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC)
+			s1.ActiveTo = lo.ToPtr(time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC))
+			s2.ActiveFrom = time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC)
+			s2.ActiveTo = lo.ToPtr(time.Date(2025, 1, 4, 0, 0, 0, 0, time.UTC))
+
+			require.NoError(t, subscription.ValidateUniqueConstraintByFeatures([]subscription.SubscriptionSpec{s1, s2}))
+		})
+	})
+
+	t.Run("Should not error if two subscriptions have overlapping features, where at most one has a price and neither has an entitlement", func(t *testing.T) {
+		clock.FreezeTime(time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC))
+		defer clock.UnFreeze()
+
+		overlappingFeatureKey := "feature1"
+		overlappingFeatureID := "01K6JCPG631MH1EKEQB2YMDBJW"
+
+		builder1 := subscriptiontestutils.BuildTestSubscriptionSpec(t)
+		builder1 = builder1.AddPhase(nil, &productcatalog.FlatFeeRateCard{
+			RateCardMeta: productcatalog.RateCardMeta{
+				Name: "overlapping feature",
+				Price: productcatalog.NewPriceFrom(productcatalog.FlatPrice{
+					Amount:      alpacadecimal.NewFromInt(int64(100)),
+					PaymentTerm: productcatalog.InAdvancePaymentTerm,
+				}),
+				Key:        overlappingFeatureKey,
+				FeatureKey: &overlappingFeatureKey,
+				FeatureID:  &overlappingFeatureID,
+			},
+		})
+		s1, err := builder1.Build()
+		require.NoError(t, err)
+
+		builder2 := subscriptiontestutils.BuildTestSubscriptionSpec(t)
+		builder2 = builder2.AddPhase(nil, &productcatalog.FlatFeeRateCard{
+			RateCardMeta: productcatalog.RateCardMeta{
+				Name:       "overlapping feature",
+				Key:        overlappingFeatureKey,
+				FeatureKey: &overlappingFeatureKey,
+				FeatureID:  &overlappingFeatureID,
+			},
+		})
+
+		s2, err := builder2.Build()
+		require.NoError(t, err)
+
+		builder3 := subscriptiontestutils.BuildTestSubscriptionSpec(t)
+		builder3 = builder3.AddPhase(nil, &productcatalog.FlatFeeRateCard{
+			RateCardMeta: productcatalog.RateCardMeta{
+				Name:       "overlapping feature",
+				Key:        overlappingFeatureKey,
+				FeatureKey: &overlappingFeatureKey,
+				FeatureID:  &overlappingFeatureID,
+			},
+		})
+		s3, err := builder3.Build()
+		require.NoError(t, err)
+
+		builder4 := subscriptiontestutils.BuildTestSubscriptionSpec(t)
+		builder4 = builder4.AddPhase(nil, &productcatalog.FlatFeeRateCard{
+			RateCardMeta: productcatalog.RateCardMeta{
+				Name:       "overlapping feature",
+				Key:        overlappingFeatureKey,
+				FeatureKey: &overlappingFeatureKey,
+				FeatureID:  &overlappingFeatureID,
+				Price: productcatalog.NewPriceFrom(productcatalog.FlatPrice{
+					Amount:      alpacadecimal.NewFromInt(int64(100)),
+					PaymentTerm: productcatalog.InAdvancePaymentTerm,
+				}),
+			},
+		})
+		s4, err := builder4.Build()
+		require.NoError(t, err)
+
+		t.Run("Should not error when overlapping", func(t *testing.T) {
+			s1.ActiveFrom = time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC)
+			s1.ActiveTo = lo.ToPtr(time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC))
+			s2.ActiveFrom = time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC)
+			s2.ActiveTo = lo.ToPtr(time.Date(2025, 1, 4, 0, 0, 0, 0, time.UTC))
+			s3.ActiveFrom = time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC)
+			s3.ActiveTo = lo.ToPtr(time.Date(2025, 1, 5, 0, 0, 0, 0, time.UTC))
+			s4.ActiveFrom = time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC)
+			s4.ActiveTo = lo.ToPtr(time.Date(2025, 1, 5, 0, 0, 0, 0, time.UTC))
+			// Looks like this
+			// | s1    |   s4  |		<- have prices
+			//     |   s2  |    		<- no prices
+			//     |     s3    |		<- no prices
+			// |   |   |   |   |
+
+			require.NoError(t, subscription.ValidateUniqueConstraintByFeatures([]subscription.SubscriptionSpec{s1, s2}), "Should not error for two")
+			require.NoError(t, subscription.ValidateUniqueConstraintByFeatures([]subscription.SubscriptionSpec{s1, s2, s3}), "Should not error for three")
+			require.NoError(t, subscription.ValidateUniqueConstraintByFeatures([]subscription.SubscriptionSpec{s1, s2, s3, s4}), "Should not error for four")
+		})
+	})
+
+	t.Run("Should error if two subscriptions have overlapping billable features", func(t *testing.T) {
+		clock.FreezeTime(time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC))
+		defer clock.UnFreeze()
+
+		overlappingFeatureKey := "feature1"
+		overlappingFeatureID := "01K6JCPG631MH1EKEQB2YMDBJW"
+
+		builder1 := subscriptiontestutils.BuildTestSubscriptionSpec(t)
+		builder1 = builder1.AddPhase(nil, &productcatalog.FlatFeeRateCard{
+			RateCardMeta: productcatalog.RateCardMeta{
+				Name: "overlapping feature",
+				Price: productcatalog.NewPriceFrom(productcatalog.FlatPrice{
+					Amount:      alpacadecimal.NewFromInt(int64(100)),
+					PaymentTerm: productcatalog.InAdvancePaymentTerm,
+				}),
+				Key:        overlappingFeatureKey,
+				FeatureKey: &overlappingFeatureKey,
+				FeatureID:  &overlappingFeatureID,
+			},
+		})
+		s1, err := builder1.Build()
+		require.NoError(t, err)
+
+		builder2 := subscriptiontestutils.BuildTestSubscriptionSpec(t)
+		builder2 = builder2.AddPhase(nil, &productcatalog.FlatFeeRateCard{
+			RateCardMeta: productcatalog.RateCardMeta{
+				Name: "overlapping feature",
+				Price: productcatalog.NewPriceFrom(productcatalog.FlatPrice{
+					Amount:      alpacadecimal.NewFromInt(int64(100)),
+					PaymentTerm: productcatalog.InAdvancePaymentTerm,
+				}),
+				Key:        overlappingFeatureKey,
+				FeatureKey: &overlappingFeatureKey,
+				FeatureID:  &overlappingFeatureID,
+			},
+		})
+		s2, err := builder2.Build()
+		require.NoError(t, err)
+
+		t.Run("Should error when overlapping", func(t *testing.T) {
+			s1.ActiveFrom = time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC)
+			s1.ActiveTo = lo.ToPtr(time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC))
+			s2.ActiveFrom = time.Date(2025, 1, 2, 0, 0, 0, 0, time.UTC)
+			s2.ActiveTo = lo.ToPtr(time.Date(2025, 1, 4, 0, 0, 0, 0, time.UTC))
+
+			err := subscription.ValidateUniqueConstraintByFeatures([]subscription.SubscriptionSpec{s1, s2})
+			require.Error(t, err)
+
+			// let's assert the error is correct
+			issues, err := models.AsValidationIssues(err)
+			require.NoError(t, err)
+			require.Len(t, issues, 1)
+			require.Equal(t, subscription.ErrOnlySingleBillableItemAllowedAtATime.Code(), issues[0].Code())
+
+			attrs := issues[0].Attributes()
+			detail, ok := attrs[subscription.ErrCodeOnlySingleBillableItemAllowedAtATime]
+			require.True(t, ok)
+
+			require.NotNil(t, detail)
+
+			detailTyped, ok := detail.(subscription.SubscriptionUniqueConstraintErrorDetail)
+			require.True(t, ok)
+
+			require.Equal(t, subscription.SpecPath("/phases/test_phase_1/items/feature1/idx/0"), detailTyped.Left.Path)
+			require.Equal(t, subscription.SpecPath("/phases/test_phase_1/items/feature1/idx/0"), detailTyped.Right.Path)
+		})
+	})
+
+	t.Run("Should error if two subscriptions have overlapping features with entitlements", func(t *testing.T) {
+		t.Fatal("Not implemented")
+	})
+
+	t.Run("Should error if two subscriptions have overlapping features with entitlements or are billable", func(t *testing.T) {
+		t.Fatal("Not implemented")
+	})
+
+	t.Run("Should error if multiple subscriptions have overlaps", func(t *testing.T) {
+		t.Fatal("Not implemented")
+	})
+
+	t.Run("Should not error if multiple subscriptions have overlapping timelines but we don't double charge or have doubled entitlements", func(t *testing.T) {
+		t.Fatal("Not implemented")
+	})
+}
+
+// builds an empty subscription without phases or items, used for testing the subscription level uniqueness constraint
+func getSimpleSub(cad models.CadencedModel) subscription.SubscriptionSpec {
+	sp := subscription.SubscriptionSpec{
+		CreateSubscriptionPlanInput: subscription.CreateSubscriptionPlanInput{
+			Plan: &subscription.PlanRef{
+				Key: "test_plan",
+			},
+			BillingCadence: datetime.NewISODuration(0, 1, 0, 0, 0, 0, 0),
+		},
+		CreateSubscriptionCustomerInput: subscription.CreateSubscriptionCustomerInput{
+			CustomerId:    "test_customer",
+			ActiveFrom:    cad.ActiveFrom,
+			ActiveTo:      cad.ActiveTo,
+			Name:          "test_subscription",
+			BillingAnchor: cad.ActiveFrom,
+			Currency:      currencyx.Code(currency.USD),
+		},
+	}
+
+	return sp
 }
