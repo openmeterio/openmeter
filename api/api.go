456dcc096579c8f22fb74abf7e83c65da6dd3281
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2025-10-13 15:15:01 +0200

    chore: use go tool directive (#3507)

--- api/api.go
+++ api/api.go
@@ -1,2 +1,2 @@
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=codegen.yaml ./openapi.yaml
+//go:generate go tool github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=codegen.yaml ./openapi.yaml
 package api
