7502a6c553a7b7754c57043f10aec6e8b99e5967
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2025-10-13 14:27:56 +0200

    chore(dagger): bump alpine base image

--- .dagger/versions_pinned.go
+++ .dagger/versions_pinned.go
@@ -13,5 +13,5 @@ const (
 	goBuildVersion = "1.25.1-alpine3.22@sha256:b6ed3fd0452c0e9bcdef5597f29cc1418f61672e9d3a2f55bf02e7222c014abd"
 	xxBaseImage    = "tonistiigi/xx:1.6.1@sha256:923441d7c25f1e2eb5789f82d987693c47b8ed987c4ab3b075d6ed2b5d6779a3"
 
-	alpineBaseImage = "alpine:3.21@sha256:a8560b36e8b8210634f77d9f7f9efd7ffa463e380b75e2e74aff4511df3ef88c"
+	alpineBaseImage = "alpine:3.22@sha256:4b7ce07002c69e8f3d704a9c5d6fd3053be500b7f1c69fc0d80990c2ad8dd412"
 )
