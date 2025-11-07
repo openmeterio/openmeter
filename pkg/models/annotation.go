898c6999cac73ac1b3f8a29dee029490fc8870c0
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2025-11-07 11:04:02 +0100

    feat: add helpers for annotations type

--- pkg/models/annotation.go
+++ pkg/models/annotation.go
@@ -19,3 +19,71 @@ func (a Annotations) GetBool(key string) bool {
 
 	return boolVal
 }
+
+func (a Annotations) GetString(key string) (string, bool) {
+	if len(a) == 0 {
+		return "", false
+	}
+
+	val, ok := a[key]
+	if !ok {
+		return "", false
+	}
+
+	strVal, ok := val.(string)
+	if !ok {
+		return "", false
+	}
+
+	return strVal, true
+}
+
+func (a Annotations) GetInt(key string) (int, bool) {
+	if len(a) == 0 {
+		return 0, false
+	}
+
+	val, ok := a[key]
+	if !ok {
+		return 0, false
+	}
+
+	intVal, ok := val.(int)
+	if !ok {
+		return 0, false
+	}
+
+	return intVal, true
+}
+
+func (a Annotations) Clone() Annotations {
+	if a == nil {
+		return nil
+	}
+
+	result := make(Annotations)
+
+	for k, v := range a {
+		result[k] = v
+	}
+
+	return result
+}
+
+func (a Annotations) Merge(m Annotations) Annotations {
+	if a == nil {
+		return m
+	}
+
+	result := a.Clone()
+
+	if len(m) == 0 {
+		return result
+	}
+
+	for k, v := range m {
+		result[k] = v
+	}
+
+	return result
+}
