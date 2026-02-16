73ab7fdd7bd56487c273ac48c1ed7d8185f528fb
Alex Goth <64845621+GAlexIHU@users.noreply.github.com>
2026-02-16 12:41:01 +0100

    fix(ent): 500 on negative value with V2 API (#3870)

--- openmeter/credit/errors.go
+++ openmeter/credit/errors.go
@@ -1,6 +1,11 @@
 package credit
 
-import "github.com/openmeterio/openmeter/pkg/models"
+import (
+	"net/http"
+
+	"github.com/openmeterio/openmeter/pkg/framework/commonhttp"
+	"github.com/openmeterio/openmeter/pkg/models"
+)
 
 const ErrCodeGrantAmountMustBePositive models.ErrorCode = "grant_amount_must_be_positive"
 
@@ -8,6 +13,7 @@ var ErrGrantAmountMustBePositive = models.NewValidationIssue(
 	ErrCodeGrantAmountMustBePositive,
 	"amount must be positive",
 	models.WithFieldString("amount"),
+	commonhttp.WithHTTPStatusCodeAttribute(http.StatusBadRequest),
 )
 
 const ErrCodeEffectiveAtMustBeSet models.ErrorCode = "grant_effective_at_must_be_set"
@@ -16,4 +22,5 @@ var ErrGrantEffectiveAtMustBeSet = models.NewValidationIssue(
 	ErrCodeEffectiveAtMustBeSet,
 	"effective at must be set",
 	models.WithFieldString("effectiveAt"),
+	commonhttp.WithHTTPStatusCodeAttribute(http.StatusBadRequest),
 )
