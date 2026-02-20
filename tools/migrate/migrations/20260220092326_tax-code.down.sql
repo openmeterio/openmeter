d8140966cebbf92b275a35448a031db59a1dd6bb
Robert Borbely <robert.borbely@konghq.com>
2026-02-20 10:26:55 +0100

    feat(ent): add tax code migration

---
+++ tools/migrate/migrations/20260220092326_tax-code.down.sql
@@ -0,0 +1,10 @@
+-- reverse: create index "taxcode_namespace_key_deleted_at" to table: "tax_codes"
+DROP INDEX "taxcode_namespace_key_deleted_at";
+-- reverse: create index "taxcode_namespace_id" to table: "tax_codes"
+DROP INDEX "taxcode_namespace_id";
+-- reverse: create index "taxcode_namespace" to table: "tax_codes"
+DROP INDEX "taxcode_namespace";
+-- reverse: create index "taxcode_id" to table: "tax_codes"
+DROP INDEX "taxcode_id";
+-- reverse: create "tax_codes" table
+DROP TABLE "tax_codes";
