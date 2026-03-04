3a0cd7dfb683446bb8cedb1d05cbecc605774b34
Peter Turi <peter.turi@openmeter.io>
2026-03-04 14:46:29 +0100

    fix: lint error

--- tools/migrate/migrations/20260304132121_billing-charges-fix-std-payments-edge.up.sql
+++ tools/migrate/migrations/20260304132121_billing-charges-fix-std-payments-edge.up.sql
@@ -1,6 +1,7 @@
 -- modify "charge_standard_invoice_payment_settlements" table
 ALTER TABLE "charge_standard_invoice_payment_settlements" DROP COLUMN "charge_id";
 -- create index "chargestandardinvoicepaymentsettlement_namespace_line_id" to table: "charge_standard_invoice_payment_settlements"
+-- atlas:nolint MF101
 CREATE UNIQUE INDEX "chargestandardinvoicepaymentsettlement_namespace_line_id" ON "charge_standard_invoice_payment_settlements" ("namespace", "line_id") WHERE ((line_id IS NOT NULL) AND (deleted_at IS NULL));
 -- modify "charge_flat_fees" table
 ALTER TABLE "charge_flat_fees" ADD COLUMN "std_invoice_payment_settlement_id" character(26) NULL, ADD CONSTRAINT "charge_flat_fees_charge_standard_invoice_payment_settlements_ch" FOREIGN KEY ("std_invoice_payment_settlement_id") REFERENCES "charge_standard_invoice_payment_settlements" ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
