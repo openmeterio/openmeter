3b7808c33028fdbd062af1ec157cf12ff17e0a6b
Peter Turi <peter.turi@openmeter.io>
2026-03-31 17:02:06 +0200

    fix: nits

--- openmeter/billing/charges/usagebased/service/rating.go
+++ openmeter/billing/charges/usagebased/service/rating.go
@@ -9,10 +9,8 @@ import (
 
 	"github.com/openmeterio/openmeter/openmeter/billing"
 	"github.com/openmeterio/openmeter/openmeter/billing/charges/usagebased"
-	"github.com/openmeterio/openmeter/openmeter/billing/models/totals"
 	"github.com/openmeterio/openmeter/openmeter/billing/rating"
 	"github.com/openmeterio/openmeter/openmeter/productcatalog/feature"
-	"github.com/openmeterio/openmeter/pkg/currencyx"
 )
 
 type getRatingForUsageResult struct {
@@ -52,11 +50,6 @@ func (s *service) getRatingForUsage(ctx context.Context, in getRatingForUsageInp
 		return getRatingForUsageResult{}, err
 	}
 
-	currencyCalculator, err := in.Charge.Intent.Currency.Calculator()
-	if err != nil {
-		return getRatingForUsageResult{}, fmt.Errorf("creating currency calculator: %w", err)
-	}
-
 	snapshotQuantity, err := s.snapshotQuantity(ctx, snapshotQuantityInput{
 		Customer:       in.Customer.Customer,
 		FeatureMeter:   in.FeatureMeter,
@@ -76,26 +69,7 @@ func (s *service) getRatingForUsage(ctx context.Context, in getRatingForUsageInp
 	}
 
 	return getRatingForUsageResult{
-		GenerateDetailedLinesResult: roundGenerateDetailedLinesResultTotals(ratingResult, currencyCalculator),
+		GenerateDetailedLinesResult: ratingResult,
 		Quantity:                    snapshotQuantity,
 	}, nil
-}
-
-func roundGenerateDetailedLinesResultTotals(in rating.GenerateDetailedLinesResult, calc currencyx.Calculator) rating.GenerateDetailedLinesResult {
-	in.Totals = roundTotalsToCurrencyPrecision(in.Totals, calc)
-
-	return in
-}
-
-func roundTotalsToCurrencyPrecision(in totals.Totals, calc currencyx.Calculator) totals.Totals {
-	in.Amount = calc.RoundToPrecision(in.Amount)
-	in.ChargesTotal = calc.RoundToPrecision(in.ChargesTotal)
-	in.DiscountsTotal = calc.RoundToPrecision(in.DiscountsTotal)
-	in.TaxesInclusiveTotal = calc.RoundToPrecision(in.TaxesInclusiveTotal)
-	in.TaxesExclusiveTotal = calc.RoundToPrecision(in.TaxesExclusiveTotal)
-	in.TaxesTotal = calc.RoundToPrecision(in.TaxesTotal)
-	in.CreditsTotal = calc.RoundToPrecision(in.CreditsTotal)
-	in.Total = calc.RoundToPrecision(in.Total)
-
-	return in
 }
