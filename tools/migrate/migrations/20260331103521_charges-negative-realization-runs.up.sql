7aa4eb3eea179bbf02076c34c65dd5a18115add0
Peter Turi <peter.turi@openmeter.io>
2026-03-31 16:11:08 +0200

    fix: migrations

--- tools/migrate/migrations/20260331103521_charges-negative-realization-runs.up.sql
+++ tools/migrate/migrations/20260331103521_charges-negative-realization-runs.up.sql
@@ -1,4 +1,6 @@
 -- modify "charge_flat_fee_credit_allocations" table
+-- atlas:nolint MF103
 ALTER TABLE "charge_flat_fee_credit_allocations" ADD COLUMN "type" character varying NOT NULL, ADD COLUMN "corrects_realization_id" character(26) NULL;
 -- modify "charge_usage_based_run_credit_allocations" table
+-- atlas:nolint MF103
 ALTER TABLE "charge_usage_based_run_credit_allocations" ADD COLUMN "type" character varying NOT NULL, ADD COLUMN "corrects_realization_id" character(26) NULL;
