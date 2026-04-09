9ce08f7f7ba3358b5e0bdd047c6aba10db8ae477
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2026-04-09 23:05:01 +0200

    refactor: labels helpers

--- api/v3/labels/validate.go
+++ api/v3/labels/validate.go
@@ -2,10 +2,12 @@ package labels
 
 import (
 	"errors"
-	"fmt"
+	"net/http"
 	"regexp"
 
 	api "github.com/openmeterio/openmeter/api/v3"
+	"github.com/openmeterio/openmeter/pkg/framework/commonhttp"
+	"github.com/openmeterio/openmeter/pkg/models"
 )
 
 var (
@@ -16,15 +18,40 @@ var (
 // https://regex101.com/?regex=%5E%28%3F%3A%5Ba-zA-Z0-9%5D%28%3F%3A%5Ba-zA-Z0-9._-%5D%5Ba-zA-Z0-9%5D%29%3F%29%7B1%2C63%7D%24&testString=openmeter_good%0Aopenmeter.good%0Aopenmeter-good%0Aopenmeter_bad_%0Aopenmeter-bad-%0Aopenmeter.bad.%0A_openmeter-bad%0A-openmeter-bad%0A.openmeter.bad%0A.openmeter.way_toooooooooooooooooooooooooooooooooooooooooooooooooooooo_long&flags=gm&flavor=pcre2&delimiter=%2F
 var keyValueFormat = regexp.MustCompile(`^(?:[a-zA-Z0-9](?:[a-zA-Z0-9._-]?[a-zA-Z0-9])?){1,63}$`)
 
+// https://regex101.com/?regex=%5E%28%3F%3A%5Ba-zA-Z0-9%5D%28%3F%3A%5Ba-zA-Z0-9._-%5D%5Ba-zA-Z0-9%5D%29%3F%29%7B1%2C63%7D%24&testString=openmeter_good%0Aopenmeter.good%0Aopenmeter-good%0Aopenmeter_bad_%0Aopenmeter-bad-%0Aopenmeter.bad.%0A_openmeter-bad%0A-openmeter-bad%0A.openmeter.bad%0A.openmeter.way_toooooooooooooooooooooooooooooooooooooooooooooooooooooo_long&flags=gm&flavor=pcre2&delimiter=%2F
+var reservedPrefixMatcher = regexp.MustCompile(`^(_|kong|konnect|insomnia|mesh|kic|kuma|openmeter)`)
+
 func ValidateLabel(k, v string) error {
 	var errs []error
 
 	if !keyValueFormat.MatchString(k) {
-		errs = append(errs, ErrInvalidLabelKey)
+		errs = append(errs, models.NewValidationIssue(
+			"invalid_label_key_format",
+			"label key must be a valid DNS_SUBDOMAIN format",
+			models.WithFieldString("labels"),
+			models.WithCriticalSeverity(),
+			commonhttp.WithHTTPStatusCodeAttribute(http.StatusBadRequest),
+		))
 	}
 
 	if !keyValueFormat.MatchString(v) {
-		errs = append(errs, ErrInvalidLabelValue)
+		errs = append(errs, models.NewValidationIssue(
+			"invalid_label_value_format",
+			"label value must be a valid DNS_SUBDOMAIN format",
+			models.WithFieldString("labels"),
+			models.WithCriticalSeverity(),
+			commonhttp.WithHTTPStatusCodeAttribute(http.StatusBadRequest),
+		))
+	}
+
+	if reservedPrefixMatcher.MatchString(k) {
+		errs = append(errs, models.NewValidationIssue(
+			"invalid_label_key_prefix",
+			"label key must be a valid DNS_SUBDOMAIN format",
+			models.WithFieldString("labels"),
+			models.WithCriticalSeverity(),
+			commonhttp.WithHTTPStatusCodeAttribute(http.StatusBadRequest),
+		))
 	}
 
 	return errors.Join(errs...)
@@ -34,15 +61,8 @@ func ValidateLabels(l api.Labels) error {
 	var errs []error
 
 	for k, v := range l {
-		err := ValidateLabel(k, v)
-		if err != nil {
-			if errors.Is(err, ErrInvalidLabelKey) {
-				errs = append(errs, fmt.Errorf("%w: %q", err, k))
-			}
-
-			if errors.Is(err, ErrInvalidLabelValue) {
-				errs = append(errs, fmt.Errorf("%w: %q", err, v))
-			}
+		if err := ValidateLabel(k, v); err != nil {
+			errs = append(errs, err)
 		}
 	}
 
