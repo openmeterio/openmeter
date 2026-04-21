adca45867779fddb16fe7cf2013d3c6f9cc45f3c
Peter Turi <peter.turi@openmeter.io>
2026-04-21 19:59:05 +0200

    chore: fix down migartion

--- tools/migrate/migrations/20260421150206_require_child_unique_reference_id_on_detailed_lines.down.sql
+++ tools/migrate/migrations/20260421150206_require_child_unique_reference_id_on_detailed_lines.down.sql
@@ -1,6 +1,6 @@
 -- reverse: modify "charge_usage_based_run_detailed_line" table
-ALTER TABLE "charge_usage_based_run_detailed_line" ALTER COLUMN "child_unique_reference_id" DROP NOT NULL, DROP CONSTRAINT "child_unique_reference_id_not_empty";
+ALTER TABLE "charge_usage_based_run_detailed_line" ALTER COLUMN "child_unique_reference_id" DROP NOT NULL, DROP CONSTRAINT IF EXISTS "child_unique_reference_id_not_empty";
 -- reverse: modify "charge_flat_fee_detailed_line" table
-ALTER TABLE "charge_flat_fee_detailed_line" ALTER COLUMN "child_unique_reference_id" DROP NOT NULL, DROP CONSTRAINT "child_unique_reference_id_not_empty";
+ALTER TABLE "charge_flat_fee_detailed_line" ALTER COLUMN "child_unique_reference_id" DROP NOT NULL, DROP CONSTRAINT IF EXISTS "child_unique_reference_id_not_empty";
 -- reverse: modify "billing_standard_invoice_detailed_lines" table
-ALTER TABLE "billing_standard_invoice_detailed_lines" ALTER COLUMN "child_unique_reference_id" DROP NOT NULL, DROP CONSTRAINT "child_unique_reference_id_not_empty";
+ALTER TABLE "billing_standard_invoice_detailed_lines" ALTER COLUMN "child_unique_reference_id" DROP NOT NULL, DROP CONSTRAINT IF EXISTS "child_unique_reference_id_not_empty";
