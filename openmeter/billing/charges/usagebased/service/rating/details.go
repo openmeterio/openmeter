8eb81cd6775684d041d224c65effd673728aa3e8
Peter Turi <peter.turi@openmeter.io>
2026-04-22 09:08:33 +0200

    chore: fix lint

--- openmeter/billing/charges/usagebased/service/rating/details.go
+++ openmeter/billing/charges/usagebased/service/rating/details.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/alpacahq/alpacadecimal"
+
 	"github.com/openmeterio/openmeter/openmeter/billing"
 	"github.com/openmeterio/openmeter/openmeter/billing/charges/usagebased"
 	billingrating "github.com/openmeterio/openmeter/openmeter/billing/rating"
