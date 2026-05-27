cb9a1277d967d3536f48ba68223071bf2ee022fb
Robert Boros <robert.boros@konghq.com>
2026-05-27 12:05:58 +0200

    fix: test

--- app/config/config_test.go
+++ app/config/config_test.go
@@ -191,7 +191,7 @@ func TestComplete(t *testing.T) {
 		Credits: CreditsConfiguration{
 			Enabled:                 false,
 			EnableCreditThenInvoice: false,
-			FeatureFlag:             "billing_credits",
+			FeatureFlag:             "",
 		},
 		Sink: SinkConfiguration{
 			GroupId:                 "openmeter-sink-worker",
