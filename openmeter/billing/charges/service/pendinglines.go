5b4ce3d0957052939da598e208f737b41d4151e0
Mark Vass <mark.vass@konghq.com>
2026-06-17 18:05:55 +0200

    feat(billing): CreateInvoicePendingLines charges support (#4508)

---
+++ openmeter/billing/charges/service/pendinglines.go
@@ -0,0 +1,290 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"slices"
+
+	"github.com/samber/lo"
+
+	"github.com/openmeterio/openmeter/openmeter/billing"
+	"github.com/openmeterio/openmeter/openmeter/billing/charges"
+	"github.com/openmeterio/openmeter/openmeter/billing/charges/flatfee"
+	"github.com/openmeterio/openmeter/openmeter/billing/charges/meta"
+	"github.com/openmeterio/openmeter/openmeter/billing/charges/usagebased"
+	"github.com/openmeterio/openmeter/openmeter/productcatalog"
+	"github.com/openmeterio/openmeter/pkg/currencyx"
+	"github.com/openmeterio/openmeter/pkg/framework/transaction"
+	"github.com/openmeterio/openmeter/pkg/slicesx"
+)
+
+func (s *service) CreatePendingInvoiceLines(ctx context.Context, input charges.CreatePendingInvoiceLinesInput) (*charges.CreatePendingInvoiceLinesResult, error) {
+	for i := range input.Lines {
+		input.Lines[i].Namespace = input.Customer.Namespace
+		input.Lines[i].Currency = input.Currency
+		if input.Lines[i].Engine == billing.LineEngineTypeInvoice {
+			input.Lines[i].Engine = ""
+		}
+	}
+
+	if err := validateChargePendingInvoiceLinesInput(input); err != nil {
+		return nil, billing.ValidationError{Err: err}
+	}
+
+	intents, err := mapPendingInvoiceLinesToChargeIntents(input)
+	if err != nil {
+		return nil, billing.ValidationError{Err: err}
+	}
+
+	return transaction.Run(ctx, s.adapter, func(ctx context.Context) (*charges.CreatePendingInvoiceLinesResult, error) {
+		result, err := s.create(ctx, charges.CreateInput{
+			Namespace: input.Customer.Namespace,
+			Intents:   intents,
+		})
+		if err != nil {
+			return nil, err
+		}
+		if result == nil {
+			return nil, fmt.Errorf("create charges for pending invoice lines: result is nil")
+		}
+
+		if _, err := s.autoAdvanceCreatedCharges(ctx, result.charges); err != nil {
+			return nil, err
+		}
+
+		if len(result.pendingLineResults) == 0 {
+			return nil, fmt.Errorf("create charges for pending invoice lines: no gathering lines were created")
+		}
+		if len(result.pendingLineResults) > 1 {
+			return nil, fmt.Errorf("create charges for pending invoice lines: expected one pending-line result, got %d", len(result.pendingLineResults))
+		}
+
+		pendingLineResult := result.pendingLineResults[0]
+		orderedLines, err := orderPendingLinesByCreatedCharges(orderPendingLinesByCreatedChargesInput{
+			lines:          pendingLineResult.Lines,
+			createdCharges: result.charges,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("validating pending line results: %w", err)
+		}
+		pendingLineResult.Lines = orderedLines
+
+		return pendingLineResult, nil
+	})
+}
+
+func validateChargePendingInvoiceLinesInput(input charges.CreatePendingInvoiceLinesInput) error {
+	var errs []error
+
+	if err := input.Validate(); err != nil {
+		errs = append(errs, err)
+	}
+
+	if len(input.Lines) == 0 {
+		errs = append(errs, errors.New("no lines provided"))
+	}
+
+	for idx, line := range input.Lines {
+		if line.ChargeID != nil {
+			errs = append(errs, fmt.Errorf("line.%d: charge ID is not allowed for charge-backed pending line creation", idx))
+		}
+
+		if line.Engine != "" {
+			errs = append(errs, fmt.Errorf("line.%d: engine is not allowed for charge-backed pending line creation", idx))
+		}
+
+		if line.ManagedBy != billing.ManuallyManagedLine {
+			errs = append(errs, fmt.Errorf("line.%d: managed by must be %s for charge-backed pending line creation", idx, billing.ManuallyManagedLine))
+		}
+
+		if line.Subscription != nil {
+			errs = append(errs, fmt.Errorf("line.%d: subscription is not allowed for charge-backed pending line creation", idx))
+		}
+	}
+
+	return errors.Join(errs...)
+}
+
+func mapPendingInvoiceLinesToChargeIntents(input charges.CreatePendingInvoiceLinesInput) (charges.ChargeIntents, error) {
+	intents := make(charges.ChargeIntents, 0, len(input.Lines))
+
+	for idx, line := range input.Lines {
+		intent, err := mapPendingInvoiceLineToChargeIntent(input.Customer.ID, input.Currency, line)
+		if err != nil {
+			return nil, fmt.Errorf("line.%d: %w", idx, err)
+		}
+
+		intents = append(intents, intent)
+	}
+
+	return intents, nil
+}
+
+func mapPendingInvoiceLineToChargeIntent(customerID string, currency currencyx.Code, line billing.GatheringLine) (charges.ChargeIntent, error) {
+	baseIntent, err := chargeIntentBaseFromPendingInvoiceLine(customerID, currency, line)
+	if err != nil {
+		return charges.ChargeIntent{}, err
+	}
+
+	switch line.Price.Type() {
+	case productcatalog.FlatPriceType:
+		flatPrice, err := line.Price.AsFlat()
+		if err != nil {
+			return charges.ChargeIntent{}, fmt.Errorf("converting price to flat: %w", err)
+		}
+
+		return charges.NewChargeIntent(flatfee.Intent{
+			Intent:                baseIntent,
+			InvoiceAt:             line.InvoiceAt,
+			SettlementMode:        productcatalog.CreditThenInvoiceSettlementMode,
+			PaymentTerm:           flatPrice.PaymentTerm,
+			FeatureKey:            line.FeatureKey,
+			PercentageDiscounts:   billingPercentageDiscountToProductCatalog(line.RateCardDiscounts.Percentage),
+			AmountBeforeProration: flatPrice.Amount,
+		}), nil
+	default:
+		return charges.NewChargeIntent(usagebased.Intent{
+			Intent:         baseIntent,
+			InvoiceAt:      line.InvoiceAt,
+			SettlementMode: productcatalog.CreditThenInvoiceSettlementMode,
+			FeatureKey:     line.FeatureKey,
+			Price:          line.Price,
+			Discounts:      billingDiscountsToProductCatalog(line.RateCardDiscounts),
+		}), nil
+	}
+}
+
+func chargeIntentBaseFromPendingInvoiceLine(customerID string, currency currencyx.Code, line billing.GatheringLine) (meta.Intent, error) {
+	annotations, err := line.Annotations.Clone()
+	if err != nil {
+		return meta.Intent{}, fmt.Errorf("cloning annotations: %w", err)
+	}
+
+	return meta.Intent{
+		Name:              line.Name,
+		Description:       line.Description,
+		Metadata:          line.Metadata.Clone(),
+		Annotations:       annotations,
+		ManagedBy:         billing.ManuallyManagedLine,
+		CustomerID:        customerID,
+		Currency:          currency,
+		ServicePeriod:     line.ServicePeriod,
+		FullServicePeriod: line.ServicePeriod,
+		BillingPeriod:     line.ServicePeriod,
+		TaxConfig:         productcatalog.TaxCodeConfigFrom(line.TaxConfig),
+		UniqueReferenceID: line.ChildUniqueReferenceID,
+	}, nil
+}
+
+func billingDiscountsToProductCatalog(discounts billing.Discounts) productcatalog.Discounts {
+	return productcatalog.Discounts{
+		Percentage: billingPercentageDiscountToProductCatalog(discounts.Percentage),
+		Usage:      billingUsageDiscountToProductCatalog(discounts.Usage),
+	}
+}
+
+func billingPercentageDiscountToProductCatalog(discount *billing.PercentageDiscount) *productcatalog.PercentageDiscount {
+	if discount == nil {
+		return nil
+	}
+
+	return lo.ToPtr(discount.PercentageDiscount.Clone())
+}
+
+func billingUsageDiscountToProductCatalog(discount *billing.UsageDiscount) *productcatalog.UsageDiscount {
+	if discount == nil {
+		return nil
+	}
+
+	return lo.ToPtr(discount.UsageDiscount.Clone())
+}
+
+type orderPendingLinesByCreatedChargesInput struct {
+	lines          []billing.GatheringLine
+	createdCharges charges.Charges
+}
+
+func (i orderPendingLinesByCreatedChargesInput) Validate() error {
+	if err := errors.Join(lo.Map(i.lines, func(line billing.GatheringLine, idx int) error {
+		if line.ChargeID == nil || *line.ChargeID == "" {
+			return fmt.Errorf("line.%d: charge ID is required on charge-backed pending line result", idx)
+		}
+
+		return nil
+	})...); err != nil {
+		return err
+	}
+
+	lineChargeIDs := lo.Map(i.lines, func(line billing.GatheringLine, _ int) string {
+		return *line.ChargeID
+	})
+	if !slices.Equal(lineChargeIDs, lo.Uniq(lineChargeIDs)) {
+		return fmt.Errorf("duplicate charge IDs found in pending line result")
+	}
+
+	return nil
+}
+
+func orderPendingLinesByCreatedCharges(input orderPendingLinesByCreatedChargesInput) ([]billing.GatheringLine, error) {
+	if err := input.Validate(); err != nil {
+		return nil, err
+	}
+
+	linesByChargeID := lo.SliceToMap(input.lines, func(line billing.GatheringLine) (string, billing.GatheringLine) {
+		return *line.ChargeID, line
+	})
+
+	indexedCreatedCharges := lo.Map(input.createdCharges, func(charge charges.Charge, idx int) createdChargeWithIndex {
+		return createdChargeWithIndex{
+			index:  idx,
+			charge: charge,
+		}
+	})
+
+	out, err := slicesx.MapWithErr(indexedCreatedCharges, func(createdCharge createdChargeWithIndex) (billing.GatheringLine, error) {
+		empty := billing.GatheringLine{}
+
+		chargeID, err := createdCharge.charge.GetChargeID()
+		if err != nil {
+			return empty, fmt.Errorf("created charge.%d: resolving charge ID: %w", createdCharge.index, err)
+		}
+
+		line, ok := linesByChargeID[chargeID.ID]
+		if !ok {
+			return empty, fmt.Errorf("created charge.%d[%s]: gathering line was not created", createdCharge.index, chargeID.ID)
+		}
+
+		expectedEngine, ok := lineEngineTypeForChargeType(createdCharge.charge.Type())
+		if !ok {
+			return empty, fmt.Errorf("created charge.%d[%s]: unsupported charge type %s", createdCharge.index, chargeID.ID, createdCharge.charge.Type())
+		}
+
+		if line.Engine != expectedEngine {
+			return empty, fmt.Errorf("created charge.%d[%s]: expected line engine %s, got %s", createdCharge.index, chargeID.ID, expectedEngine, line.Engine)
+		}
+
+		return line, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
+
+type createdChargeWithIndex struct {
+	index  int
+	charge charges.Charge
+}
+
+func lineEngineTypeForChargeType(chargeType meta.ChargeType) (billing.LineEngineType, bool) {
+	switch chargeType {
+	case meta.ChargeTypeFlatFee:
+		return billing.LineEngineTypeChargeFlatFee, true
+	case meta.ChargeTypeUsageBased:
+		return billing.LineEngineTypeChargeUsageBased, true
+	default:
+		return "", false
+	}
+}
---
+++ openmeter/billing/charges/service/pendinglines_test.go
@@ -0,0 +1,318 @@
+package service
+
+import (
+	"time"
+
+	"github.com/alpacahq/alpacadecimal"
+	"github.com/samber/lo"
+
+	"github.com/openmeterio/openmeter/openmeter/billing"
+	"github.com/openmeterio/openmeter/openmeter/billing/charges"
+	"github.com/openmeterio/openmeter/openmeter/billing/charges/meta"
+	"github.com/openmeterio/openmeter/openmeter/productcatalog"
+	"github.com/openmeterio/openmeter/pkg/datetime"
+	"github.com/openmeterio/openmeter/pkg/models"
+	"github.com/openmeterio/openmeter/pkg/timeutil"
+	billingtest "github.com/openmeterio/openmeter/test/billing"
+)
+
+func (s *InvoicableChargesTestSuite) TestCreatePendingInvoiceLinesCreatesChargeBackedGatheringLines() {
+	ctx := s.T().Context()
+	ns := s.GetUniqueNamespace("charges-service-create-pending-lines")
+	s.ProvisionDefaultTaxCodes(ctx, ns)
+
+	customInvoicing := s.SetupCustomInvoicing(ns)
+	cust := s.CreateTestCustomer(ns, "test-subject")
+	s.NotEmpty(cust.ID)
+
+	_ = s.ProvisionBillingProfile(ctx, ns, customInvoicing.App.GetID(),
+		billingtest.WithCollectionInterval(datetime.MustParseDuration(s.T(), "P2D")),
+		billingtest.WithManualApproval(),
+	)
+
+	servicePeriod := timeutil.ClosedPeriod{
+		From: datetime.MustParseTimeInLocation(s.T(), "2026-01-01T00:00:00Z", time.UTC).AsTime(),
+		To:   datetime.MustParseTimeInLocation(s.T(), "2026-02-01T00:00:00Z", time.UTC).AsTime(),
+	}
+
+	apiRequestsTotal := s.SetupApiRequestsTotalFeature(ctx, ns)
+	featureKey := apiRequestsTotal.Feature.Key
+
+	usageLine := billing.GatheringLine{
+		GatheringLineBase: billing.GatheringLineBase{
+			ManagedResource: models.NewManagedResource(models.ManagedResourceInput{
+				Namespace: ns,
+				Name:      "manual usage",
+			}),
+			ManagedBy:     billing.ManuallyManagedLine,
+			Engine:        billing.LineEngineTypeInvoice,
+			Currency:      USD,
+			ServicePeriod: servicePeriod,
+			InvoiceAt:     servicePeriod.To,
+			Price: lo.FromPtr(productcatalog.NewPriceFrom(productcatalog.UnitPrice{
+				Amount: alpacadecimal.NewFromInt(2),
+			})),
+			FeatureKey: featureKey,
+			RateCardDiscounts: billing.Discounts{
+				Usage: &billing.UsageDiscount{
+					UsageDiscount: productcatalog.UsageDiscount{
+						Quantity: alpacadecimal.NewFromInt(3),
+					},
+				},
+			},
+			ChildUniqueReferenceID: lo.ToPtr("manual-usage"),
+		},
+	}
+
+	flatLine := billing.NewFlatFeeGatheringLine(billing.NewFlatFeeLineInput{
+		Namespace:     ns,
+		Period:        servicePeriod,
+		InvoiceAt:     servicePeriod.From,
+		ManagedBy:     billing.ManuallyManagedLine,
+		Name:          "manual flat",
+		Currency:      USD,
+		PerUnitAmount: alpacadecimal.NewFromInt(10),
+		PaymentTerm:   productcatalog.InAdvancePaymentTerm,
+		RateCardDiscounts: billing.Discounts{
+			Percentage: &billing.PercentageDiscount{
+				PercentageDiscount: productcatalog.PercentageDiscount{
+					Percentage: models.NewPercentage(10),
+				},
+			},
+		},
+	})
+	flatLine.Engine = billing.LineEngineTypeInvoice
+	flatLine.ChildUniqueReferenceID = lo.ToPtr("manual-flat")
+
+	result, err := s.Charges.CreatePendingInvoiceLines(ctx, charges.CreatePendingInvoiceLinesInput{
+		Customer: cust.GetID(),
+		Currency: USD,
+		Lines: []billing.GatheringLine{
+			usageLine,
+			flatLine,
+		},
+	})
+	s.NoError(err)
+	s.Require().NotNil(result)
+	s.NotEmpty(result.Invoice.ID)
+	s.Require().Len(result.Lines, 2)
+
+	s.Equal("manual usage", result.Lines[0].Name)
+	s.Equal(billing.LineEngineTypeChargeUsageBased, result.Lines[0].Engine)
+	s.Require().NotNil(result.Lines[0].ChargeID)
+
+	s.Equal("manual flat", result.Lines[1].Name)
+	s.Equal(billing.LineEngineTypeChargeFlatFee, result.Lines[1].Engine)
+	s.Require().NotNil(result.Lines[1].ChargeID)
+
+	usageCharge := s.mustGetChargeByID(meta.ChargeID{
+		Namespace: ns,
+		ID:        lo.FromPtr(result.Lines[0].ChargeID),
+	})
+	usageBasedCharge, err := usageCharge.AsUsageBasedCharge()
+	s.NoError(err)
+	s.Equal(productcatalog.CreditThenInvoiceSettlementMode, usageBasedCharge.Intent.SettlementMode)
+	s.Equal(billing.ManuallyManagedLine, usageBasedCharge.Intent.ManagedBy)
+	s.Equal("manual-usage", lo.FromPtr(usageBasedCharge.Intent.UniqueReferenceID))
+	s.Equal(featureKey, usageBasedCharge.Intent.FeatureKey)
+	s.Require().NotNil(usageBasedCharge.Intent.Discounts.Usage)
+	s.Equal(float64(3), usageBasedCharge.Intent.Discounts.Usage.Quantity.InexactFloat64())
+
+	flatCharge := s.mustGetChargeByID(meta.ChargeID{
+		Namespace: ns,
+		ID:        lo.FromPtr(result.Lines[1].ChargeID),
+	})
+	flatFeeCharge, err := flatCharge.AsFlatFeeCharge()
+	s.NoError(err)
+	s.Equal(productcatalog.CreditThenInvoiceSettlementMode, flatFeeCharge.Intent.SettlementMode)
+	s.Equal(billing.ManuallyManagedLine, flatFeeCharge.Intent.ManagedBy)
+	s.Equal("manual-flat", lo.FromPtr(flatFeeCharge.Intent.UniqueReferenceID))
+	s.Equal(productcatalog.InAdvancePaymentTerm, flatFeeCharge.Intent.PaymentTerm)
+	s.Equal(float64(10), flatFeeCharge.Intent.AmountBeforeProration.InexactFloat64())
+	s.Require().NotNil(flatFeeCharge.Intent.PercentageDiscounts)
+	s.Equal(float64(10), flatFeeCharge.Intent.PercentageDiscounts.Percentage.InexactFloat64())
+}
+
+func (s *InvoicableChargesTestSuite) TestCreatePendingInvoiceLinesRollsBackCreatedChargesOnFailure() {
+	ctx := s.T().Context()
+	ns := s.GetUniqueNamespace("charges-service-create-pending-lines-rollback")
+	s.ProvisionDefaultTaxCodes(ctx, ns)
+
+	cust := s.CreateTestCustomer(ns, "test-subject")
+	s.NotEmpty(cust.ID)
+
+	servicePeriod := timeutil.ClosedPeriod{
+		From: datetime.MustParseTimeInLocation(s.T(), "2026-01-01T00:00:00Z", time.UTC).AsTime(),
+		To:   datetime.MustParseTimeInLocation(s.T(), "2026-02-01T00:00:00Z", time.UTC).AsTime(),
+	}
+
+	zeroFlatLine := billing.NewFlatFeeGatheringLine(billing.NewFlatFeeLineInput{
+		Namespace:     ns,
+		Period:        servicePeriod,
+		InvoiceAt:     servicePeriod.From,
+		ManagedBy:     billing.ManuallyManagedLine,
+		Name:          "manual zero flat",
+		Currency:      USD,
+		PerUnitAmount: alpacadecimal.Zero,
+		PaymentTerm:   productcatalog.InAdvancePaymentTerm,
+	})
+	zeroFlatLine.Engine = billing.LineEngineTypeInvoice
+	zeroFlatLine.ChildUniqueReferenceID = lo.ToPtr("manual-zero-flat")
+
+	result, err := s.Charges.CreatePendingInvoiceLines(ctx, charges.CreatePendingInvoiceLinesInput{
+		Customer: cust.GetID(),
+		Currency: USD,
+		Lines: []billing.GatheringLine{
+			zeroFlatLine,
+		},
+	})
+	s.Nil(result)
+	s.Require().Error(err)
+	s.Contains(err.Error(), "no gathering lines were created")
+
+	listed, err := s.Charges.ListCharges(ctx, charges.ListChargesInput{
+		Namespace:   ns,
+		CustomerIDs: []string{cust.ID},
+	})
+	s.NoError(err)
+	s.Empty(listed.Items)
+}
+
+func (s *InvoicableChargesTestSuite) TestCreatePendingInvoiceLinesRejectsNonManualInput() {
+	ctx := s.T().Context()
+	ns := s.GetUniqueNamespace("charges-service-create-pending-lines-policy")
+
+	cust := s.CreateTestCustomer(ns, "test-subject")
+	s.NotEmpty(cust.ID)
+
+	servicePeriod := timeutil.ClosedPeriod{
+		From: datetime.MustParseTimeInLocation(s.T(), "2026-01-01T00:00:00Z", time.UTC).AsTime(),
+		To:   datetime.MustParseTimeInLocation(s.T(), "2026-02-01T00:00:00Z", time.UTC).AsTime(),
+	}
+
+	systemLine := billing.NewFlatFeeGatheringLine(billing.NewFlatFeeLineInput{
+		Namespace:     ns,
+		Period:        servicePeriod,
+		InvoiceAt:     servicePeriod.From,
+		ManagedBy:     billing.SystemManagedLine,
+		Name:          "system flat",
+		Currency:      USD,
+		PerUnitAmount: alpacadecimal.NewFromInt(10),
+		PaymentTerm:   productcatalog.InAdvancePaymentTerm,
+	})
+
+	_, err := s.Charges.CreatePendingInvoiceLines(ctx, charges.CreatePendingInvoiceLinesInput{
+		Customer: cust.GetID(),
+		Currency: USD,
+		Lines: []billing.GatheringLine{
+			systemLine,
+		},
+	})
+	s.Require().Error(err)
+	s.Contains(err.Error(), "managed by must be manual")
+
+	subscriptionLine := billing.NewFlatFeeGatheringLine(billing.NewFlatFeeLineInput{
+		Namespace:     ns,
+		Period:        servicePeriod,
+		InvoiceAt:     servicePeriod.From,
+		ManagedBy:     billing.ManuallyManagedLine,
+		Name:          "subscription flat",
+		Currency:      USD,
+		PerUnitAmount: alpacadecimal.NewFromInt(10),
+		PaymentTerm:   productcatalog.InAdvancePaymentTerm,
+	})
+	subscriptionLine.Subscription = &billing.SubscriptionReference{
+		SubscriptionID: "sub-1",
+		PhaseID:        "phase-1",
+		ItemID:         "item-1",
+		BillingPeriod:  servicePeriod,
+	}
+
+	_, err = s.Charges.CreatePendingInvoiceLines(ctx, charges.CreatePendingInvoiceLinesInput{
+		Customer: cust.GetID(),
+		Currency: USD,
+		Lines: []billing.GatheringLine{
+			subscriptionLine,
+		},
+	})
+	s.Require().Error(err)
+	s.Contains(err.Error(), "subscription is not allowed")
+}
+
+func (s *InvoicableChargesTestSuite) TestCreatePendingInvoiceLinesRollsBackPartialChargeLineResults() {
+	ctx := s.T().Context()
+	ns := s.GetUniqueNamespace("charges-service-create-pending-lines-partial-rollback")
+	s.ProvisionDefaultTaxCodes(ctx, ns)
+
+	customInvoicing := s.SetupCustomInvoicing(ns)
+	cust := s.CreateTestCustomer(ns, "test-subject")
+	s.NotEmpty(cust.ID)
+
+	_ = s.ProvisionBillingProfile(ctx, ns, customInvoicing.App.GetID(),
+		billingtest.WithCollectionInterval(datetime.MustParseDuration(s.T(), "P2D")),
+		billingtest.WithManualApproval(),
+	)
+
+	servicePeriod := timeutil.ClosedPeriod{
+		From: datetime.MustParseTimeInLocation(s.T(), "2026-01-01T00:00:00Z", time.UTC).AsTime(),
+		To:   datetime.MustParseTimeInLocation(s.T(), "2026-02-01T00:00:00Z", time.UTC).AsTime(),
+	}
+
+	apiRequestsTotal := s.SetupApiRequestsTotalFeature(ctx, ns)
+
+	usageLine := billing.GatheringLine{
+		GatheringLineBase: billing.GatheringLineBase{
+			ManagedResource: models.NewManagedResource(models.ManagedResourceInput{
+				Namespace: ns,
+				Name:      "manual usage",
+			}),
+			ManagedBy:     billing.ManuallyManagedLine,
+			Engine:        billing.LineEngineTypeInvoice,
+			Currency:      USD,
+			ServicePeriod: servicePeriod,
+			InvoiceAt:     servicePeriod.To,
+			Price: lo.FromPtr(productcatalog.NewPriceFrom(productcatalog.UnitPrice{
+				Amount: alpacadecimal.NewFromInt(2),
+			})),
+			FeatureKey: apiRequestsTotal.Feature.Key,
+		},
+	}
+
+	zeroFlatLine := billing.NewFlatFeeGatheringLine(billing.NewFlatFeeLineInput{
+		Namespace:     ns,
+		Period:        servicePeriod,
+		InvoiceAt:     servicePeriod.From,
+		ManagedBy:     billing.ManuallyManagedLine,
+		Name:          "manual zero flat",
+		Currency:      USD,
+		PerUnitAmount: alpacadecimal.Zero,
+		PaymentTerm:   productcatalog.InAdvancePaymentTerm,
+	})
+	zeroFlatLine.Engine = billing.LineEngineTypeInvoice
+
+	result, err := s.Charges.CreatePendingInvoiceLines(ctx, charges.CreatePendingInvoiceLinesInput{
+		Customer: cust.GetID(),
+		Currency: USD,
+		Lines: []billing.GatheringLine{
+			usageLine,
+			zeroFlatLine,
+		},
+	})
+	s.Nil(result)
+	s.Require().Error(err)
+	s.Contains(err.Error(), "gathering line was not created")
+
+	listedCharges, err := s.Charges.ListCharges(ctx, charges.ListChargesInput{
+		Namespace:   ns,
+		CustomerIDs: []string{cust.ID},
+	})
+	s.NoError(err)
+	s.Empty(listedCharges.Items)
+
+	listedInvoices, err := s.BillingService.ListGatheringInvoices(ctx, billing.ListGatheringInvoicesInput{
+		Namespaces: []string{ns},
+		Customers:  []string{cust.ID},
+	})
+	s.NoError(err)
+	s.Empty(listedInvoices.Items)
+}
