5c8527158e2ad20e462fbda9f9176a8dc1987964
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2026-07-21 10:18:44 +0200

    refactor: validation of currencyx.Code

--- pkg/currencyx/code.go
+++ pkg/currencyx/code.go
@@ -3,24 +3,89 @@ package currencyx
 import (
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/invopop/gobl/currency"
+
+	"github.com/openmeterio/openmeter/pkg/models"
 )
 
-var _ fmt.Stringer = (*Code)(nil)
+var (
+	_ fmt.Stringer     = (*Code)(nil)
+	_ models.Validator = (*Code)(nil)
+)
 
 // Code represents a fiat or custom currency code. Code values used directly as
 // Currency values are treated as fiat currencies for backwards compatibility.
 type Code currency.Code
+
+const (
+	CustomCurrencyCodeMinLength = 4
+	CustomCurrencyCodeMaxLength = 24
+)
 
 func (c Code) String() string {
 	return string(c)
 }
 
 func (c Code) Validate() error {
+	var errs []error
+
 	if c == "" {
-		return errors.New("currency code is required")
+		errs = append(errs, errors.New("currency code is required"))
+
+		return models.NewNillableGenericValidationError(errors.Join(errs...))
 	}
 
-	return currency.Code(c).Validate()
+	if len(c) == 3 {
+		if err := validateFiatCurrencyCode(c); err != nil {
+			errs = append(errs, err)
+		}
+	} else {
+		if err := validateCustomCurrencyCode(c); err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	return models.NewNillableGenericValidationError(errors.Join(errs...))
+}
+
+func validateFiatCurrencyCode(code Code) error {
+	if len(code) != 3 {
+		return fmt.Errorf("invalid fiat currency code: %s", code)
+	}
+
+	definition := currency.Get(currency.Code(code))
+	if definition == nil || definition.ISONumeric == "" {
+		return fmt.Errorf("invalid fiat currency code: %s", code)
+	}
+
+	return nil
+}
+
+func validateCustomCurrencyCode(code Code) error {
+	var errs []error
+
+	if code == "" {
+		errs = append(errs, errors.New("currency code is required"))
+	}
+
+	codeString := code.String()
+	if len(codeString) != len(strings.TrimSpace(codeString)) {
+		errs = append(errs, fmt.Errorf("invalid currency code: cannot contain leading or trailing spaces: %s", code))
+	}
+
+	if strings.Contains(codeString, "|") {
+		errs = append(errs, fmt.Errorf("invalid currency code: cannot contain route delimiter: %s", code))
+	}
+
+	if fiatDefinition := currency.Get(currency.Code(code)); fiatDefinition != nil {
+		errs = append(errs, fmt.Errorf("currency code %s is a fiat currency", code))
+	}
+
+	if codeLength := len(codeString); codeLength < CustomCurrencyCodeMinLength || codeLength > CustomCurrencyCodeMaxLength {
+		errs = append(errs, fmt.Errorf("invalid currency code: it must be between %d and %d characters", CustomCurrencyCodeMinLength, CustomCurrencyCodeMaxLength))
+	}
+
+	return errors.Join(errs...)
 }
---
+++ pkg/currencyx/code_test.go
@@ -0,0 +1,81 @@
+package currencyx_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/openmeterio/openmeter/pkg/currencyx"
+	"github.com/openmeterio/openmeter/pkg/models"
+)
+
+func TestCodeValidate(t *testing.T) {
+	testCases := []struct {
+		name          string
+		code          currencyx.Code
+		expectedError string
+	}{
+		{
+			name:          "empty",
+			expectedError: "currency code is required",
+		},
+		{
+			name: "valid fiat",
+			code: "USD",
+		},
+		{
+			name:          "unknown three-character code",
+			code:          "ZZZ",
+			expectedError: "invalid fiat currency code",
+		},
+		{
+			name:          "non-fiat three-character code",
+			code:          "BTC",
+			expectedError: "invalid fiat currency code",
+		},
+		{
+			name: "custom minimum length",
+			code: "TOKN",
+		},
+		{
+			name: "custom maximum length",
+			code: currencyx.Code(strings.Repeat("A", currencyx.CustomCurrencyCodeMaxLength)),
+		},
+		{
+			name:          "custom code too short",
+			code:          "AB",
+			expectedError: "between 4 and 24 characters",
+		},
+		{
+			name:          "custom code too long",
+			code:          currencyx.Code(strings.Repeat("A", currencyx.CustomCurrencyCodeMaxLength+1)),
+			expectedError: "between 4 and 24 characters",
+		},
+		{
+			name:          "custom code contains route delimiter",
+			code:          "CRE|DITS",
+			expectedError: "cannot contain route delimiter",
+		},
+		{
+			name:          "custom code contains surrounding whitespace",
+			code:          " CREDITS",
+			expectedError: "cannot contain leading or trailing spaces",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			err := testCase.code.Validate()
+			if testCase.expectedError == "" {
+				require.NoError(t, err)
+
+				return
+			}
+
+			require.Error(t, err)
+			require.True(t, models.IsGenericValidationError(err))
+			require.Contains(t, err.Error(), testCase.expectedError)
+		})
+	}
+}
