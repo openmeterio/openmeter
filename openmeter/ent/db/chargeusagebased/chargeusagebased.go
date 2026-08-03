55905ea67d76777af3355bd274a8a2f594518b7f
Peter Turi <peter.turi@openmeter.io>
2026-08-03 17:27:58 +0200

    fix(billing): regenerate charge status schemas

--- openmeter/ent/db/chargeusagebased/chargeusagebased.go
+++ openmeter/ent/db/chargeusagebased/chargeusagebased.go
@@ -355,7 +355,7 @@ func RatingEngineValidator(re usagebased.RatingEngine) error {
 // StatusDetailedValidator is a validator for the "status_detailed" field enum values. It is called by the builders before save.
 func StatusDetailedValidator(sd usagebased.Status) error {
 	switch sd {
-	case "created", "active", "active.realization.started", "active.realization.waiting_for_collection", "active.realization.processing", "active.realization.issuing", "active.realization.completed", "active.awaiting_payment_settlement", "final", "deleted":
+	case "created", "active", "active.realization.started", "active.realization.waiting_for_collection", "active.realization.processing", "active.realization.issuing", "active.realization.zero_fiat_amount_overage_completed", "active.realization.completed", "active.awaiting_payment_settlement", "final", "deleted":
 		return nil
 	default:
 		return fmt.Errorf("chargeusagebased: invalid enum value for status_detailed field: %q", sd)
