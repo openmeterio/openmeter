ed04696f010e9554dd9bb1066351b2bc94046a29
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2026-08-04 11:28:32 +0200

    fix(streaming):  bind customer attribution map values (#4848)

--- openmeter/streaming/clickhouse/queryhelper.go
+++ openmeter/streaming/clickhouse/queryhelper.go
@@ -2,14 +2,11 @@ package clickhouse
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/huandu/go-sqlbuilder"
 
 	"github.com/openmeterio/openmeter/openmeter/streaming"
 )
-
-const subjectToCustomerIDDictionary = "subject_to_customer_id"
 
 // selectCustomerIdColumn
 func selectCustomerIdColumn(eventsTableName string, customers []streaming.Customer, query *sqlbuilder.SelectBuilder) *sqlbuilder.SelectBuilder {
@@ -23,42 +20,38 @@ func selectCustomerIdColumn(eventsTableName string, customers []streaming.Custom
 	subjectColumn := getColumn("subject")
 
 	// Build a map of event subjects to customer ids
-	var values []string
+	var subjectKeys, customerIDs []string
 
 	// For each customer, we map event subjects to customer ids
 	for _, customer := range customers {
-		customerIDSQL := fmt.Sprintf("'%s'", sqlbuilder.Escape(customer.GetUsageAttribution().ID))
+		usageAttribution := customer.GetUsageAttribution()
 
 		// We map the customer key to the customer id if it exists
-		if customer.GetUsageAttribution().Key != nil {
-			customerKeySQL := fmt.Sprintf("'%s'", sqlbuilder.Escape(*customer.GetUsageAttribution().Key))
-			values = append(values, customerKeySQL, customerIDSQL)
+		if usageAttribution.Key != nil {
+			subjectKeys = append(subjectKeys, *usageAttribution.Key)
+			customerIDs = append(customerIDs, usageAttribution.ID)
 		}
 
 		// We map each subject key to the customer id
-		for _, subjectKey := range customer.GetUsageAttribution().SubjectKeys {
-			subjectSQL := fmt.Sprintf("'%s'", sqlbuilder.Escape(subjectKey))
-
-			values = append(values, subjectSQL, customerIDSQL)
+		for _, subjectKey := range usageAttribution.SubjectKeys {
+			subjectKeys = append(subjectKeys, subjectKey)
+			customerIDs = append(customerIDs, usageAttribution.ID)
 		}
 	}
 
 	// If there are no values, we return an empty customer id column
 	// This can happen if none of the customers has key or usage attribution subjects
-	if len(values) == 0 {
+	if len(subjectKeys) == 0 {
 		return query.SelectMore("'' AS customer_id")
 	}
 
-	// Name of the map (dictionary)
-
-	mapSQL := fmt.Sprintf("WITH map(%s) as %s", strings.Join(values, ", "), subjectToCustomerIDDictionary)
-
-	// Add the map to query via WITH clause
-	mapQuery := sqlbuilder.ClickHouse.NewCTEBuilder().SQL(mapSQL)
-	query = query.With(mapQuery)
-
 	// Select the customer id column
-	query = query.SelectMore(fmt.Sprintf("%s[%s] AS customer_id", subjectToCustomerIDDictionary, subjectColumn))
+	query = query.SelectMore(fmt.Sprintf(
+		"mapFromArrays(%s, %s)[%s] AS customer_id",
+		query.Var(subjectKeys),
+		query.Var(customerIDs),
+		subjectColumn,
+	))
 
 	return query
 }
