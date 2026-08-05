273d3e31bc6395d9ab4e03f02e206b176bef6820
Mark Vass <mark.vass@konghq.com>
2026-08-05 14:05:56 +0200

    fix: remove guards

--- openmeter/ledger/chargeadapter/flatfee.go
+++ openmeter/ledger/chargeadapter/flatfee.go
@@ -288,9 +288,6 @@ func (h *flatFeeHandler) OnPaymentAuthorized(ctx context.Context, input flatfee.
 	}
 
 	intent := input.Charge.Intent
-	if intent.GetCurrency().IsCustom() {
-		return ledgertransaction.GroupReference{}, fmt.Errorf("payment authorized: %w", meta.ErrCustomCurrencyNotSupported)
-	}
 
 	invoiceCurrency, err := input.Charge.GetInvoiceCurrency()
 	if err != nil {
@@ -355,9 +352,6 @@ func (h *flatFeeHandler) OnPaymentSettled(ctx context.Context, input flatfee.OnP
 	}
 
 	intent := input.Charge.Intent
-	if intent.GetCurrency().IsCustom() {
-		return ledgertransaction.GroupReference{}, fmt.Errorf("payment settled: %w", meta.ErrCustomCurrencyNotSupported)
-	}
 
 	invoiceCurrency, err := input.Charge.GetInvoiceCurrency()
 	if err != nil {
