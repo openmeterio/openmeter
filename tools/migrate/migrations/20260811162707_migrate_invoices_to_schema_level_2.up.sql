fb263ff9745e39c3f36ef56231e6b0f3441cd739
Peter Turi <peter.turi@openmeter.io>
2026-08-12 06:23:41 +0200

    fix(billing): migrate invoices without lock rows

--- tools/migrate/migrations/20260811162707_migrate_invoices_to_schema_level_2.up.sql
+++ tools/migrate/migrations/20260811162707_migrate_invoices_to_schema_level_2.up.sql
@@ -10,11 +10,8 @@ CREATE TEMPORARY TABLE om_tmp_billing_schema_level_2_affected_customers (
 ) ON COMMIT DROP;
 
 INSERT INTO om_tmp_billing_schema_level_2_affected_customers (customer_id)
-SELECT DISTINCT l.customer_id::TEXT
-FROM billing_customer_locks l
-JOIN billing_invoices i
-    ON i.namespace = l.namespace
-    AND i.customer_id = l.customer_id
+SELECT DISTINCT i.customer_id::TEXT
+FROM billing_invoices i
 WHERE i.schema_level = 1;
 
 SELECT om_func_migrate_customer_invoices_to_schema_level_2_bulk(
