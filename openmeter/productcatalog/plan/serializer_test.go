1282ca53c99424438ffd2cfa0e69197c8e03ae3b
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2026-08-19 18:18:23 +0200

    test(productcatalog): distinguish serialized feature identity

--- openmeter/productcatalog/plan/serializer_test.go
+++ openmeter/productcatalog/plan/serializer_test.go
@@ -76,7 +76,7 @@ func TestPlanSerialization(t *testing.T) {
 								Metadata: models.Metadata{
 									"key3": "value3",
 								},
-								Feature: productcatalog.NewFeatureReference(lo.ToPtr("feature-1"), lo.ToPtr("feature-1")),
+								Feature: productcatalog.NewFeatureReference(lo.ToPtr("feature-id-1"), lo.ToPtr("feature-key-1")),
 								Price: productcatalog.NewPriceFrom(productcatalog.FlatPrice{
 									Amount:      decimal.NewFromInt(1000),
 									PaymentTerm: productcatalog.InAdvancePaymentTerm,
