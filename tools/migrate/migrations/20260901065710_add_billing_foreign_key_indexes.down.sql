750a4657f4334540ce860dd1996b98927989f1ed
Peter Turi <peter.turi@openmeter.io>
2026-09-01 16:32:45 +0200

    fix(billing): use single index migration

--- tools/migrate/migrations/20260901065710_add_billing_foreign_key_indexes.down.sql
+++ tools/migrate/migrations/20260901065710_add_billing_foreign_key_indexes.down.sql
@@ -1,16 +1,14 @@
--- atlas:txmode none
-
 -- reverse: create index "billing_std_invoice_detailed_lines_parent_id_idx" to table: "billing_standard_invoice_detailed_lines"
-DROP INDEX CONCURRENTLY "billing_std_invoice_detailed_lines_parent_id_idx";
+DROP INDEX "billing_std_invoice_detailed_lines_parent_id_idx";
 -- reverse: create index "billing_std_invoice_detail_amount_discounts_line_id_idx" to table: "billing_standard_invoice_detailed_line_amount_discounts"
-DROP INDEX CONCURRENTLY "billing_std_invoice_detail_amount_discounts_line_id_idx";
+DROP INDEX "billing_std_invoice_detail_amount_discounts_line_id_idx";
 -- reverse: create index "billing_invoice_lines_usage_config_id_idx" to table: "billing_invoice_lines"
-DROP INDEX CONCURRENTLY "billing_invoice_lines_usage_config_id_idx";
+DROP INDEX "billing_invoice_lines_usage_config_id_idx";
 -- reverse: create index "billing_invoice_lines_parent_id_idx" to table: "billing_invoice_lines"
-DROP INDEX CONCURRENTLY "billing_invoice_lines_parent_id_idx";
+DROP INDEX "billing_invoice_lines_parent_id_idx";
 -- reverse: create index "billing_invoice_lines_fee_config_id_idx" to table: "billing_invoice_lines"
-DROP INDEX CONCURRENTLY "billing_invoice_lines_fee_config_id_idx";
+DROP INDEX "billing_invoice_lines_fee_config_id_idx";
 -- reverse: create index "billing_invoice_line_usage_discounts_line_id_idx" to table: "billing_invoice_line_usage_discounts"
-DROP INDEX CONCURRENTLY "billing_invoice_line_usage_discounts_line_id_idx";
+DROP INDEX "billing_invoice_line_usage_discounts_line_id_idx";
 -- reverse: create index "billing_invoice_line_discounts_line_id_idx" to table: "billing_invoice_line_discounts"
-DROP INDEX CONCURRENTLY "billing_invoice_line_discounts_line_id_idx";
+DROP INDEX "billing_invoice_line_discounts_line_id_idx";
